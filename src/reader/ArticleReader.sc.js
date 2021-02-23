096165d86075480ddb4626fe0cf78c013a51286d
Mircea Lungu <mircea.lungu@gmail.com>
2021-02-23 22:46:12 +0100

    rename

--- src/reader/ArticleReader.sc.js
+++ src/reader/ArticleReader.sc.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-import { BigSquareButton } from '../components/buttons.sc'
+import { BigSquareButton } from '../components/allButtons.sc'
 
 import * as color from '../components/colors'
 
