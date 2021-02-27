7a18637befeb2cc9b315f8ff8f253a51024edcb2
Mircea Lungu <mircea.lungu@gmail.com>
2021-02-27 01:24:53 +0100

    students

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,82 +1,83 @@
-import { Link } from 'react-router-dom'
-import { useContext, useState } from 'react'
-import { UserContext } from '../UserContext'
+import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { UserContext } from "../UserContext";
 
-import * as s from './SideBar.sc'
+import * as s from "./SideBar.sc";
 
-export default function SideBar (props) {
-  const user = useContext(UserContext)
-  const [initialSidebarState, setInitialSidebarState] = useState(true)
+export default function SideBar(props) {
+  const user = useContext(UserContext);
+  const [initialSidebarState, setInitialSidebarState] = useState(true);
 
-  function toggleSidebar (e) {
-    e.preventDefault()
-    setInitialSidebarState(!initialSidebarState)
+  function toggleSidebar(e) {
+    e.preventDefault();
+    setInitialSidebarState(!initialSidebarState);
   }
 
-  function resetSidebarToDefault (e) {
-    setInitialSidebarState(true)
+  function resetSidebarToDefault(e) {
+    setInitialSidebarState(true);
   }
 
   let sidebarContent = (
     <>
-      <div className='logo'>
-        <a href='/articles' rel='external'>
+      <div className="logo">
+        <a href="/articles" rel="external">
           <img
-            src='/static/images/zeeguuWhiteLogo.svg'
-            alt='Zeeguu Logo - The Elephant'
+            src="/static/images/zeeguuWhiteLogo.svg"
+            alt="Zeeguu Logo - The Elephant"
           />
         </a>
       </div>
-      <div className='arrowHolder'>
-        <span className='toggleArrow' onClick={toggleSidebar}>
+      <div className="arrowHolder">
+        <span className="toggleArrow" onClick={toggleSidebar}>
           ▲
         </span>
       </div>
-      <div className='navigationLink'>
-        <Link to='/articles' onClick={resetSidebarToDefault}>
+      <div className="navigationLink">
+        <Link to="/articles" onClick={resetSidebarToDefault}>
           Articles
         </Link>
       </div>
-      <div className='navigationLink'>
-        <Link to='/words/history' onClick={resetSidebarToDefault}>
+      <div className="navigationLink">
+        <Link to="/words/history" onClick={resetSidebarToDefault}>
           Words
         </Link>
       </div>
-      <div className='navigationLink'>
-        <Link to='/exercises' onClick={resetSidebarToDefault}>
+      <div className="navigationLink">
+        <Link to="/exercises" onClick={resetSidebarToDefault}>
           Exercises
         </Link>
       </div>
-      {(user.is_teacher === 'true' || user.is_teacher === true) && (
-        <div className='navigationLink'>
+      {(user.is_teacher === "true" || user.is_teacher === true) && (
+        <div className="navigationLink">
           <Link
-            target='_blank'
-            to='/teacher-dashboard'
+            target="_blank"
+            to="/teacher-dashboard"
             onClick={resetSidebarToDefault}
           >
-            <small>My Students</small>
+            Students
           </Link>
         </div>
       )}
 
-      <div className='navigationLink'>
-        <Link to='/account_settings' onClick={resetSidebarToDefault}>
+      <br />
+      <div className="navigationLink">
+        <Link to="/account_settings" onClick={resetSidebarToDefault}>
           Settings
         </Link>
       </div>
       <br />
-      <div className='navigationLink'>
+      <div className="navigationLink">
         <Link
-          to='/'
+          to="/"
           onClick={() => {
-            user.logoutMethod()
+            user.logoutMethod();
           }}
         >
           <small>Logout</small>
         </Link>
       </div>
     </>
-  )
+  );
 
   if (!initialSidebarState) {
     return (
@@ -84,7 +85,7 @@ export default function SideBar (props) {
         {sidebarContent}
         <s.MainContentToggled>{props.children}</s.MainContentToggled>
       </s.SideBarToggled>
-    )
+    );
   }
 
   return (
@@ -92,5 +93,5 @@ export default function SideBar (props) {
       {sidebarContent}
       <s.MainContentInitial>{props.children}</s.MainContentInitial>
     </s.SideBarInitial>
-  )
+  );
 }
