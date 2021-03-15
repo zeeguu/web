69b86b0e12f0d8357d52d5ab60c9feb9a17e4906
Mircea Lungu <mircea.lungu@gmail.com>
2021-03-15 16:32:08 +0100

    towards loaclization

---
+++ src/i18n/definitions.js
@@ -0,0 +1,17 @@
+import LocalizedStrings from "localized-strings";
+
+let strings = new LocalizedStrings(
+  {
+    en: {
+      articles: "Texts",
+    },
+    da: {
+      articles: "Tekster",
+    },
+  },
+  {
+    /* options */
+  }
+);
+
+export { strings };
