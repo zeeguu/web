0253629f80760b456cf39d9bd2da3c3f8f828a15
Sara Tolstrup Husum <sarathusum@gmail.com>
2021-03-25 20:46:47 +0100

    Basic routing on the teacher side works

--- src/teacher/TeacherTexts.js
+++ src/teacher/TeacherTexts.js
@@ -2,11 +2,11 @@ import strings from "../i18n/definitions";
 import * as s from "../components/NarrowColumn.sc";
 import * as sc from "../components/TopTabs.sc";
 
-export default function TeacherClasses() {
+export default function TeacherTexts() {
   return (
       <s.NarrowColumn>
         <sc.TopTabs>
-          <h1>{strings.myClasses}</h1>
+          <h1>{strings.myTexts}</h1>
         </sc.TopTabs>
       </s.NarrowColumn>
   );
