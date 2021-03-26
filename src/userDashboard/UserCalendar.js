78c5bf7e9bb6cf17542c2ac4ada27f1938f35238
Maria <maria90timis@yahoo.com>
2021-03-26 10:49:37 +0100

    user calendar for word counts

---
+++ src/userDashboard/UserCalendar.js
@@ -0,0 +1,37 @@
+import { ResponsiveCalendar } from '@nivo/calendar'
+
+export default function UserCalendar ({ data }) {
+
+    const startDate = data[0].day;
+    const endDate = data[data.length -1].day;
+
+    return (
+        <>
+        <h1>Number of translated words / date</h1>
+        <ResponsiveCalendar
+            data={data}
+            from={startDate}
+            to={endDate}
+            emptyColor="#eeeeee"
+            colors={[ '#61cdbb', '#97e3d5', '#e8c1a0', '#f47560' ]}
+            margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
+            yearSpacing={40}
+            monthBorderColor="#ffffff"
+            dayBorderWidth={2}
+            dayBorderColor="#ffffff"
+            legends={[
+                {
+                    anchor: 'bottom-right',
+                    direction: 'row',
+                    translateY: 36,
+                    itemCount: 4,
+                    itemWidth: 42,
+                    itemHeight: 36,
+                    itemsSpacing: 14,
+                    itemDirection: 'right-to-left'
+                }
+            ]}
+        />
+        </>
+    )
+}
