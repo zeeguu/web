dc07527c728edcb8c453902326c56213c4cd410e
Sara Tolstrup Husum <sarathusum@gmail.com>
2021-03-26 16:07:40 +0100

    Colour change in SideBar implemented

--- src/components/colors.js
+++ src/components/colors.js
@@ -1,5 +1,8 @@
+
 let lightOrange = "#ffd047";
 let veryPaleOrange = "#ffbe007a";
 let zeeguuOrange = "#ffbb54";
+let lightBlue = "#4492b3";
+let darkBlue = "#54cdff";
 
-export { lightOrange, veryPaleOrange, zeeguuOrange };
+export { lightOrange, veryPaleOrange, zeeguuOrange, lightBlue, darkBlue };
