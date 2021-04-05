4f5e8f6ae64a7acc270c10b6815ee9053e3acb99
Sara Tolstrup Husum <sarathusum@gmail.com>
2021-04-05 23:13:02 +0200

    CleanUp and Tutorials layout in place

--- src/teacher/Tutorials.js
+++ src/teacher/Tutorials.js
@@ -1,13 +1,16 @@
 import strings from "../i18n/definitions";
+import TutorialItemCard from "./TutorialItemCard";
 import * as s from "../components/NarrowColumn.sc";
 import * as sc from "../components/TopTabs.sc";
 
-export default function TeacherTutorials() {
+export default function Tutorials() {
   return (
       <s.NarrowColumn>
         <sc.TopTabs>
           <h1>{strings.tutorials}</h1>
         </sc.TopTabs>
+        <TutorialItemCard headline="Testing with random video..." video="https://www.youtube.com/embed/WmVLcj-XKnM" />
+        <TutorialItemCard headline="Testing with no video..." />
       </s.NarrowColumn>
   );
 }
