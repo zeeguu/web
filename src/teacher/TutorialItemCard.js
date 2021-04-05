ffc88c2376eef536ff5ebd52fa87fa2ee3468318
Sara Tolstrup Husum <sarathusum@gmail.com>
2021-04-05 23:52:03 +0200

    Tutorials layout implemented

--- src/teacher/TutorialItemCard.js
+++ src/teacher/TutorialItemCard.js
@@ -7,16 +7,21 @@ function TutorialItemCard(props) {
         <div className="vertical-line-border-box">
           <p className="tutorialTitle">{props.headline}</p>
         </div>
-        {props.video ?
-        <iframe
-          width="644"
-          height="362"
-          src={props.video}
-          title={props.headline}
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-        /> : <div className="placeholder">This video tutorial is coming soon...</div>}
+        {props.video ? (
+          <iframe
+            width="644"
+            height="362"
+            src={props.video}
+            title={props.headline}
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          />
+        ) : (
+          <div className="placeholder">
+            <p>This video tutorial is coming soon...(STRINGS)</p>
+          </div>
+        )}
       </s.StyledTutorialItemCard>
     </>
   );
