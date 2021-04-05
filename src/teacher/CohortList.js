8b28468ad91af32bf1fbb7388092a1eba6e9210c
Sara Tolstrup Husum <sarathusum@gmail.com>
2021-04-05 15:26:45 +0200

    dummy rendering/css implemented

--- src/teacher/CohortList.js
+++ src/teacher/CohortList.js
@@ -2,31 +2,25 @@ import strings from "../i18n/definitions";
 import * as s from "../components/NarrowColumn.sc";
 import * as sc from "../components/TopTabs.sc";
 import { Link } from "react-router-dom";
+import { CohortItemCard } from "./CohortItemCard";
+import { StyledButton, TopButton } from "./TeacherButtons.sc";
 
 export default function CohortList() {
   const cohortID = "ClassName(HARDCODED)";
+
   return (
     <>
       <s.NarrowColumn>
         <sc.TopTabs>
           <h1>{strings.myClasses}</h1>
         </sc.TopTabs>
-        <p>
+        <TopButton>
+          <StyledButton primary>Add class (STRINGS)</StyledButton>
+        </TopButton>
+        <CohortItemCard />
+git         <p>
           <br />
-          <button>STRINGSAdd class</button>
-          <br />
-          <br />
-          <b>ClassName</b> <br /> ... languange, invite code, no of students
-          everywhere...<br/>
-      
-        <Link to={`/teacher/classes/viewClass/${cohortID}`}>
-          <button>STRINGSSee students' activity</button>
-        </Link>{" "}
-        <br />
-        <button>STRINGSEdit class</button>
-        <br/> 
-        <br/>
-        ("Add class" and "Edit class"opens popup.)
+          ("Add class" and "Edit class"opens popup.)
         </p>
       </s.NarrowColumn>
     </>
