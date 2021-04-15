889b9137e6fa56fec35e2c795b4c3fe26bfc1390
Sara Tolstrup Husum <sarathusum@gmail.com>
2021-04-15 17:07:18 +0200

    CohortFormInputFields css improved

--- src/teacher/CohortFormInputFields.js
+++ src/teacher/CohortFormInputFields.js
@@ -1,30 +1,29 @@
 import React, { Fragment } from "react";
 import { Listbox, ListboxOption } from "@reach/listbox";
-import "@reach/listbox/styles.css";
+import * as s from "./CohortFormInputFields.sc";
 
 import { TextField } from "@material-ui/core";
 
 export const CohortNameTextfield = ({ value, onChange }) => {
   return (
     <Fragment>
-      <label htmlFor="cohort_name">Choose the class name STRINGS</label>
-      <TextField
-        aria-label="cohort_name"
-        value={value}
-        onChange={onChange}
-        name="cohort_name"
-        id="cohort_name"
-        placeholder="  ex: 'B-level 2021'"
-        fullWidth
-        type="text"
-        required
-        style={{
-          border: "solid 2px #4492b3",
-          borderRadius: 5,
-          marginTop: 10,
-          marginBottom: 10,
-        }}
-      />
+      <s.CohortFormInputFields>
+        <div className="input-container">
+          <label htmlFor="cohort_name">Choose the class name STRINGS</label>
+          <TextField
+            className="input-field"
+            aria-label="cohort_name"
+            value={value}
+            onChange={onChange}
+            name="cohort_name"
+            id="cohort_name"
+            placeholder="  ex: 'B-level 2021'"
+            fullWidth
+            type="text"
+            required
+          />
+        </div>
+      </s.CohortFormInputFields>
     </Fragment>
   );
 };
@@ -32,24 +31,23 @@ export const CohortNameTextfield = ({ value, onChange }) => {
 export const InviteCodeTextfield = ({ value, onChange }) => {
   return (
     <Fragment>
-      <label htmlFor="invite_code">Choose an invitecode STRINGS</label>
-      <TextField
-        aria-label="invite_code"
-        value={value}
-        onChange={onChange}
-        name="invite_code"
-        id="invite_code"
-        placeholder="  ex. 'L34n1ng4u'"
-        fullWidth
-        type="text"
-        required
-        style={{
-          border: "solid 2px #4492b3",
-          borderRadius: 5,
-          marginTop: 10,
-          marginBottom: 10,
-        }}
-      />
+      <s.CohortFormInputFields>
+        <div className="input-container">
+          <label htmlFor="invite_code">Choose an invitecode STRINGS</label>
+          <TextField
+            className="input-field"
+            aria-label="invite_code"
+            value={value}
+            onChange={onChange}
+            name="invite_code"
+            id="invite_code"
+            placeholder="  ex. 'L34n1ng4u'"
+            fullWidth
+            type="text"
+            required
+          />
+        </div>
+      </s.CohortFormInputFields>
     </Fragment>
   );
 };
@@ -57,30 +55,28 @@ export const InviteCodeTextfield = ({ value, onChange }) => {
 export function LanguageSelector({ value, onChange }) {
   return (
     <Fragment>
-      <label htmlFor="language_id"></label>
-      Choose the classroom language STRINGS
-      <Listbox
-        id="language_id"
-        style={{
-          border: "solid 2px #4492b3",
-          borderRadius: 5,
-          marginTop: 10,
-          marginBottom: 10,
-          minWidth: "99%",
-        }}
-        aria-labelledby="language_id"
-        value={value}
-        onChange={onChange}
-      >
-        <ListboxOption value="zh-CN">Chinese STRINGS</ListboxOption>
-        <ListboxOption value="da">Danish STRINGS</ListboxOption>
-        <ListboxOption value="nl">Dutch STRINGS</ListboxOption>
-        <ListboxOption value="en">English STRINGS</ListboxOption>
-        <ListboxOption value="fr">French STRINGS</ListboxOption>
-        <ListboxOption value="de">German STRINGS</ListboxOption>
-        <ListboxOption value="it">Italian STRINGS</ListboxOption>
-        <ListboxOption value="es">Spanish STRINGS</ListboxOption>
-      </Listbox>
+      <s.CohortFormInputFields>
+        <div className="input-container">
+          <label htmlFor="language_id"></label>
+          Choose the classroom language STRINGS
+          <Listbox
+            className="input-field"
+            id="language_id"
+            aria-labelledby="language_id"
+            value={value}
+            onChange={onChange}
+          >
+            <ListboxOption value="zh-CN">Chinese STRINGS</ListboxOption>
+            <ListboxOption value="da">Danish STRINGS</ListboxOption>
+            <ListboxOption value="nl">Dutch STRINGS</ListboxOption>
+            <ListboxOption value="en">English STRINGS</ListboxOption>
+            <ListboxOption value="fr">French STRINGS</ListboxOption>
+            <ListboxOption value="de">German STRINGS</ListboxOption>
+            <ListboxOption value="it">Italian STRINGS</ListboxOption>
+            <ListboxOption value="es">Spanish STRINGS</ListboxOption>
+          </Listbox>
+        </div>
+      </s.CohortFormInputFields>
     </Fragment>
   );
 }
