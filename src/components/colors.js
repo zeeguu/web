eccfe9bbc773c4ea9e00687c44e99fe2421c9d1d
Mircea Lungu <mircea.lungu@gmail.com>
2021-04-16 15:15:48 +0300

    Revert "manually solving recuring git-rebase problem"
    This reverts commit 4edac29eda8646be2cc652c95ff9640e8d13b8d2.

--- src/components/colors.js
+++ src/components/colors.js
@@ -1,17 +1,5 @@
 let lightOrange = "#ffd047";
 let veryPaleOrange = "#ffbe007a";
 let zeeguuOrange = "#ffbb54";
-let lightBlue = "#54cdff";
-let darkBlue = "#4492b3";
-let darkGrey = "#808080";
-let lightGrey = "#c1c1c1";
 
-export {
-  lightOrange,
-  veryPaleOrange,
-  zeeguuOrange,
-  lightBlue,
-  darkBlue,
-  darkGrey,
-  lightGrey,
-};
+export { lightOrange, veryPaleOrange, zeeguuOrange };
