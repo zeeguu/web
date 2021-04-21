98e57caffdc55acf47066b5ae7fac35bca65a630
Sara Tolstrup Husum <sarathusum@gmail.com>
2021-04-21 17:14:35 +0200

    mmissed minor bug fix from before

--- src/teacher/StyledDialog.sc.js
+++ src/teacher/StyledDialog.sc.js
@@ -3,6 +3,6 @@ import { Dialog } from "@reach/dialog";
 import "@reach/dialog/styles.css";
 
 export const StyledDialog = styled(Dialog)`
-  max-width: ${(props) => props.maxWidth};
+  max-width: ${(props) => props.max_width};
   border-radius: 15px;
 `;
