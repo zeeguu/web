ca000c09fca9dc95b58a9a9916652caed537d848
eleonora-k <eleonora.kurilchik@gmail.com>
2021-04-23 18:35:21 +0200

    title instead of class id

--- src/teacher/StudentsActivityOverview.js
+++ src/teacher/StudentsActivityOverview.js
@@ -1,28 +1,37 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import StudentInfoLine from "./StudentInfoLine";
 import { StyledButton, TopButtonWrapper } from "./TeacherButtons.sc";
 import * as s from "../components/ColumnWidth.sc";
 import * as sc from "../components/TopTabs.sc";
 
-export default function StudentsActivityOverview() {
+export default function StudentsActivityOverview({api}) {
   const cohortID = useParams().cohortID;
-  //TODO We need a way to turn a cohortID into a cohort.name - maybe an api-call: api.getCohortName(cohortID){//returns the name of the cohort that has the cohortID}
+  const [cohortName, setCohortName] = useState("");
+  //TODO Would be nicer to have an api-call: api.getCohortName(cohortID){//returns the name of the cohort that has the cohortID} instead of the mess below
   const studentID = "StudentName(HARDCODED)";
+ 
+  api.getCohortsInfo((res) => {
+    const b = res.filter(
+      (cohort) => cohort.id ===cohortID
+      );
+      setCohortName(b[0].name)
+      console.log(cohortName);
+  });
   return (
     <Fragment>
       <s.WideColumn>
         <sc.TopTabs>
-          <h1>{cohortID}</h1>
+          <h1>{cohortName}</h1>
         </sc.TopTabs>
         <div>
           <br />
           <br />
           <TopButtonWrapper>
             <Link to="/teacher/texts/AddTextOptions">
-              <StyledButton primary>STRINGSAdd text</StyledButton>
+              <StyledButton primary>STRINGS Add text</StyledButton>
             </Link>
-            <StyledButton primary>STRINGAdd student</StyledButton>
+            <StyledButton primary>STRINGS Add student</StyledButton>
           </TopButtonWrapper>
           <br />
           <br />
