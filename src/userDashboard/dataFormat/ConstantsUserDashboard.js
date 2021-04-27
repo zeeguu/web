53316c4641299330469f5c4ff0f7f0ef526ec90d
Mircea Lungu <mircea.lungu@gmail.com>
2021-04-27 10:17:35 +0300

    extracting strings to separate i18n file

--- src/userDashboard/dataFormat/ConstantsUserDashboard.js
+++ src/userDashboard/dataFormat/ConstantsUserDashboard.js
@@ -2,8 +2,10 @@
  * Constants used for the UserDashboard module
  */
 
+import udstrings from "../../i18n/userDashboard";
+
 const OPTIONS = {
-  WEEK: "Last 7 days",
+  WEEK: udstrings.last7days,
   MONTH: "Last 30 days",
   YEAR: "Last 12 months",
   YEARS: "Available Years",
