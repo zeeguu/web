34e1943cd5864be6498d430b82a6c074b0b82c5f
Maria <maria90timis@yahoo.com>
2021-04-29 10:26:38 +0200

    strings wip

--- src/i18n/userDashboard.js
+++ src/i18n/userDashboard.js
@@ -5,13 +5,49 @@ let compactFormat = {
     en: "Dashboard",
     da: "Dashboard",
   },
+  tabActivity: {
+    en: "Activity",
+    da: "Aktivitet",
+  },
+  tabWords: {
+    en: "Words",
+    da: "Ord",
+  },
   minutes: {
     en: "minutes",
     da: "minutter",
   },
+  hours: {
+    en: "hours",
+    da: "timer",
+  },
   last7days: {
     en: "Last 7 days",
-    da: "",
+    da: "Sidste 7 dage",
+  },
+  last30days: {
+    en: "Last 30 days",
+    da: "Sidste 30 dage",
+  },
+  last12months: {
+    en: "Last 12 months",
+    da: "Sidste 12 måneder",
+  },
+  availableYears: {
+    en: "Available years",
+    da: "Tilgængelige år",
+  },
+  custom7DaysUntil: {
+    en: "7 days until",
+    da: "7 dage indtil d.",
+  },
+  custom30DaysUntil: {
+    en: "30 days until",
+    da: "30 dage indtil d.",
+  },
+  custom12MonthsUntil: {
+    en: "12 months until",
+    da: "12 måneder indtil d.",
   },
 };
 
