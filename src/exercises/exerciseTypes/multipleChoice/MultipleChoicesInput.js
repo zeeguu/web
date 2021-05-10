3474cd4b9a44e6ff59f2c848a49fe9e57065138c
Beatrice Verzier <beam@itu.dk>
2021-05-10 15:13:42 +0200

    animation on wrong answer for exercises

--- src/exercises/exerciseTypes/multipleChoice/MultipleChoicesInput.js
+++ src/exercises/exerciseTypes/multipleChoice/MultipleChoicesInput.js
@@ -1,33 +1,34 @@
-import { useState, useEffect } from "react";
 import * as s from "../Exercise.sc";
 
 function MultipleChoicesInput({
   buttonOptions,
   notifyChoiceSelection,
   incorrectAnswer,
+  setIncorrectAnswer,
 }) {
-  const [incorrectAnswers, setIncorrectAnswers] = useState([]);
-
-  useEffect(() => {
-    if (incorrectAnswer) {
-      setIncorrectAnswers([...incorrectAnswers, incorrectAnswer]);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [notifyChoiceSelection]);
-
   return (
     <s.BottomRow>
       {buttonOptions ? (
-        buttonOptions.map((option) => (
-          <s.OrangeButton
-            key={option}
-            id={option}
-            onClick={(e) => notifyChoiceSelection(e.target.id)}
-            disabled={incorrectAnswer && incorrectAnswers.includes(option)}
-          >
-            {option}
-          </s.OrangeButton>
-        ))
+        buttonOptions.map((option) =>
+          incorrectAnswer === option ? (
+            <s.AnimatedOrangeButton
+              key={option}
+              id={option}
+              onClick={(e) => notifyChoiceSelection(e.target.id)}
+              onAnimationEnd={() => setIncorrectAnswer("")}
+            >
+              {option}
+            </s.AnimatedOrangeButton>
+          ) : (
+            <s.OrangeButton
+              key={option}
+              id={option}
+              onClick={(e) => notifyChoiceSelection(e.target.id)}
+            >
+              {option}
+            </s.OrangeButton>
+          )
+        )
       ) : (
         <></>
       )}
