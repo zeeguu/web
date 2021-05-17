698cf9bed3611b4b4744216313cc4884f80ebd40
Beatrice Verzier <beam@itu.dk>
2021-05-17 18:37:43 +0200

    Removed debugging logs

--- src/exercises/exerciseTypes/findWordInContext/FindWordInContext.js
+++ src/exercises/exerciseTypes/findWordInContext/FindWordInContext.js
@@ -42,8 +42,6 @@ export default function FindWordInContext({
   }
 
   function handleAnswer(message, duration) {
-    console.log(message);
-
     api.uploadExerciseFeedback(
       message,
       EXERCISE_TYPE,
