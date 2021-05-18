6e2ab51ebcc680ead82c340ff8712b1eba473c1a
Maria <maria90timis@yahoo.com>
2021-05-18 14:17:22 +0300

    changed the default max value for graph to change for minutes/hours

--- src/userDashboard/userdashboard_Graphs/ReadingAndExercisesTimeGraph.js
+++ src/userDashboard/userdashboard_Graphs/ReadingAndExercisesTimeGraph.js
@@ -3,11 +3,13 @@ import {
   BAR_GRAPH_KEYS,
   DEFAULT_MAX_VALUE_BAR_GRAPH,
   PERIOD_OPTIONS,
+  ACTIVITY_TIME_FORMAT_OPTIONS,
 } from "../ConstantsUserDashboard";
 
 export default function ReadingAndExercisesTimeGraph({
   data,
   activeCustomTimeInterval,
+  activeTimeFormatOption,
 }) {
   function getMaxYValueFromData(data) {
     var max = 0;
@@ -21,25 +23,58 @@ export default function ReadingAndExercisesTimeGraph({
     return max;
   }
 
-  function getMaxValueForYScale(activeCustomTimeInterval, data) {
+  /**
+   * Function to determine the highest y value to be shown on the graph.
+   *
+   * The default values corresponding to time period and count options
+   * can be seen in DEFAULT_MAX_VALUE_BAR_GRAPH from ConstantsUserDashboard.
+   *
+   * If one of the values from the data set received is higher than
+   * the corresponding default value for period and count,
+   * then set the maxValue to "auto" in the nivo graph,
+   * which will set the highest y value to the highest value from the data.
+   *
+   */
+
+  function getMaxValueForYScale(
+    activeCustomTimeInterval,
+    activeTimeFormatOption,
+    data
+  ) {
     var maxValueFromData = getMaxYValueFromData(data);
 
     switch (activeCustomTimeInterval) {
       case PERIOD_OPTIONS.WEEK:
-        return maxValueFromData < DEFAULT_MAX_VALUE_BAR_GRAPH.WEEK
-          ? DEFAULT_MAX_VALUE_BAR_GRAPH.WEEK
+        return activeTimeFormatOption === ACTIVITY_TIME_FORMAT_OPTIONS.MINUTES
+          ? maxValueFromData < DEFAULT_MAX_VALUE_BAR_GRAPH.WEEK.MINUTES
+            ? DEFAULT_MAX_VALUE_BAR_GRAPH.WEEK.MINUTES
+            : DEFAULT_MAX_VALUE_BAR_GRAPH.AUTO
+          : maxValueFromData < DEFAULT_MAX_VALUE_BAR_GRAPH.WEEK.HOURS
+          ? DEFAULT_MAX_VALUE_BAR_GRAPH.WEEK.HOURS
           : DEFAULT_MAX_VALUE_BAR_GRAPH.AUTO;
       case PERIOD_OPTIONS.MONTH:
-        return maxValueFromData < DEFAULT_MAX_VALUE_BAR_GRAPH.MONTH
-          ? DEFAULT_MAX_VALUE_BAR_GRAPH.MONTH
+        return activeTimeFormatOption === ACTIVITY_TIME_FORMAT_OPTIONS.MINUTES
+          ? maxValueFromData < DEFAULT_MAX_VALUE_BAR_GRAPH.MONTH.MINUTES
+            ? DEFAULT_MAX_VALUE_BAR_GRAPH.MONTH.MINUTES
+            : DEFAULT_MAX_VALUE_BAR_GRAPH.AUTO
+          : maxValueFromData < DEFAULT_MAX_VALUE_BAR_GRAPH.MONTH.HOURS
+          ? DEFAULT_MAX_VALUE_BAR_GRAPH.MONTH.HOURS
           : DEFAULT_MAX_VALUE_BAR_GRAPH.AUTO;
       case PERIOD_OPTIONS.YEAR:
-        return maxValueFromData < DEFAULT_MAX_VALUE_BAR_GRAPH.YEAR
-          ? DEFAULT_MAX_VALUE_BAR_GRAPH.YEAR
+        return activeTimeFormatOption === ACTIVITY_TIME_FORMAT_OPTIONS.MINUTES
+          ? maxValueFromData < DEFAULT_MAX_VALUE_BAR_GRAPH.YEAR.MINUTES
+            ? DEFAULT_MAX_VALUE_BAR_GRAPH.YEAR.MINUTES
+            : DEFAULT_MAX_VALUE_BAR_GRAPH.AUTO
+          : maxValueFromData < DEFAULT_MAX_VALUE_BAR_GRAPH.YEAR.HOURS
+          ? DEFAULT_MAX_VALUE_BAR_GRAPH.YEAR.HOURS
           : DEFAULT_MAX_VALUE_BAR_GRAPH.AUTO;
       case PERIOD_OPTIONS.YEARS:
-        return maxValueFromData < DEFAULT_MAX_VALUE_BAR_GRAPH.YEARS
-          ? DEFAULT_MAX_VALUE_BAR_GRAPH.YEARS
+        return activeTimeFormatOption === ACTIVITY_TIME_FORMAT_OPTIONS.MINUTES
+          ? maxValueFromData < DEFAULT_MAX_VALUE_BAR_GRAPH.YEARS.MINUTES
+            ? DEFAULT_MAX_VALUE_BAR_GRAPH.YEARS.MINUTES
+            : DEFAULT_MAX_VALUE_BAR_GRAPH.AUTO
+          : maxValueFromData < DEFAULT_MAX_VALUE_BAR_GRAPH.YEARS.HOURS
+          ? DEFAULT_MAX_VALUE_BAR_GRAPH.YEARS.HOURS
           : DEFAULT_MAX_VALUE_BAR_GRAPH.AUTO;
       default:
         return DEFAULT_MAX_VALUE_BAR_GRAPH.AUTO;
@@ -53,7 +88,11 @@ export default function ReadingAndExercisesTimeGraph({
       indexBy={BAR_GRAPH_KEYS.INDEX_BY}
       margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
       padding={0.3}
-      maxValue={getMaxValueForYScale(activeCustomTimeInterval, data)}
+      maxValue={getMaxValueForYScale(
+        activeCustomTimeInterval,
+        activeTimeFormatOption,
+        data
+      )}
       valueScale={{ type: "linear" }}
       indexScale={{ type: "band", round: true }}
       colors={{ scheme: "nivo" }}
