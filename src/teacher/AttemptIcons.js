3d88972685d590d44df0aa1dea903ef91b13029d
Sara Tolstrup Husum <sarathusum@gmail.com>
2021-05-23 14:52:43 +0200

    further in showing exercise data (inline styling)

---
+++ src/teacher/AttemptIcons.js
@@ -0,0 +1,58 @@
+import CheckRoundedIcon from "@material-ui/icons/CheckRounded";
+import ClearRoundedIcon from "@material-ui/icons/ClearRounded";
+import PriorityHighRoundedIcon from "@material-ui/icons/PriorityHighRounded";
+
+const CorrectAttempt = () => {
+  return (
+    <CheckRoundedIcon
+      style={{ color: "green", margin: "12px -3px 0 -2px", fontSize: 18 }}
+    />
+  );
+};
+
+const WrongAttempt = () => {
+  return (
+    <ClearRoundedIcon
+      style={{ color: "red", margin: "12px -3px 0 -2px", fontSize: 18 }}
+    />
+  );
+};
+
+const SolutionShown = () => {
+  return (
+    <PriorityHighRoundedIcon
+      style={{ margin: "13px -5px 0 -3.5px", fontSize: "15px" }}
+    />
+  );
+};
+
+const HintUsed = () => {
+  return (
+    <p style={{ color: "orange", fontWeight: 600, margin: "12px 0px 0 1.5px", fontSize: "14px" }}>
+      ?
+    </p>
+  );
+};
+
+export const AttemptIcons = ({ attemptString }) => {
+  const setIcon = (char) => {
+    switch (char) {
+      case "w":
+        return <WrongAttempt />;
+      case "h":
+        return <HintUsed />;
+      case "s":
+        return <SolutionShown />;
+
+      default:
+        return <CorrectAttempt />;
+    }
+  };
+
+  const attemptChars = attemptString.split("");
+  return(
+    <div style={{display:"flex", flexDirection:"row", marginLeft:".5em", marginRight:"1em"}}>
+    {attemptChars.map((char) => setIcon(char))}
+    </div>
+  );
+};
--- src/teacher/WordsDropDown.js
+++ src/teacher/WordsDropDown.js
@@ -1,4 +1,4 @@
-import { CorrectAttempt, HintUsed, SolutionShown, WrongAttempt } from "./StudentExerciseAttemptIcons";
+import { AttemptIcons } from "./AttemptIcons";
 import { v4 as uuid } from "uuid";
 
 const WordsDropDown = ({ headline, words }) => {
@@ -13,27 +13,47 @@ const WordsDropDown = ({ headline, words }) => {
       }}
     >
       <h3 style={{ color: "#5492b3" }}>{headline}</h3>
-      {words.map((word) => (
-        <>
-      <p style={{color:"#44cdff", marginBottom:"-15px"}}>{word.translation}</p>
-        <div key={uuid()} style={{display:"flex", flexDirection:"row"}} >
-          <p>{word.word}</p>
-          <HintUsed />
-          <CorrectAttempt/>
-          <CorrectAttempt/>
-          <WrongAttempt/>
-          <SolutionShown/>
-          <CorrectAttempt/>
-          <WrongAttempt/>
-          <HintUsed/>
-          <CorrectAttempt/>
-          <SolutionShown/>
-          <HintUsed/>
-          <SolutionShown/>
-          <SolutionShown/>
-        </div>
-        </>
-      ))}
+      <div style={{ display: "flex", flexDirection: "row", justifyContent:"flex-start", flexWrap:"wrap" }}>
+        {words.map((word) => (
+          <div
+            style={{ borderLeft: "solid 3px #5492b3", marginBottom: "38px", minWidth:270}}
+          >
+            <p
+              style={{
+                color: "#44cdff",
+                marginBottom: "-15px",
+                marginTop: "0px",
+                marginLeft: "1em",
+              }}
+            >
+              {word.translation}
+            </p>
+            <p style={{ marginLeft: "1em", marginBottom: "-5px" }}>
+              <b>{word.word}</b>
+            </p>
+            <div key={uuid()}>
+              {word.exerciseAttempts.map((exercise) => (
+                <div
+                  key={uuid()}
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    marginLeft: "1em",
+                    fontSize: "small",
+                    marginBottom: "-25px",
+                  }}
+                >
+                  <p style={{ color: "#808080" }}>{exercise.date}</p>
+                  <p style={{ color: "#808080", marginLeft: ".5em" }}>
+                    {exercise.type}
+                  </p>
+                  <AttemptIcons attemptString={exercise.attempts} />
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
