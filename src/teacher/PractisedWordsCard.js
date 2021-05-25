8acdc6d29ddd0b920dfb26aac8805247bd543dbc
Sara Tolstrup Husum <sarathusum@gmail.com>
2021-05-25 15:32:32 +0200

    typos

--- src/teacher/PractisedWordsCard.js
+++ src/teacher/PractisedWordsCard.js
@@ -5,7 +5,7 @@ const PractisedWordsCard = ({ time, wordCount, correctness, isOpen }) => {
     //TODO everything here should be localised! STRINGS 
   return (
     <ExerciseDataItemCard isOpen={isOpen} headline="Practised words">
-      <ExerciseDataCircle circleText="time spent on exercise" circleData={time}/>
+      <ExerciseDataCircle circleText="time spent on exercises" circleData={time}/>
       <ExerciseDataCircle circleText="number of words" circleData={wordCount}/>
       <ExerciseDataCircle circleText="solved on 1st attempt" circleData={correctness}/>
     </ExerciseDataItemCard>
