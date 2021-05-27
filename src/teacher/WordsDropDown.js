e471a945274f9d13c59de859137242e467a059d5
Sara Tolstrup Husum <sarathusum@gmail.com>
2021-05-27 18:15:06 +0200

    Tooltip implemeted

--- src/teacher/WordsDropDown.js
+++ src/teacher/WordsDropDown.js
@@ -1,6 +1,9 @@
 import PractisedWordsList from "./PractisedWordsList";
 import LearnedWordsList from "./LearnedWordsList";
 import NonStudiedWordsList from "./NonStudiedWordsList";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import { StyledTooltip } from "./StyledTooltip.sc";
+import { IconExplanation } from "./AttemptIcons";
 
 const WordsDropDown = ({ words, card }) => {
   const setHeadline = () => {
@@ -21,10 +24,21 @@ const WordsDropDown = ({ words, card }) => {
         width: "95%",
         boxShadow:
           "0 4px 8px 0 rgba(0, 0, 0, 0.12), 0 2px 4px 0 rgba(0, 0, 0, 0.08)",
-        borderRadius: "10px",
+        borderRadius: "15px",
       }}
     >
-      <h3 style={{ color: "#5492b3" }}>{setHeadline()}</h3>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <h3 style={{ color: "#5492b3" }}>{setHeadline()}</h3>
+        <StyledTooltip label={IconExplanation}>
+          <InfoOutlinedIcon style={{ color: "#5492b3", fontSize: "45px" }} />
+        </StyledTooltip>
+      </div>
       <div
         style={{
           display: "flex",
@@ -33,9 +47,9 @@ const WordsDropDown = ({ words, card }) => {
           flexWrap: "wrap",
         }}
       >
-          {card==="practised" && <PractisedWordsList words={words}/>}
-          {card==="learned" && <LearnedWordsList words={words}/> }
-          {card==="non-studied" && <NonStudiedWordsList words={words}/> }
+        {card === "practised" && <PractisedWordsList words={words} />}
+        {card === "learned" && <LearnedWordsList words={words} />}
+        {card === "non-studied" && <NonStudiedWordsList words={words} />}
       </div>
     </div>
   );
