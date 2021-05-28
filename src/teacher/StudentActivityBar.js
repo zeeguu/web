11fcdc4820b5be260fe951980f9f19d62abf4b9d
eleonora-k <eleonora.kurilchik@gmail.com>
2021-05-28 13:28:09 +0200

    fixed the progress bar issue

--- src/teacher/StudentActivityBar.js
+++ src/teacher/StudentActivityBar.js
@@ -1,28 +1,31 @@
 import React, { useEffect, useState } from "react";
 import * as s from "./StudentActivityBar.sc";
 
-const StudentActivityBar = ({ student, readingTime, exerciseTime }) => {
+const StudentActivityBar = ({ student }) => {
   const [readingTimeString, setReadingTimeString] = useState("");
   const [exerciseTimeString, setExerciseTimeString] = useState("");
 
   useEffect(() => {
-    const readingHours = Math.floor(readingTime / 3600);
-    const readingMinutes = Math.ceil((readingTime / 60) % 60);
+    const readingHours = Math.floor(student.reading_time / 3600);
+    const readingMinutes = Math.ceil((student.reading_time / 60) % 60);
     readingHours < 1
       ? setReadingTimeString(readingMinutes + "m")
       : setReadingTimeString(readingHours + "h " + readingMinutes + "m");
 
-    const exerciseHours = Math.floor(exerciseTime / 3600);
-    const exerciseMinutes = Math.ceil((exerciseTime / 60) % 60);
+    const exerciseHours = Math.floor(student.exercises_done / 3600);
+    const exerciseMinutes = Math.ceil((student.exercises_done / 60) % 60);
     exerciseHours < 1
       ? setExerciseTimeString(exerciseMinutes + "m")
       : setExerciseTimeString(exerciseHours + "h " + exerciseMinutes + "m");
     //eslint-disable-next-line
   }, [student]);
 
+  console.log(student);
+
+
   const setReadingCorners = () => {
     let readingCorners = "25px 0 0 25px";
-    if (exerciseTime === 0) {
+    if (student.exercises_done === 0) {
       readingCorners = "25px";
     }
     return readingCorners;
@@ -30,13 +33,13 @@ const StudentActivityBar = ({ student, readingTime, exerciseTime }) => {
 
   const setExerciseCorners = () => {
     let exerciseCorners = "0 25px 25px 0";
-    if (readingTime === 0) {
+    if (student.reading_time === 0) {
       exerciseCorners = "25px";
     }
     return exerciseCorners;
   };
 
-  const computedWidth = exerciseTime === 0 ? "0%" : 100 - student.reading_percentage + "%"
+  const computedWidth = student.exercises_done === 0 ? "0%" : 100 - student.reading_percentage + "%"
   //making sure we are not returning an activity bar if time is less than 3 minutes
   if (student.total_time < 240) { return null }
   return (
@@ -58,7 +61,7 @@ const StudentActivityBar = ({ student, readingTime, exerciseTime }) => {
           }}
         >
           {/* Not showing the reading time if it is less than 3 min */}
-          {readingTime > 120 ? readingTimeString : ""}
+          {student.reading_time > 120 ? readingTimeString : ""}
         </div>
         <div
           className="activity-bar"
@@ -68,7 +71,7 @@ const StudentActivityBar = ({ student, readingTime, exerciseTime }) => {
           }}
         >
           {/* Not showing the exercise time if it is less than 3 min */}
-          {exerciseTime > 120 ? exerciseTimeString : ""}
+          {student.exercises_done > 120 ? exerciseTimeString : ""}
         </div>
       </div>
     </s.StudentActivityBar>
