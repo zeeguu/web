990692d1b0793ef6c98ad1195015a472732d5133
Sara Tolstrup Husum <sarathusum@gmail.com>
2021-06-01 14:48:22 +0200

    getExerciseHistory implemented but returns {} ???

--- src/teacher/StudentExercisesInsights.js
+++ src/teacher/StudentExercisesInsights.js
@@ -6,6 +6,7 @@ import PractisedWordsCard from "./PractisedWordsCard";
 import WordCountCard from "./WordCountCard";
 import { StyledButton } from "./TeacherButtons.sc";
 import WordsDropDown from "./WordsDropDown";
+import { DUMMYWORDS } from "./DUMMIES_TO_DELETE";
 
 export default function StudentExercisesInsights({ api }) {
   const [forceUpdate, setForceUpdate] = useState(0);
@@ -15,118 +16,40 @@ export default function StudentExercisesInsights({ api }) {
   const [studentInfo, setStudentInfo] = useState({});
   const [doneExercises, setDoneExercises] = useState(null);
   const [isOpen, setIsOpen] = useState("");
-  const practisedWords = [
-    {
-      word: "learned1",
-      translation: "lært1",
-      isStudied: "true",
-      isLearned: "true",
-      exerciseAttempts: [
-        { date: "Jan 1. 2020", type: "recognise", attempts: "c", feedback: "" },
-        { date: "Jan 2. 2020", type: "multiple choice", attempts: "whc", feedback: "" },
-        { date: "Jan 3. 2020", type: "multiple choice", attempts: "c", feedback: "" },
-        { date: "Jan 4. 2020", type: "multiple choice", attempts: "c", feedback: "" },
-        { date: "Jan 5. 2020", type: "multiple choice", attempts: "c", feedback: "" },
-      ],
-      exclusionReason: ""
-    },
-    {
-      word: "practised1",
-      translation: "øvet1",
-      isStudied: "true",
-      isLearned: "false",
-      exerciseAttempts: [
-        { date: "Feb 1. 2020", type: "recognise", attempts: "wwhc", feedback: "" },
-        { date: "Feb 2. 2020", type: "multiple choice", attempts: "s", feedback: "" },
-      ],
-      exclusionReason: ""
-    },
-    {
-      word: "practised2",
-      translation: "øvet2",
-      isStudied: "true",
-      isLearned: "false",
-      exerciseAttempts: [
-        { date: "Mar 1. 2020", type: "recognise", attempts: "wc", feedback: "" },
-        { date: "Mar 2. 2020", type: "multiple choice", attempts: "c", feedback: "Too easy" },
-      ],
-      exclusionReason: ""
-    },
-    {
-      word: "practised3",
-      translation: "øvet3",
-      isStudied: "true",
-      isLearned: "false",
-      exerciseAttempts: [
-        { date: "Apr 1. 2020", type: "recognise", attempts: "hwhc", feedback: "" },
-        { date: "Apr 2. 2020", type: "multiple choice", attempts: "c", feedback: "" },
-      ],
-      exclusionReason: ""
-    },
-    {
-      word: "non-studied1",
-      translation: "ikke-øvet1",
-      isStudied: "false",
-      isLearned: "false",
-      exerciseAttempts: [],
-      exclusionReason: "Excluded by algorithm"
-    },
-    {
-      word: "non-studied2",
-      translation: "ikke-øvet2",
-      isStudied: "false",
-      isLearned: "false",
-      exerciseAttempts: [],
-      exclusionReason: "Excluded by algorithm"
-    },
-    {
-      word: "non-studied3",
-      translation: "ikke-øvet3",
-      isStudied: "false",
-      isLearned: "false",
-      exerciseAttempts: [],
-      exclusionReason: "Scheduled - not yet practiced"
-    },
-    {
-      word: "practised4",
-      translation: "øvet4",
-      isStudied: "true",
-      isLearned: "false",
-      exerciseAttempts: [
-        { date: "Apr 1. 2020", type: "recognise", attempts: "hwhs", feedback: "" },
-        { date: "Apr 2. 2020", type: "multiple choice", attempts: "hh", feedback: "Too hard" },
-      ],
-      exclusionReason: ""
-    },
-    {
-      word: "practised5",
-      translation: "øvet5",
-      isStudied: "true",
-      isLearned: "false",
-      exerciseAttempts: [
-        { date: "May 1. 2020", type: "recognise", attempts: "hhhc" },
-        { date: "May 2. 2020", type: "multiple choice", attempts: "c" },
-        { date: "Jan 3. 2020", type: "multiple choice", attempts: "wwws" },
-        { date: "Jan 3. 2020", type: "multiple choice", attempts: "hc" },
-      ],
-      exclusionReason: ""
-    },
-  ];
+
+  const practisedWords = DUMMYWORDS
 
   useEffect(() => {
     api.loadUserInfo(studentID, selectedTimePeriod, (userInfo) => {
       setStudentInfo(userInfo);
     });
-    setDoneExercises(["ex1", "ex2"]);
+    
     // eslint-disable-next-line
   }, [forceUpdate]);
+
+  useEffect(() => {
+    api.getExerciseHistory(
+      studentID,
+      selectedTimePeriod,
+      cohortID,
+      (res) => {
+        console.log("Success");
+        console.log(res);
+        setDoneExercises(DUMMYWORDS);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+    // eslint-disable-next-line
+  }, [selectedTimePeriod]);
 
   const customText =
     doneExercises &&
     studentInfo.name +
-    " has completed " +
-    doneExercises.length +
-    " exercises in the last ";
+      " has completed " +
+      doneExercises.length +
+      " exercises in the last ";
 
   const handleCardClick = (cardName) => {
     if (isOpen === cardName) {
