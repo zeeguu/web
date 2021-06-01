2c27fc9ff721ec77c22e6dcd970c228c82e387b5
Maja Petersen <maja_albin@hotmail.com>
2021-06-01 18:31:09 +0200

    add button for students to join a class

--- src/articles/ClassroomArticles.js
+++ src/articles/ClassroomArticles.js
@@ -1,18 +1,30 @@
-import { useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import LoadingAnimation from "../components/LoadingAnimation";
 import { setTitle } from "../assorted/setTitle";
-import strings from "../i18n/definitions" 
+import strings from "../i18n/definitions";
 
 import ArticlePreview from "./ArticlePreview";
 
 import SortingButtons from "./SortingButtons";
 
+import { StyledButton, TopButtonWrapper } from "../teacher/TeacherButtons.sc";
+import { OrangeRoundButton } from "../components/allButtons.sc";
+
 import * as s from "../components/TopMessage.sc";
+import * as sc from "../components/ColumnWidth.sc";
+import { Link } from "react-router-dom";
 
 export default function ClassroomArticles({ api }) {
   const [articleList, setArticleList] = useState(null);
+  const [studentJoinedCohort, setStudentJoinedCohort] = useState(null);
 
   let originalList = articleList;
+
+  useEffect(() => {
+    api.getStudent((student) =>
+      setStudentJoinedCohort(student.cohort_id !== null)
+    );
+  }, []);
 
   if (articleList == null) {
     api.getCohortArticles((articles) => {
@@ -25,7 +37,27 @@ export default function ClassroomArticles({ api }) {
   }
 
   if (articleList.length === 0) {
-    return <s.TopMessage>{strings.noArticlesInClassroom}</s.TopMessage>;
+    return (
+      <Fragment>
+        {!studentJoinedCohort ? (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              textAlign: "center",
+            }}
+          >
+            <h4> You haven't joined a class yet. </h4>
+            <Link to={`/account_settings`}>
+              <OrangeRoundButton> STRINGS Join class </OrangeRoundButton>
+            </Link>
+          </div>
+        ) : (
+          <s.TopMessage>{strings.noArticlesInClassroom}</s.TopMessage>
+        )}
+      </Fragment>
+    );
   }
 
   return (
