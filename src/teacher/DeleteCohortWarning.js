fc9296504b1f99730cf2179deec8f7b9ea2c84b8
Sara Tolstrup Husum <sarathusum@gmail.com>
2021-06-02 20:10:58 +0200

    Two bugs fixed

--- src/teacher/DeleteCohortWarning.js
+++ src/teacher/DeleteCohortWarning.js
@@ -2,28 +2,49 @@ import React from "react";
 import { StyledButton, PopupButtonWrapper } from "./TeacherButtons.sc";
 import { StyledDialog } from "./StyledDialog.sc";
 import CohortItemCard from "./CohortItemCard";
+import { Error } from "./Error";
 
-const DeleteCohortWarning = ({ api, cohort, setShowWarning, deleteCohort }) => {
+const DeleteCohortWarning = ({
+  api,
+  cohort,
+  setShowWarning,
+  deleteCohort,
+  isDeleteError,
+  setIsDeleteError,
+  setIsLoading,
+}) => {
+
+  const handleCancel = () => {
+    setIsDeleteError(false)
+    setShowWarning(false)
+  }
 
   return (
     <StyledDialog
       aria-label="Delete a class warning"
-      onDismiss={() => setShowWarning(false)}
+      onDismiss={handleCancel}
       max_width="530px"
     >
       <div className="centered">
-      <h1>Danger Zone! STRINGS</h1>
-      <p>
-        Are you sure you want to delete this class? This cannot be undone.
-        STRINGS
-      </p>
+        <h1>Danger Zone! STRINGS</h1>
+        <p>
+          Are you sure you want to remove this class from your list? <br/>
+          This cannot be undone.STRINGS
+        </p>
       </div>
-      <CohortItemCard api={api} cohort={cohort} isWarning={true} />
+      <CohortItemCard api={api} cohort={cohort} isWarning={true} />{" "}
+      {isDeleteError && (
+        <Error
+          message={
+            "Something went wrong. If you still share texts with this class, you cannot remove it from your list. Please, check that in 'My texts' and try again."}
+          setLoading={setIsLoading}
+        />
+      )}
       <PopupButtonWrapper>
-        <StyledButton primary onClick={() => setShowWarning(false)}>
+        <StyledButton primary onClick={handleCancel}>
           Cancel STRINGS
         </StyledButton>
-        <StyledButton secondary onClick={()=>deleteCohort(cohort.id)}>
+        <StyledButton secondary onClick={() => deleteCohort(cohort.id)}>
           Delete STRINGS
         </StyledButton>
       </PopupButtonWrapper>
