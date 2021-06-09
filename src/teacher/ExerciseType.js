d9cae12d23830d1762e12a0a38fab1f3b59c2d2b
Sara Tolstrup Husum <sarathusum@gmail.com>
2021-06-09 12:25:07 +0200

    Learned words works with dummy data

--- src/teacher/ExerciseType.js
+++ src/teacher/ExerciseType.js
@@ -4,6 +4,8 @@ const ExerciseType = ({source}) =>{
     switch (source) {
         case "MULTIPLE_CHOICE":
           return <p style={{ color: "#808080", margin: "1em 0 0 .5em" }}>multiple choice</p>
+        case "LEARNED":
+          return <p style={{ color: "#808080", margin: "1em .5em 0 .5em" }}>Learned on:</p>
         default:
           return <p style={{ color: "#808080", margin: "1em 0 0 .5em" }}>recognise</p>;
       }
