93d4259ca910474e6f5efbee2126dc8b89975908
Sara Tolstrup Husum <sarathusum@gmail.com>
2021-06-10 13:46:57 +0200

    removed filtering from NonStudiedWordsList

--- src/teacher/NonStudiedWordsList.js
+++ src/teacher/NonStudiedWordsList.js
@@ -1,29 +1,10 @@
-import { Fragment, useEffect, useState } from "react";
+import { Fragment } from "react";
 import { v4 as uuid } from "uuid";
 import NonStudiedWordCard from "./NonStudiedWordCard";
 
 const NonStudiedWordsList = ({ words }) => {
-  const [nonStudiedWords, setNonStudiedWords] = useState([]);
-  const wordsNotYetScheduled = nonStudiedWords.filter((word) => word.fit_for_study === 1);
-  const wordsExcludedByAlgorithm = nonStudiedWords.filter((word) => word.fit_for_study === null);
-
-  useEffect(() => {
-    let tempList = [];
-    words.reverse().forEach((word) => {
-      if (tempList.length === 0) {
-        tempList.push(word);
-      } else {
-        const lastIndex = tempList.length - 1;
-        const lastWord = tempList[lastIndex].word.toLowerCase();
-        if (!lastWord.includes(word.word.toLowerCase())) {
-          tempList.push(word);
-        }
-      }
-    });
-
-    setNonStudiedWords(tempList.reverse());
-    //eslint-disable-next-line
-  }, []);
+  const wordsNotYetScheduled = words.filter((word) => word.fit_for_study === 1);
+  const wordsExcludedByAlgorithm = words.filter((word) => word.fit_for_study === null);
 
   return (
     <Fragment>
