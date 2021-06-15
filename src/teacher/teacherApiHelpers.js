5e54304efa5c1e669d70d1dd34f482d7322cd183
Sara Tolstrup Husum <sarathusum@gmail.com>
2021-06-15 16:24:18 +0200

    better conversion on its way

--- src/teacher/teacherApiHelpers.js
+++ src/teacher/teacherApiHelpers.js
@@ -1,26 +1,29 @@
+//This function is used to set each students data relative to the other students' data for the studentActivityBar to be rendered correctly.
 export function transformStudents(students) {
-  let maxActivity = 0
+  let maxActivity = 0;
 
-  let transformedStudents = students.map(student => {
-    const { reading_time, exercises_done } = student
-    const reading_percentage = getReadingPercentage(reading_time, exercises_done)
-    const total_time = reading_time + exercises_done
+  let transformedStudents = students.map((student) => {
+    const { reading_time, exercises_done } = student;
+    const reading_percentage = getReadingPercentage(
+      reading_time,
+      exercises_done
+    );
+    const total_time = reading_time + exercises_done;
 
-    maxActivity = maxActivity > total_time ? maxActivity : total_time
+    maxActivity = maxActivity > total_time ? maxActivity : total_time;
     return {
       ...student,
       reading_percentage,
-      total_time
-    }
-  })
+      total_time,
+    };
+  });
   if (maxActivity > 240) {
-    transformedStudents = transformedStudents.map(student =>
+    transformedStudents = transformedStudents.map((student) =>
       addTotalAndNormalizedTime(student, maxActivity)
-    )
+    );
   }
-  return transformedStudents
+  return transformedStudents;
 }
-
 
 /**
  * Computes the proportion between two numbers
@@ -30,16 +33,16 @@ export function transformStudents(students) {
 export function getReadingPercentage(a, b) {
   //student has done both reading and exercises -> return % of reading time
   if (!(a === 0 && b === 0)) {
-    return (100 * a) / (b + a)
+    return (100 * a) / (b + a);
     //student has done no reading and no exercises:
   } else if (a === 0 && b === 0) {
-    return 0
+    return 0;
     //student has done no reading:
   } else if (a === 0) {
-    return 0
+    return 0;
     //student has done reading only (no exercises)
   } else {
-    return 100
+    return 100;
   }
 }
 
@@ -52,9 +55,9 @@ export function addTotalAndNormalizedTime(student, maxActivity) {
   if (maxActivity > 0) {
     return {
       ...student,
-      normalized_activity_proportion: (student.total_time / maxActivity) * 100
-    }
+      normalized_activity_proportion: (student.total_time / maxActivity) * 100,
+    };
   } else {
-    return student
+    return student;
   }
 }
