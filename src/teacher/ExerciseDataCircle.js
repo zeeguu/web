e3da62c7674295626437ab986233d33f376c753c
Sara Tolstrup Husum <sarathusum@gmail.com>
2021-06-17 17:54:53 +0200

    more css

--- src/teacher/ExerciseDataCircle.js
+++ src/teacher/ExerciseDataCircle.js
@@ -1,13 +1,15 @@
-import { StudentActivityDataCircle } from "./StudentActivityDataCircle.sc"
+import { StudentActivityDataCircle } from "./StudentActivityDataCircle.sc";
 
-const ExerciseDataCircle = ({circleText, circleData})=>{
-    return(
-        <div className="circle-text-wrapper" style={{margin:".2em"}}>
-        <p style={{ width:100 }}>
-          {circleText}
-        </p>
-        <StudentActivityDataCircle style={{marginLeft:"auto", marginRight:"auto"}}>{circleData}</StudentActivityDataCircle>
-      </div>
-    )
-}
+const ExerciseDataCircle = ({ circleText, circleData }) => {
+  return (
+    <div className="circle-text-wrapper">
+      <p style={{ width: 90 }}>{circleText}</p>
+      <StudentActivityDataCircle
+        style={{ marginLeft: "auto", marginRight: "auto" }}
+      >
+        {circleData}
+      </StudentActivityDataCircle>
+    </div>
+  );
+};
 export default ExerciseDataCircle;
