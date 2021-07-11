0be8874d8890c1611585a6c014ecf66fedf3450e
Sara Tolstrup Husum <sarathusum@gmail.com>
2021-07-11 14:00:32 +0200

    cleaning up warnings

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -32,6 +32,7 @@ export default function Settings({ api, setUser }) {
   useEffect(() => {
     const language = LocalStorage.getUiLanguage();
     setUiLanguage(language);
+    // eslint-disable-next-line 
   }, []);
 
   function onSysChange(lang) {
