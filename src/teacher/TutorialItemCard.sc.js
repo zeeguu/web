207091cd8d46cd3bc08b85d6db3bb97d12079640
Maja Petersen <maja_albin@hotmail.com>
2021-07-28 23:40:56 +0200

    Fiddles around with implementing tutorials

--- src/teacher/TutorialItemCard.sc.js
+++ src/teacher/TutorialItemCard.sc.js
@@ -1,27 +1,46 @@
 import styled from "styled-components";
 import { darkGrey, darkBlue, lightBlue } from "../components/colors";
 
-
 export const StyledTutorialItemCard = styled.div`
-font-size:22px;
+  font-size: 22px;
 
-.vertical-line-border-box{
+  /* .vertical-line-border-box {
     margin-top: 2em;
-    padding: .05em .5em;
-    border-left: solid 3px ${darkBlue}
-}
+    padding: 0.05em 0.5em;
+    border-left: solid 3px ${darkBlue};
+  } */
 
-.placeholder{
-    margin-top: .5em;
+  /* .placeholder {
+    margin-top: 0.5em;
     margin-left: 2em;
     border-radius: 20px;
-    color:white;
-    text-align:center;
-    width:644px;
-    height:362px;
+    color: white;
+    text-align: center;
+    width: 644px;
+    height: 362px;
     background-color: ${darkGrey};
     border: solid 5px ${lightBlue};
+*/
+  /* p {
+    margin-top: 25%;
+  } */
 
-    p {margin-top: 25%}
-}
+  .video-responsive {
+    overflow: hidden;
+    padding-bottom: 56.25%;
+    position: relative;
+    height: 0;
+  }
+
+  .video-responsive iframe {
+    left: 0;
+    top: 0;
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    //  margin-top: 1em;
+    padding-bottom: 4em;
+    padding: 0.05em 0.5em;
+    border-left: solid 3px ${darkBlue};
+  }
 `;
