6dc342050ebc4faf99f92d53bbc6db69dd9e38ab
Maja Petersen <maja_albin@hotmail.com>
2021-07-30 13:35:23 +0200

    tutorials implemented but from wrong youtube channel

--- src/teacher/TutorialItemCard.sc.js
+++ src/teacher/TutorialItemCard.sc.js
@@ -22,6 +22,11 @@ export const StyledTutorialItemCard = styled.div`
     border: solid 5px ${lightBlue};
 */
 
+  p {
+    font-size: 1.4em;
+    font-weight: 600; //700 is normal bold
+  }
+
   .video-responsive {
     overflow: hidden;
     padding-bottom: 56.25%;
@@ -35,8 +40,12 @@ export const StyledTutorialItemCard = styled.div`
     height: 100%;
     width: 100%;
     position: absolute;
-    margin-top: 1em;
+    margin-top: 0.5em;
     padding: 0.05em 0.5em;
     border-left: solid 3px ${darkBlue};
+  }
+
+  .video-responsive {
+    margin-bottom: 3em;
   }
 `;
