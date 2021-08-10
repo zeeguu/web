c920eb59f98e980b2ff18dc97619ec6b37637b62
Sara Tolstrup Husum <sarathusum@gmail.com>
2021-08-10 15:48:15 +0200

    wording improved, outcommented code removed

--- src/teacher/TutorialItemCard.sc.js
+++ src/teacher/TutorialItemCard.sc.js
@@ -1,27 +1,7 @@
 import styled from "styled-components";
-import { darkGrey, darkBlue, lightBlue } from "../components/colors";
+import { darkBlue } from "../components/colors";
 
 export const StyledTutorialItemCard = styled.div`
-  /* font-size: 20px; */
-
-  /* .vertical-line-border-box {
-    margin-top: 2em;
-    padding: 0.05em 0.5em;
-    border-left: solid 3px ${darkBlue};
-  } */
-
-  /* .placeholder {
-    margin-top: 0.5em;
-    margin-left: 2em;
-    border-radius: 20px;
-    color: white;
-    text-align: center;
-    width: 644px;
-    height: 362px;
-    background-color: ${darkGrey};
-    border: solid 5px ${lightBlue};
-*/
-
   p {
     font-size: 1.4em;
     font-weight: 600; //700 is normal bold
