02834f693e778f0461c9b10c0573bb323359e6b0
Maja Petersen <maja_albin@hotmail.com>
2021-08-13 19:56:25 +0200

    Fixing attempt icons

---
+++ src/teacher/AttemptIcons.sc.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const StyledAttemptIcons = styled.div`
+  .correct-attempt-icon {
+    color: green;
+    margin: 12px -3px 0 -2px;
+    font-size: 18px;
+  }
+
+  .wrong-attempt-icon {
+    color: red;
+    margin: 12px -3px 0 -2px;
+    font-size: 18px;
+  }
+
+  .solution-shown-icon {
+    margin: 13px -5px 0 -3.5px;
+    font-size: 15px;
+  }
+
+  .used-attempt-icon {
+    display: flex;
+    flex-direction: row;
+    margin-left: 0.5em;
+    margin-right: 2em;
+  }
+
+  .hint-used-icon {
+    color: orange;
+    font-weight: 600;
+    margin: 12px 0px 0 1.5px;
+    font-size: 14px;
+  }
+
+  .student-feedback {
+    font-weight: 500;
+    margin: 13px 0px 0 7px;
+    font-size: small;
+  }
+`;
