849872f5f2e5f9c209548a6596732d73be2ce506
Maja Petersen <maja_albin@hotmail.com>
2021-08-22 15:42:10 +0200

    Styling of students reading activities fixed

--- src/teacher/StudentTranslations.js
+++ src/teacher/StudentTranslations.js
@@ -3,6 +3,7 @@ import { StyledTooltip } from "./StyledTooltip.sc";
 import { StarExplanation } from "./AttemptIcons";
 import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
 import strings from "../i18n/definitions";
+import * as s from "./StudentTranslations.sc";
 
 const StudentTranslations = ({ article }) => {
   const [translations, setTranslations] = useState(null);
@@ -30,65 +31,50 @@ const StudentTranslations = ({ article }) => {
     const suffix = sentence.substring(wordStart + wordLength);
 
     return (
-      <div
-        style={{
-          borderLeft: "solid 3px #4492b3",
-          margin: "0 20px 20px 30px",
-          lineHeight: 2,
-          paddingLeft: "15px",
-        }}
-      >
-        {prefix}
-        <span
-          style={{
-            display: "inline",
-            border: "solid 2px #54cdff",
-            margin: "15px 0px 0px 0px",
-            padding: "5px",
-            borderRadius: "25px",
-          }}
-        >
-          <b>{word}</b>
-        </span>
-        {isPracticed === 1 && (
-          <span style={{ color: "#54cdff", fontSize: "25px" }}>
-            <b>*</b>
+      <s.StyledStudentTranslations>
+        <div className="sentences-containing-translated-word">
+          {prefix}
+          <span className="translated-exercised-word">
+            <b>{word}</b>
           </span>
-        )}
-        {suffix}
-      </div>
+          {isPracticed === 1 && (
+            <span className="star-indicating-translated-word-being-exercised">
+              <b>*</b>
+            </span>
+          )}
+          {suffix}
+        </div>{" "}
+      </s.StyledStudentTranslations>
     );
   };
 
   return (
-    <Fragment>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-        }}
-      >
-        <h4 className="panel-headline">{strings.translatedWordsInSentence}</h4>
-        <StyledTooltip label={StarExplanation()}>
-          <InfoOutlinedIcon style={{ color: "#5492b3", fontSize: "45px" }} />
-        </StyledTooltip>
-      </div>
-      {translations &&
-        translations.map((translation) => (
-          <DivideSentence
-            key={translation.id}
-            sentence={translation.context}
-            word={translation.word}
-            isPracticed={translation.practiced}
-          />
-        ))}
+    <s.StyledStudentTranslations>
+      <Fragment>
+        <div className="dropdown-headline-container">
+          <h4 className="panel-headline">
+            {strings.translatedWordsInSentence}
+          </h4>
+          <StyledTooltip label={StarExplanation()}>
+            <InfoOutlinedIcon className="information-icon" />
+          </StyledTooltip>
+        </div>
+        {translations &&
+          translations.map((translation) => (
+            <DivideSentence
+              key={translation.id}
+              sentence={translation.context}
+              word={translation.word}
+              isPracticed={translation.practiced}
+            />
+          ))}
 
-      {translations === null ||
-        (translations.length === 0 && (
-          <p className="panel-no-words">{strings.translatedWordInText}</p>
-        ))}
-    </Fragment>
+        {translations === null ||
+          (translations.length === 0 && (
+            <p className="panel-no-words">{strings.translatedWordInText}</p>
+          ))}
+      </Fragment>
+    </s.StyledStudentTranslations>
   );
 };
 export default StudentTranslations;
