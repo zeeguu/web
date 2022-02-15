ec415efb58c4c26ebfe79aac54fcc8908e3aa8d1
emte <emte@itu.dk>
2022-02-15 16:20:52 +0100

    Implemented login (and saving in localStorage)

--- src/popup/Login.js
+++ src/popup/Login.js
@@ -1,17 +1,58 @@
-import logo from "../images/zeeguu128.png"
+/*global chrome*/
 
-export default function Login() {
-    return (
-        <form>
-            <div class="imgcontainer">
-            <img src={logo} alt="Zeeguu logo" class="logo"/>
-            </div>
-            <div class="container">
-              <label for="uname"><b>Username</b></label>
-              <input type="text" placeholder="Enter Username" name="uname" required/>
-              <label for="psw"><b>Password</b></label>
-              <input type="password" placeholder="Enter Password" name="psw" required/>
-              <button type="submit">Login</button>
-            </div>
-      </form>
-    )}
+import { useState } from "react";
+export default function Login({ setLoggedIn }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSignIn(e) {
+    e.preventDefault();
+    chrome.storage.local.set({ loggedIn: true });
+    setLoggedIn(true);
+    //api.signIn(email, password, setErrorMessage, (sessionId) => {
+    //  api.getUserDetails((userInfo) => {
+    //    signInAndRedirect(userInfo, history);
+    // });
+    //});
+  }
+
+  return (
+    <form action="" method="post">
+      <div class="container">
+        <label for="email">
+          <b>Username</b>
+        </label>
+        <input
+          type="email"
+          placeholder="Enter email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <label for="psw">
+          <b>Password</b>
+        </label>
+        <input
+          type="password"
+          placeholder="Enter Password"
+          name="psw"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button
+          type="submit"
+          onClick={handleSignIn}
+          name="login"
+          value="Login"
+          className="loginButton"
+        >
+          Login
+        </button>
+      </div>
+    </form>
+  );
+}
