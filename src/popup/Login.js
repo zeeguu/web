48b84d9d3fa92b043405cb24d1c4d2fe4caf9840
emte <emte@itu.dk>
2022-02-15 16:45:26 +0100

    Zeeguu sessionID

--- src/popup/Login.js
+++ src/popup/Login.js
@@ -16,6 +16,42 @@ export default function Login({ setLoggedIn }) {
     //});
   }
 
+  /****  This is what Zeeguu does with sessionID  ****/
+
+      //let userDict = {};
+      //// we use the _api to initialize the api state variable
+      //let _api = new Zeeguu_API(process.env.REACT_APP_API_URL);
+      //if (LocalStorage.hasSession()) {
+      //  userDict = {
+      //    session: localStorage["sessionID"],
+      //    ...LocalStorage.userInfo(),
+      //  };
+      //  _api.session = localStorage["sessionID"];
+      //}
+      //useUILanguage();
+      //const [api] = useState(_api);
+      //const [user, setUser] = useState(userDict);     
+
+
+      // function handleSuccessfulSignIn(userInfo, history) {
+      //   setUser({
+      //     session: api.session,
+      //     name: userInfo.name,
+      //     learned_language: userInfo.learned_language,
+      //     native_language: userInfo.native_language,
+      //     is_teacher: userInfo.is_teacher,
+      //   });
+      //   LocalStorage.setSession(api.session);
+      //   LocalStorage.setUserInfo(userInfo);
+      //   // TODO: this is required by the teacher dashboard
+      //   // could be cool to remove it from there and make that
+      //   // one also use the localStorage
+      //   document.cookie = `sessionID=${api.session};`;
+      //   userInfo.is_teacher
+      //     ? history.push("/teacher/classes")
+      //     : history.push("/articles");
+      // }
+
   return (
     <form action="" method="post">
       <div class="container">
