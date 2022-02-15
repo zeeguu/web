83e042568967e7500a097ea47956001bb7bd4841
Frida Helene Beck-Larsen <frib@itu.dk>
2022-02-15 16:39:27 +0100

    prettier

--- src/popup/Popup.js
+++ src/popup/Popup.js
@@ -1,47 +1,54 @@
 /*global chrome*/
-import Login from "./Login"
-import {getCurrentTab, reading, setCurrentURL, getSourceAsDOM} from "./functions";
-import {isProbablyReaderable} from "@mozilla/readability";
+import Login from "./Login";
+import {
+  getCurrentTab,
+  reading,
+  setCurrentURL,
+  getSourceAsDOM,
+} from "./functions";
+import { isProbablyReaderable } from "@mozilla/readability";
 import useState from "react";
 
-  //for isProbablyReadable options object
-  const minLength = 120;
-  const minScore = 20;
+//for isProbablyReadable options object
+const minLength = 120;
+const minScore = 20;
 
-export default function Popup() {  
-  async function openModal(){
+export default function Popup() {
+  async function openModal() {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    
-  //readability check
+
+    //readability check
     const documentFromTab = getSourceAsDOM(tab.url);
-    const isProbablyReadable = isProbablyReaderable(documentFromTab, minLength, minScore);
+    const isProbablyReadable = isProbablyReaderable(
+      documentFromTab,
+      minLength,
+      minScore
+    );
     if (!isProbablyReadable) {
-      return (
-        alert("This page is not readable")
-      )
-    } 
+      return alert("This page is not readable");
+    }
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
-      function: reading(tab.url),
+      //function: reading(tab.url),
       files: ["./main.js"],
-      }); 
+    });
     chrome.scripting.insertCSS({
       target: { tabId: tab.id },
-      files: ["./modal.css"]
-  });
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setCurrentURL(tab.url),
-  });  
-  } 
+      files: ["./modal.css"],
+    });
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: setCurrentURL(tab.url),
+    });
+  }
 
   let currentTab = getCurrentTab();
   chrome.storage.local.set({ tabId: currentTab });
-  
-    return (
-        <>
-        <Login/>
-        <button onClick={openModal}>Read article</button>
-      </>
-    );
-  }
+
+  return (
+    <>
+      <Login />
+      <button onClick={openModal}>Read article</button>
+    </>
+  );
+}
