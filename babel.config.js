b0b626835e3da6f225ecf92f2e70799cf52899cf
emte <emte@itu.dk>
2022-02-16 15:08:04 +0100

    Fixed react hooks error

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,9 @@
 module.exports ={
     presets: [
         ["@babel/preset-env"],
-       ["@babel/preset-react"]
+       ["@babel/preset-react", {"runtime": "automatic"}]
     ],
     plugins: [
         ["@babel/transform-runtime"]
-    ]
+    ],
 }
--- webpack.config.js
+++ webpack.config.js
@@ -11,5 +11,10 @@ module.exports = {
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] }
     ]
-  }
+  },
+  resolve: {
+      alias: {
+        react: path.resolve('./node_modules/react'),
+      },
+},
 }; 
