c5330163079e95338c3a9a1975af5cdfaedc6563
emte <emte@itu.dk>
2022-02-28 15:03:13 +0100

    Fixed "func and files error"

--- src/popup/Popup.js
+++ src/popup/Popup.js
@@ -29,16 +29,12 @@ export default function Popup({loggedIn, setLoggedIn}) {
     }
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
-      //function: reading(tab.url),
       files: ["./main.js"],
+      func: setCurrentURL(tab.url),
     });
     chrome.scripting.insertCSS({
       target: { tabId: tab.id },
       files: ["./modal.css"],
-    });
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: setCurrentURL(tab.url),
     });
   }
 
