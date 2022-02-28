5f6fe64e9e042780842ea3b4a7dd4a2695fb762e
emte <emte@itu.dk>
2022-02-28 15:07:21 +0100

    Close popup, when clicking "read article"

--- src/popup/Popup.js
+++ src/popup/Popup.js
@@ -36,6 +36,7 @@ export default function Popup({loggedIn, setLoggedIn}) {
       target: { tabId: tab.id },
       files: ["./modal.css"],
     });
+    window.close();
   }
 
   let currentTab = getCurrentTab();
