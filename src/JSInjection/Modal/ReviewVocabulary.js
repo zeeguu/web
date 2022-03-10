dae8b0a5ecd104921e1fb31b8abc3bd792e2a7f5
emte <emte@itu.dk>
2022-03-10 17:34:12 +0100

    Applied prettier

--- src/JSInjection/Modal/ReviewVocabulary.js
+++ src/JSInjection/Modal/ReviewVocabulary.js
@@ -1,15 +1,17 @@
-import * as s from "../../zeeguu-react/src/reader/ArticleReader.sc"
-import strings from "../../zeeguu-react/src/i18n/definitions"
+import * as s from "../../zeeguu-react/src/reader/ArticleReader.sc";
+import strings from "../../zeeguu-react/src/i18n/definitions";
 
 export default function ReviewVocabulary() {
   return (
     <s.FeedbackBox>
-    <h2>{strings.reviewVocabulary}</h2>
-    <small>{strings.reviewVocabExplanation}</small>
-    <br />
-    <br />
-    <s.CenteredContent>
-      <a href="http://zeeguu.org" target="_blank" rel="noopener noreferrer">{strings.reviewVocabulary} » (Broken link)</a>
+      <h2>{strings.reviewVocabulary}</h2>
+      <small>{strings.reviewVocabExplanation}</small>
+      <br />
+      <br />
+      <s.CenteredContent>
+        <a href="http://zeeguu.org" target="_blank" rel="noopener noreferrer">
+          {strings.reviewVocabulary} » (Broken link)
+        </a>
       </s.CenteredContent>
     </s.FeedbackBox>
   );
