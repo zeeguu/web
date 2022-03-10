17a66897a66a9ed7f0f052e19dfb5cd035f20854
emte <emte@itu.dk>
2022-03-10 17:14:03 +0100

    Changed toolbar height

--- src/JSInjection/Modal/ToolbarButtons.js
+++ src/JSInjection/Modal/ToolbarButtons.js
@@ -4,7 +4,7 @@ import {toggle} from "../../zeeguu-react/src/reader/ArticleReader"
 
 export default function ToolbarButtons({translating, setTranslating, pronouncing, setPronouncing}) {
   return (
-    <s.Toolbar style={{ display: "flex", "justify-content": "flex-end" }}>
+    <s.Toolbar style={{ display: "flex", "justify-content": "flex-end", height: "75px"}}>
       <button
         className={translating ? "selected" : ""}
         onClick={(e) => toggle(translating, setTranslating)}
