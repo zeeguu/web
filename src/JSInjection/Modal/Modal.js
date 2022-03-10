cf2f6a2590510b5f588f074d83768fb8fb454c3a
emte <emte@itu.dk>
2022-03-10 17:56:42 +0100

    Refactored Modal.js

--- src/JSInjection/Modal/Modal.js
+++ src/JSInjection/Modal/Modal.js
@@ -1,19 +1,19 @@
 /*global chrome*/
 import { useEffect, useState } from "react";
-import { StyledModal, StyledCloseButton, StyledHeading, StyledButton, GlobalStyle } from "./Modal.styles";
-import InteractiveText from "../../zeeguu-react/src/reader/InteractiveText"
-import { TranslatableText } from "../../zeeguu-react/src/reader/TranslatableText"
+import {StyledModal, StyledCloseButton, StyledHeading, StyledButton, GlobalStyle} from "./Modal.styles";
+import InteractiveText from "../../zeeguu-react/src/reader/InteractiveText";
+import { TranslatableText } from "../../zeeguu-react/src/reader/TranslatableText";
 import { getImage } from "../Cleaning/generelClean";
 import { interactiveTextsWithTags } from "./interactivityFunctions";
 import { getNativeLanguage } from "../../popup/functions";
 import ZeeguuLoader from "../ZeeguuLoader";
-import { EXTENSION_SOURCE } from "../constants";
+import { EXTENSION_SOURCE, LIST_CONTENT, TEXT_CONTENT } from "../constants";
 import ToolbarButtons from "./ToolbarButtons";
 import {onScroll, onBlur, onFocus} from "../../zeeguu-react/src/reader/ArticleReader";
 import ReviewVocabulary from "./ReviewVocabulary";
 
 
-export function Modal({ title, content, modalIsOpen, setModalIsOpen, api, url, language, author }) {
+export function Modal({title, content, modalIsOpen, setModalIsOpen, api, url, author}) {
   const [interactiveTextArray, setInteractiveTextArray] = useState();
   const [interactiveTitle, setInteractiveTitle] = useState();
   const [articleImage, setArticleImage] = useState();
@@ -23,7 +23,7 @@ export function Modal({ title, content, modalIsOpen, setModalIsOpen, api, url, l
   const [nativeLang, setNativeLang] = useState();
   const [DBArticleInfo, setDBArticleInfo] = useState();
   const [articleLanguage, setArticleLanguage] = useState();
- 
+
   useEffect(() => {
     if (content !== undefined) {
       let info = {
@@ -57,89 +57,89 @@ export function Modal({ title, content, modalIsOpen, setModalIsOpen, api, url, l
         language: articleLanguage,
         starred: false,
       };
+
       let image = getImage(content);
       setArticleImage(image);
       let arrInteractive = interactiveTextsWithTags(content, articleInfo, api);
       setInteractiveTextArray(arrInteractive);
-  
+
       let itTitle = new InteractiveText(title, articleInfo, api);
       setInteractiveTitle(itTitle);
-      api.logReaderActivity(EXTENSION_SOURCE, api.OPEN_ARTICLE,  articleId);
+      api.logReaderActivity(EXTENSION_SOURCE, api.OPEN_ARTICLE, articleId);
 
-      window.addEventListener("focus", function(){onFocus(EXTENSION_SOURCE, api, articleId)});
-      window.addEventListener("blur", function(){onBlur(EXTENSION_SOURCE, api, articleId)});
+      window.addEventListener("focus", function () {onFocus(EXTENSION_SOURCE, api, articleId);});
+      window.addEventListener("blur", function () {onBlur(EXTENSION_SOURCE, api, articleId);});
 
-      let getModalClass = document.getElementsByClassName("Modal")
-      if ((getModalClass !== undefined) && (getModalClass !== null)){
+      let getModalClass = document.getElementsByClassName("Modal");
+      if (getModalClass !== undefined && getModalClass !== null) {
         setTimeout(() => {
-          if(getModalClass.item(0) != undefined){
-            getModalClass.item(0).addEventListener("scroll", function(){onScroll(EXTENSION_SOURCE, api, articleId)});
+          if (getModalClass.item(0) != undefined) {
+            getModalClass.item(0).addEventListener("scroll", function () {
+              onScroll(EXTENSION_SOURCE, api, articleId);
+            });
           }
         }, 0);
       }
     }
-      
   }, [articleId]);
 
-localStorage.setItem("native_language", nativeLang)
+  localStorage.setItem("native_language", nativeLang);
 
-
-function handleClose() {
-  location.reload();
-  setModalIsOpen(false);
-  api.logReaderActivity(EXTENSION_SOURCE, "ARTICLE CLOSED", articleId);
-  window.removeEventListener("focus", function(){onFocus(EXTENSION_SOURCE, api, articleId)});
-  window.removeEventListener("blur", function(){onBlur(EXTENSION_SOURCE, api, articleId)});
-  document.getElementById("scrollHolder") !== null &&
-  document
-    .getElementById("scrollHolder")
-    .removeEventListener("scroll", function(){onScroll(EXTENSION_SOURCE, api, articleId)});
-};
-
-if (!modalIsOpen) {
-  location.reload();
-}
-
-function handlePostCopy() {
-  api.makePersonalCopy(articleId, (message) => alert(message));
-  api.logReaderActivity(EXTENSION_SOURCE, api.PERSONAL_COPY,  articleId);
-};
-
-function reportProblem(e) {
-  let answer = prompt("What is wrong with the article?");
-  if (answer) {
-    let feedback = "problem_" + answer.replace(/ /g, "_");
-    api.logReaderActivity(EXTENSION_SOURCE, api.EXTENSION_FEEDBACK, articleId, feedback);
+  //Could be moved into another file
+  function handleClose() {
+    location.reload();
+    setModalIsOpen(false);
+    api.logReaderActivity(EXTENSION_SOURCE, "ARTICLE CLOSED", articleId);
+    window.removeEventListener("focus", function () {onFocus(EXTENSION_SOURCE, api, articleId);});
+    window.removeEventListener("blur", function () {onBlur(EXTENSION_SOURCE, api, articleId);});
+    document.getElementById("scrollHolder") !== null &&
+      document
+        .getElementById("scrollHolder")
+        .removeEventListener("scroll", function () {
+          onScroll(EXTENSION_SOURCE, api, articleId);
+        });
   }
-}
-  
-if (interactiveTextArray === undefined) {
-  return <ZeeguuLoader/>
-}
+
+  if (!modalIsOpen) {
+    location.reload();
+  }
+
+  //Could be moved into another file
+  function handlePostCopy() {
+    api.makePersonalCopy(articleId, (message) => alert(message));
+    api.logReaderActivity(EXTENSION_SOURCE, api.PERSONAL_COPY, articleId);
+  }
+
+  //Could be moved into another file
+  function reportProblem(e) {
+    let answer = prompt("What is wrong with the article?");
+    if (answer) {
+      let feedback = "problem_" + answer.replace(/ /g, "_");
+      api.logReaderActivity(EXTENSION_SOURCE, api.EXTENSION_FEEDBACK, articleId, feedback);
+    }
+  }
+
+  if (interactiveTextArray === undefined) {
+    return <ZeeguuLoader />;
+  }
 
   return (
     <div>
-      <GlobalStyle/>
-      <StyledModal
-        isOpen={modalIsOpen}
-        className="Modal"
-        id="scrollHolder"
-      >
-         <StyledHeading >
+      <GlobalStyle />
+      <StyledModal isOpen={modalIsOpen} className="Modal" id="scrollHolder">
+        <StyledHeading>
           <StyledCloseButton role="button" onClick={handleClose} id="qtClose">
             X
-            </StyledCloseButton>
+          </StyledCloseButton>
           <ToolbarButtons
-           translating={translating}
-           pronouncing={pronouncing}
-           setTranslating={setTranslating}
-           setPronouncing={setPronouncing}
+            translating={translating}
+            pronouncing={pronouncing}
+            setTranslating={setTranslating}
+            setPronouncing={setPronouncing}
           />
         </StyledHeading>
-        <StyledButton onClick={reportProblem} >Report problems</StyledButton>
-        <StyledButton onClick={handlePostCopy}>
-          Make Personal Copy
-          </StyledButton>
+        <StyledButton onClick={reportProblem}>Report problems</StyledButton>
+        <StyledButton onClick={handlePostCopy}>Make Personal Copy</StyledButton>
         <h1>
           <TranslatableText
             interactiveText={interactiveTitle}
@@ -149,10 +149,12 @@ if (interactiveTextArray === undefined) {
         </h1>
         <p>{author}</p>
         <hr />
-        {articleImage === undefined ? null : <img id="zeeguuImage" alt={articleImage.alt} src={articleImage.src}></img>}
+        {articleImage === undefined ? null : (
+          <img id="zeeguuImage" alt={articleImage.alt} src={articleImage.src}></img>
+        )}
         {interactiveTextArray.map((paragraph) => {
-            const CustomTag = `${paragraph.tag}`;
-            if ((paragraph.tag === "P") || (paragraph.tag === "H3") || (paragraph.tag === "H2") || (paragraph.tag === "H4") || (paragraph.tag === "H5")){
+          const CustomTag = `${paragraph.tag}`;
+          if (TEXT_CONTENT.includes(paragraph.tag)) {
             return (
               <CustomTag>
                 <TranslatableText
@@ -161,25 +163,28 @@ if (interactiveTextArray === undefined) {
                   pronouncing={pronouncing}
                 />
               </CustomTag>
-            )}
-          if((paragraph.tag ==="OL") || (paragraph.tag ==="UL")){
-            let list = Array.from(paragraph.list)
+            );
+          }
+          if (LIST_CONTENT.includes(paragraph.tag)) {
+            let list = Array.from(paragraph.list);
             return (
               <CustomTag>
-              {list.map((paragraph, i) => {
-                return(
-                <li key={i}>
-                <TranslatableText
-                  interactiveText={paragraph.text}
-                  translating={translating}
-                  pronouncing={pronouncing}
-                />
-                </li>)})}
-                </CustomTag>
-            )
+                {list.map((paragraph, i) => {
+                  return (
+                    <li key={i}>
+                      <TranslatableText
+                        interactiveText={paragraph.text}
+                        translating={translating}
+                        pronouncing={pronouncing}
+                      />
+                    </li>
+                  );
+                })}
+              </CustomTag>
+            );
           }
         })}
-        <ReviewVocabulary/>
+        <ReviewVocabulary />
       </StyledModal>
     </div>
   );
--- src/JSInjection/constants.js
+++ src/JSInjection/constants.js
@@ -1,1 +1,3 @@
 export const EXTENSION_SOURCE = "EXTENSION - ";
+export const TEXT_CONTENT = ['P', 'H1', 'H2', 'H3', 'H4', 'H5'];
+export const LIST_CONTENT = ['OL', 'UL'];
