96532e4b1e065322deedf1fc2dceb4a9526bc2a3
emte <emte@itu.dk>
2022-03-15 20:37:38 +0100

    Fixed error on Berlingske

--- src/JSInjection/Cleaning/Pages/berlingske.js
+++ src/JSInjection/Cleaning/Pages/berlingske.js
@@ -1,29 +1,32 @@
 export const berlingskeRegex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]berlingske+)\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
 
-
 export function cleanBerlingske(readabilityContent) {
   const div = document.createElement("div");
   div.innerHTML = readabilityContent;
   let notice = div.querySelector("#speechkit-notice");
-  if (notice !== undefined && notice !== null) {
+  if (notice) {
     notice.remove();
   }
   let shift = div.querySelector("#speechkit-shift");
-  if (shift !== undefined && shift !== null) {
+  if (shift) {
     shift.remove();
   }
   let consent = div.querySelector("#speechkit-consent-box");
-  if (consent !== undefined && consent !== null) {
+  if (consent) {
     consent.remove();
   }
   return div.innerHTML;
 }
 
-export function cleanBerlingskeBefore(documentClone){
-  const figcaption = documentClone.getElementsByTagName("figcaption")[0]
-  figcaption.remove()
-  const bylineClass = documentClone.getElementsByClassName("article-byline article-byline--regular")[0]
-  const authorsName = documentClone.getElementsByClassName("article-byline__author-name")[0]
-  bylineClass.insertBefore(authorsName, bylineClass.firstChild);
+export function cleanBerlingskeBefore(documentClone) {
+  const figcaption = documentClone.getElementsByTagName("figcaption")[0];
+  if (figcaption) {
+    figcaption.remove();
+  }
+  const bylineClass = documentClone.getElementsByClassName("article-byline article-byline--regular")[0];
+  const authorsName = documentClone.getElementsByClassName("article-byline__author-name")[0];
+  if (bylineClass && authorsName) {
+    bylineClass.insertBefore(authorsName, bylineClass.firstChild);
+  }
   return documentClone;
 }
