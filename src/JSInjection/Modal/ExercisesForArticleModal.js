2f319149f24662e50db5bc497b3cfcedb30ad0dd
emte <emte@itu.dk>
2022-03-17 18:47:17 +0100

    Imported from zeeguu-react instead

--- src/JSInjection/Modal/ExercisesForArticleModal.js
+++
@@ -1,7 +0,0 @@
-
-//import Exercises from "../../zeeguu-react/src/exercises/Exercises"//
-
-//export default function ExercisesForArticle({ api, articleID}) {
-//0
-//  return <Exercises api={api} articleID={articleID} />;
-//}//
