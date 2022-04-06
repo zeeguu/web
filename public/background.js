3e4fa443c70abdd39ba37a74694ae0d5342515d7
emte <emte@itu.dk>
2022-04-06 17:19:46 +0200

    Removed commented code

--- public/background.js
+++ public/background.js
@@ -1,22 +1,5 @@
 
 chrome.runtime.onMessageExternal.addListener((request, sender, sendResponse) => {
     console.log("Received message from " + sender + ": ", request);
-    //sendResponse("hi there" + sender + request); //respond however you like
-    sendResponse({message: true}); //respond however you like
+    sendResponse({message: true});
 });
-
-//chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//    console.log({ request })
-//});
-
-//chrome.runtime.onMessageExternal.addListener(
-//    function(request, sender, sendResponse) {
-//        console.log(request)
-//        if (request) {
-//           
-//                    sendResponse("i am an zeeguu extension");
-//            
-//        }
-//        return true;
-//    });
-
