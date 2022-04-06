53ab7373c691fdf0be05706955d89f41a1139529
emte <emte@itu.dk>
2022-04-06 16:02:40 +0200

    Implemented a check of whether user has extension

--- src/LoggedInRouter.js
+++ src/LoggedInRouter.js
@@ -7,10 +7,55 @@ import TeacherRouter from "./teacher/_routing/_TeacherRouter";
 import Settings from "./pages/Settings";
 import ArticleReader from "./reader/ArticleReader";
 import UserDashboard from "./userDashboard/UserDashboard";
+import React, { useState, useEffect } from "react";
+import ExtensionMessage from "./components/ExtensionMessage";
 
+/*global chrome*/
 export default function LoggedInRouter({ api, setUser }) {
+  const EXTENSION_ID = "ghnfbnnmkbhhbcionebpncddbpflehmp";
+
+  const [hasExtension, setHasExtension] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    if (chrome.runtime) {
+      chrome.runtime.sendMessage(
+        EXTENSION_ID,
+        "This is a message from Zeeguu.org",
+        function (reply) {
+          if (chrome.runtime.lastError) {
+            console.log(chrome.runtime.lastError);
+          }
+          if (reply.message === true) {
+            setHasExtension(true);
+            console.log("Extension installed!");
+          }
+          else{
+            handleOpen()
+          }
+        }
+      );
+    } else {
+      handleOpen();
+    }
+  }, []);
+
+  function handleClose() {
+    setOpen(false);
+  }
+
+  function handleOpen() {
+    setOpen(true);
+  }
+
   return (
     <SideBar api={api}>
+      {!hasExtension ? (
+        <ExtensionMessage
+          handleClose={handleClose}
+          open={open}
+        ></ExtensionMessage>
+      ) : null}
       <PrivateRoute path="/articles" api={api} component={ArticlesRouter} />
       <PrivateRoute path="/exercises" api={api} component={ExercisesRouter} />
       <PrivateRoute path="/words" api={api} component={WordsRouter} />
