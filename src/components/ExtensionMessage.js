bb89b3f603d1f39839c9e08fdd9ac7ee002acfc5
emte <emte@itu.dk>
2022-04-06 16:33:10 +0200

    Updated text in Extension popup

--- src/components/ExtensionMessage.js
+++ src/components/ExtensionMessage.js
@@ -13,7 +13,13 @@ export default function ExtensionMessage({handleClose, open}) {
     aria-describedby="modal-modal-description"
   >
     <Box sx={s.style}>
-      <p>No extension installed</p>
+      <p>Zeeguu has launched a Chrome Extension, which you will need to use, when reading articles</p>
+      <p>The extension allows you to enrich your vocabulary in a foreign language while browsing the web and reading articles that are interesting to you.
+          These could be articles on news sites, blogs, or encyclopedias like Wikipedia.
+          For better readability, all excess clutter, like adverts, buttons, and videos are removed, leaving the article looking more like a page in a book.
+          <br/> <br/>
+      <a href="https://chrome.google.com/webstore/detail/zeeguu/ckncjmaednfephhbpeookmknhmjjodcd" target="_blank" rel="noopener noreferrer"> Install it in the Chrome Web Store</a>
+      </p>
     </Box>
   </Modal>
 
