4d006e98c98d4d6f626c00c5d02530de7b150d7e
emte <emte@itu.dk>
2022-04-07 17:31:31 +0200

    checks if mobile

--- src/utils/misc/mobileDetection.js
+++ src/utils/misc/mobileDetection.js
@@ -1,6 +1,6 @@
-function isMobile() {
   // cf: https://stackoverflow.com/questions/3514784/what-is-the-best-way-to-detect-a-mobile-device
-  return /Mobi|Android/i.test(navigator.userAgent);
+function isMobile() {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobi|Android/i.test(navigator.userAgent);
 }
 
 export { isMobile };
