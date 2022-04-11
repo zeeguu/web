de55dc94f7fc4f19fa09990e191568913bacf378
emte <emte@itu.dk>
2022-04-11 15:35:28 +0200

    Open "Create account" on install

--- public/background.js
+++ public/background.js
@@ -3,3 +3,10 @@ chrome.runtime.onMessageExternal.addListener((request, sender, sendResponse) => 
     console.log("Received message from " + sender.url + ": ", request);
     sendResponse({message: true});
 });
+
+chrome.runtime.onInstalled.addListener(function (object) {
+    let externalUrl = "https://www.zeeguu.org/create_account";
+    if (object.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+        chrome.tabs.create({ url: externalUrl })
+    }
+});
