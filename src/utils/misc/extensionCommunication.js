76311d7fe0f33d42bd47a3095d576373bcd403a2
Mircea Lungu <mircea.lungu@gmail.com>
2022-04-12 16:34:30 +0200

    one more log

--- src/utils/misc/extensionCommunication.js
+++ src/utils/misc/extensionCommunication.js
@@ -2,9 +2,10 @@
 // (this will let our linter know we are accessing Chrome browser methods)
 
 function checkExtensionInstalled(setHasExtension) {
-  console.log("inside of checkExtensionInstalled")
+  console.log("inside of checkExtensionInstalled");
+  console.log(process.env.REACT_APP_EXTENSION_ID);
   if (chrome.runtime) {
-    console.log("chrome runtime: " + chrome.runtime)
+    console.log("chrome runtime: " + chrome.runtime);
     chrome.runtime.sendMessage(
       process.env.REACT_APP_EXTENSION_ID,
       "You are on Zeeguu.org!",
@@ -19,7 +20,7 @@ function checkExtensionInstalled(setHasExtension) {
       }
     );
   } else {
-    console.log("setHasExtension is false")
+    console.log("setHasExtension is false");
     setHasExtension(false);
   }
 }
