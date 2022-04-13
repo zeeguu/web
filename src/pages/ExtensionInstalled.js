529c1828d9f3d0d9fe07fff0c6f8900639bd1578
Frida Helene Beck-Larsen <frib@itu.dk>
2022-04-13 14:22:08 +0200

    Own texts changed to my texts

--- src/pages/ExtensionInstalled.js
+++ src/pages/ExtensionInstalled.js
@@ -1,26 +1,26 @@
 import { Link } from "react-router-dom";
 import * as s from "../components/ExtensionInstalled.sc";
-import {getUserSession} from "../utils/cookies/userInfo";
-
+import { getUserSession } from "../utils/cookies/userInfo";
 
 export default function ExtensionInstalled() {
   if (getUserSession()) {
     return (
       <s.ExtensionInstalledWrapper>
-        <h1>Congratulations</h1>
-        <p>Don't forget to pin the extension</p>
+        <h1>Congratulations!</h1>
+        <p>You have installed the extension.</p>
+        <p>Don't forget to pin it</p>
         <Link to="/articles">Go to articles</Link>
-        </s.ExtensionInstalledWrapper>
+      </s.ExtensionInstalledWrapper>
     );
   } else {
     return (
       <s.ExtensionInstalledWrapper>
-      <h1>Congratulations</h1>
-      <p>Don't forget to pin the extension</p>
-      <s.PaddedLink to="/login">Login</s.PaddedLink>
-      <s.PaddedLink to="/create_account">Create account</s.PaddedLink>
+        <h1>Congratulations!</h1>
+        <p>You have installed the extension.</p>
+        <p>Don't forget to pin it.</p>
+        <s.PaddedLink to="/login">Login</s.PaddedLink>
+        <s.PaddedLink to="/create_account">Create account</s.PaddedLink>
       </s.ExtensionInstalledWrapper>
-    )
+    );
   }
- 
 }
