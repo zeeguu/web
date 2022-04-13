24faeb46258874c38560fe8ab655bfbb440d4ec4
Frida Helene Beck-Larsen <frib@itu.dk>
2022-04-13 13:46:43 +0200

    ExtensionInstalled component

--- src/pages/ExtensionInstalled.js
+++ src/pages/ExtensionInstalled.js
@@ -1,11 +1,26 @@
 import { Link } from "react-router-dom";
+import * as s from "../components/ExtensionInstalled.sc";
+import {getUserSession} from "../utils/cookies/userInfo";
+
 
 export default function ExtensionInstalled() {
-  return (
-    <>
+  if (getUserSession()) {
+    return (
+      <s.ExtensionInstalledWrapper>
+        <h1>Congratulations</h1>
+        <p>Don't forget to pin the extension</p>
+        <Link to="/articles">Go to articles</Link>
+        </s.ExtensionInstalledWrapper>
+    );
+  } else {
+    return (
+      <s.ExtensionInstalledWrapper>
       <h1>Congratulations</h1>
       <p>Don't forget to pin the extension</p>
-      <Link to="/articles">Go to articles</Link>
-    </>
-  );
+      <s.PaddedLink to="/login">Login</s.PaddedLink>
+      <s.PaddedLink to="/create_account">Create account</s.PaddedLink>
+      </s.ExtensionInstalledWrapper>
+    )
+  }
+ 
 }
