192b96d3c0f2f0eeff5aa6bc937f5fcd1c007d06
Frida Helene Beck-Larsen <frib@itu.dk>
2022-05-31 16:01:56 +0200

    Video guide

--- src/pages/ExtensionInstalled.js
+++ src/pages/ExtensionInstalled.js
@@ -9,7 +9,7 @@ export default function ExtensionInstalled({ api }) {
   useEffect(() => {
     api.logUserActivity(api.OPEN_EXTENSION_INSTALLED);
   }, []);
-
+  
   return (
     <s.PageBackground>
       <z.LogoOnTop />
@@ -17,6 +17,9 @@ export default function ExtensionInstalled({ api }) {
         <s.ExtensionInstalledWrapper>
           <h1>{strings.congratulations}</h1>
           <h4>{strings.pinExtension}</h4>
+          <s.VideoLink>Learn how it works by watching
+          <a href="https://vimeo.com/715531198" target="_blank" rel="noreferrer"> this video</a>
+          </s.VideoLink>
           <img
             src={"https://zeeguu.org/static/images/zeeguuExtension.gif"}
             alt="How to pin Chrome Extension to Chrome Toolbar gif"
