6ce35e1a46177f234cc8507d0d43ed4bec80a6e7
emte <emte@itu.dk>
2022-06-23 13:34:27 +0200

    update egyszervolt

--- src/JSInjection/Cleaning/Pages/egyszervolt.js
+++ src/JSInjection/Cleaning/Pages/egyszervolt.js
@@ -3,7 +3,7 @@ export const egyszervoltRegex = /(http|https):\/\/(egyszervolt.hu).*/;
 export function removeIMGTag(content) {
     const div = document.createElement("div");
     div.innerHTML = content;
-    let image = div.getElementsByTagName("img")[0];
+    let image = div.querySelector("img");
     if(image){
         image.remove();
         div.appendChild(image);
