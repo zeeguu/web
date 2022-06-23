1d9eae8143c50d62e23d0eb28af9c748034ad9d2
emte <emte@itu.dk>
2022-06-23 11:30:34 +0200

    updated bbc

--- src/JSInjection/Cleaning/Pages/bbc.js
+++ src/JSInjection/Cleaning/Pages/bbc.js
@@ -1,11 +1,22 @@
 export const bbcRegex = /(http|https):\/\/(www.bbc.com).*/;
+import { removeFirstElementIfExistent } from "../util";
+
+//export function cleanBBC(readabilityContent) {
+//  const newDiv = document.createElement("div");
+//  newDiv.innerHTML = readabilityContent;
+//  const readMore = newDiv.querySelectorAll("[data-component=links-block]")[0];
+//  if (readMore) {
+//    readMore.remove();
+//  }
+//  return newDiv.innerHTML;
+//}//
+
 
 export function cleanBBC(readabilityContent) {
-  const newDiv = document.createElement("div");
-  newDiv.innerHTML = readabilityContent;
-  const readMore = newDiv.querySelectorAll("[data-component=links-block]")[0];
-  if (readMore) {
-    readMore.remove();
-  }
-  return newDiv.innerHTML;
+  const div = document.createElement("div");
+  div.innerHTML = readabilityContent;
+
+  removeFirstElementIfExistent("[data-component=links-block]", div)
+
+  return div.innerHTML;
 }
