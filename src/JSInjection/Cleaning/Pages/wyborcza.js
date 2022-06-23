2811a0e880bac53a192709e9be30a176aa76cdd3
emte <emte@itu.dk>
2022-06-23 17:53:10 +0200

    update wyborcza

--- src/JSInjection/Cleaning/Pages/wyborcza.js
+++ src/JSInjection/Cleaning/Pages/wyborcza.js
@@ -1,13 +1,10 @@
+import { removeAllElementsIfExistent } from "../util";
+
 export const wyborczaRegex = /(http|https):\/\/(wyborcza.pl).*/;
 
 export function cleanWyborcza(readabilityContent) {
-  const newDiv = document.createElement("div");
-  newDiv.innerHTML = readabilityContent;
-
-  const signUpBanner = newDiv.querySelectorAll("[id^=mcBan]")
-  for (let index = 0; index < signUpBanner.length; index++) {
-      signUpBanner[index].remove()
-  }
-
-  return newDiv.innerHTML;
+  const div = document.createElement("div");
+  div.innerHTML = readabilityContent;
+  removeAllElementsIfExistent("[id^=mcBan]", div)
+  return div.innerHTML;
 }
