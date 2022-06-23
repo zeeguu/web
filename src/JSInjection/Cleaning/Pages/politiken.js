399feefb959c51ddb7fa0b5dce5acfaa0db4d93b
emte <emte@itu.dk>
2022-06-23 17:17:01 +0200

    update politiken

--- src/JSInjection/Cleaning/Pages/politiken.js
+++ src/JSInjection/Cleaning/Pages/politiken.js
@@ -1,13 +1,10 @@
+import { removeAllElementsIfExistent } from "../util";
+
 export const politikenRegex = /(http|https):\/\/(politiken.dk).*/;
 
 export function removeSignUp(readabilityContent) {
-  const newDiv = document.createElement("div");
-  newDiv.innerHTML = readabilityContent;
-  const signUp = newDiv.querySelectorAll('[data-element-type="relation"]');
-  if (signUp) {
-    for (let i = 0; i < signUp.length; i++) {
-      signUp[i].remove();
-    }
-  }
-  return newDiv.innerHTML;
+  const div = document.createElement("div");
+  div.innerHTML = readabilityContent;
+  removeAllElementsIfExistent('[data-element-type="relation"]', div)
+  return div.innerHTML;
 }
