ab60e88f1d629339e472b326ba02acc8cd111c6e
Frida Helene Beck-Larsen <frib@itu.dk>
2022-06-28 16:58:29 +0200

    eb function

--- src/JSInjection/Cleaning/Pages/ekstrabladet.js
+++ src/JSInjection/Cleaning/Pages/ekstrabladet.js
@@ -1,9 +1,12 @@
 import { getHTMLContent } from "../pageSpecificClean";
-import { createDivWithContent, removeAllElementsWithText } from "../util";
+import { createDivWithContent, removeAllElementsIfExistent, removeAllElementsWithText } from "../util";
 
 export const ekstrabladetRegex = /^(http|https):\/\/ekstrabladet.dk\/.*/;
 
 export function cleanEkstraBladetBefore(documentClone) {
+  [".split-element--ekstra", ".jw-reset", ".figure.image-container"].forEach((elem) => {
+    removeAllElementsIfExistent(elem, documentClone)
+  });
   removeAllElementsWithText("p", "Artiklen fortsætter under", documentClone)
   return documentClone;
 }
