8c1bff7cc832c454a5060305b6a9a35134448237
Mircea Filip Lungu <mircea.lungu@gmail.com>
2022-11-30 23:51:39 +0100

    Update zeeguuOrgContentScript.js

--- public/zeeguuOrgContentScript.js
+++ public/zeeguuOrgContentScript.js
@@ -1,8 +1,22 @@
-console.log("running the content script");
-window.postMessage(
-  {
-    direction: "from-content-script",
-    message: "Message from extension",
-  },
-  "*"
-);
+function repeat_with_delay(i) {
+  setTimeout(function () {
+    notifyZeeguuOrgOfExtensionPresence();
+    i--;
+    if (i > 0) {
+      repeat_with_delay(i);
+    }
+  }, 100);
+}
+
+function notifyZeeguuOrgOfExtensionPresence() {
+  r = window.postMessage(
+    {
+      direction: "from-content-script",
+      message: "Greetings from the Zeeguu Extension",
+    },
+    "*"
+  );
+  console.log(r);
+}
+
+repeat_with_delay(10); //  start the loop
