a5ae57f4b9f35727435cb5fcaa45387f561ebc50
Mircea Filip Lungu <mircea.lungu@gmail.com>
2022-12-01 00:08:27 +0100

    fixing issue #36

--- public/zeeguuOrgContentScript.js
+++ public/zeeguuOrgContentScript.js
@@ -1,22 +1,26 @@
-function repeat_with_delay(i) {
-  setTimeout(function () {
-    notifyZeeguuOrgOfExtensionPresence();
-    i--;
-    if (i > 0) {
-      repeat_with_delay(i);
-    }
-  }, 100);
-}
+// for some reason this gets injected too early and the page is not ready yet
+// in order to handle this, we repeat the injection a few times
+
+NUMBER_OF_RETRIES = 10;
+DELAY_BETWEEN_RETRIES = 100;
 
 function notifyZeeguuOrgOfExtensionPresence() {
-  r = window.postMessage(
+  window.postMessage(
     {
       direction: "from-content-script",
       message: "Greetings from the Zeeguu Extension",
     },
     "*"
   );
-  console.log(r);
 }
 
-repeat_with_delay(10); //  start the loop
+function repeat_with_delay(i, fun) {
+  setTimeout(function () {
+    fun();
+    if (i > 0) {
+      repeat_with_delay(i - 1, fun);
+    }
+  }, DELAY_BETWEEN_RETRIES);
+}
+
+repeat_with_delay(NUMBER_OF_RETRIES, notifyZeeguuOrgOfExtensionPresence); //  start the loop
