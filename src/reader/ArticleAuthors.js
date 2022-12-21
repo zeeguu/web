c433e437606437de365050abafaef9ebb09a0329
Mircea Filip Lungu <mircea.lungu@gmail.com>
2022-12-21 22:50:56 +0200

    small refactoring

---
+++ src/reader/ArticleAuthors.js
@@ -0,0 +1,18 @@
+function multipleAuthors(authors_str) {
+  return authors_str.split(",").length > 1;
+}
+
+export default function ArticleAuthors({ articleInfo }) {
+  return (
+    <div>
+      <div>
+        {articleInfo.authors &&
+          "Author" +
+            (multipleAuthors(articleInfo.authors) ? "s" : "") +
+            ": "}{" "}
+        {articleInfo.authors}
+      </div>
+      <br />
+    </div>
+  );
+}
