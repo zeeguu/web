14c89499c1310d1baed5f1afbf242ca4ec447fba
Mircea Filip Lungu <mircea.lungu@gmail.com>
2023-01-06 16:01:44 +0200

    Update SoundPlayer.js

--- src/reader/SoundPlayer.js
+++ src/reader/SoundPlayer.js
@@ -1,44 +1,57 @@
 import { useEffect, useState } from "react";
 
+// (This is a tiny MP3 file that is silent and extremely short - retrieved from https://bigsoundbank.com and then modified)
+const EMPTY_SOUND_DATA =
+  "data:audio/mpeg;base64,SUQzBAAAAAABEVRYWFgAAAAtAAADY29tbWVudABCaWdTb3VuZEJhbmsuY29tIC8gTGFTb25vdGhlcXVlLm9yZwBURU5DAAAAHQAAA1N3aXRjaCBQbHVzIMKpIE5DSCBTb2Z0d2FyZQBUSVQyAAAABgAAAzIyMzUAVFNTRQAAAA8AAANMYXZmNTcuODMuMTAwAAAAAAAAAAAAAAD/80DEAAAAA0gAAAAATEFNRTMuMTAwVVVVVVVVVVVVVUxBTUUzLjEwMFVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVf/zQsRbAAADSAAAAABVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVf/zQMSkAAADSAAAAABVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV";
+
 export default function SoundPlayer({ api, interactiveText }) {
   const [state, setState] = useState("initial");
   const [mp3Player, setMp3Player] = useState();
 
+  function onDestroy() {
+    if (mp3Player) {
+      mp3Player.pause();
+    }
+  }
+
+  function onCreate() {}
+
   useEffect(() => {
+    onCreate();
     return () => {
-      if (mp3Player) {
-        mp3Player.pause();
-      }
+      onDestroy();
     };
   }, [mp3Player]);
+
+  function startPlaying() {
+    // onClick of first interaction on page before I need the sounds
+    let player = playSilentAudio();
+
+    api.getLinkToFullArticleReadout(
+      interactiveText.articleInfo,
+      interactiveText.articleInfo.id,
+      (linkToMp3) => {
+        player.src = linkToMp3;
+        setMp3Player(player);
+        setState("playing");
+      }
+    );
+  }
+
+  function pausePlay() {
+    setState("paused");
+    mp3Player.pause();
+  }
+
+  function resumePlay() {
+    setState("playing");
+    mp3Player.play();
+  }
 
   if (state === "initial") {
     return (
       <div>
-        <button
-          onClick={() => {
-            // interactiveText.playAll();
-            let player = new Audio();
-            player.autoplay = true;
-
-            // onClick of first interaction on page before I need the sounds
-            // (This is a tiny MP3 file that is silent and extremely short - retrieved from https://bigsoundbank.com and then modified)
-            player.src =
-              "data:audio/mpeg;base64,SUQzBAAAAAABEVRYWFgAAAAtAAADY29tbWVudABCaWdTb3VuZEJhbmsuY29tIC8gTGFTb25vdGhlcXVlLm9yZwBURU5DAAAAHQAAA1N3aXRjaCBQbHVzIMKpIE5DSCBTb2Z0d2FyZQBUSVQyAAAABgAAAzIyMzUAVFNTRQAAAA8AAANMYXZmNTcuODMuMTAwAAAAAAAAAAAAAAD/80DEAAAAA0gAAAAATEFNRTMuMTAwVVVVVVVVVVVVVUxBTUUzLjEwMFVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVf/zQsRbAAADSAAAAABVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVf/zQMSkAAADSAAAAABVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV";
-
-            api.getLinkToFullArticleReadout(
-              interactiveText.articleInfo,
-              interactiveText.articleInfo.id,
-              (linkToMp3) => {
-                player.src = linkToMp3;
-                player.autoplay = true;
-                console.log(player);
-                setMp3Player(player);
-                setState("playing");
-              }
-            );
-          }}
-        >
+        <button onClick={startPlaying}>
           <img width="35px" src="/static/images/play-button.svg" />
         </button>
         <div class="buttonText">Read aloud</div>
@@ -46,27 +59,26 @@ export default function SoundPlayer({ api, interactiveText }) {
     );
   } else if (state === "playing") {
     return (
-      <button
-        onClick={() => {
-          setState("paused");
-          console.dir(mp3Player);
-          mp3Player.pause();
-        }}
-      >
-        <img width="35px" src="/static/images/pause-button.svg" />
-      </button>
+      <div>
+        <button onClick={pausePlay}>
+          <img width="35px" src="/static/images/pause-button.svg" />
+        </button>
+      </div>
     );
   } else if (state === "paused") {
     return (
-      <button
-        onClick={() => {
-          setState("playing");
-          console.dir(mp3Player);
-          mp3Player.play();
-        }}
-      >
-        <img width="35px" src="/static/images/play-button.svg" />
-      </button>
+      <div>
+        <button onClick={resumePlay}>
+          <img width="35px" src="/static/images/play-button.svg" />
+        </button>
+      </div>
     );
   }
+}
+
+function playSilentAudio() {
+  let player = new Audio();
+  player.autoplay = true;
+  player.src = EMPTY_SOUND_DATA;
+  return player;
 }
