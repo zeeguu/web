b522f9d5e46658a3b325a3fda33b577626a7eb0f
Mircea Filip Lungu <mircea.lungu@gmail.com>
2023-01-06 16:49:34 +0200

    showing elapsed time in audio player

--- src/reader/SoundPlayer.js
+++ src/reader/SoundPlayer.js
@@ -1,12 +1,18 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 // (This is a tiny MP3 file that is silent and extremely short - retrieved from https://bigsoundbank.com and then modified)
 const EMPTY_SOUND_DATA =
   "data:audio/mpeg;base64,SUQzBAAAAAABEVRYWFgAAAAtAAADY29tbWVudABCaWdTb3VuZEJhbmsuY29tIC8gTGFTb25vdGhlcXVlLm9yZwBURU5DAAAAHQAAA1N3aXRjaCBQbHVzIMKpIE5DSCBTb2Z0d2FyZQBUSVQyAAAABgAAAzIyMzUAVFNTRQAAAA8AAANMYXZmNTcuODMuMTAwAAAAAAAAAAAAAAD/80DEAAAAA0gAAAAATEFNRTMuMTAwVVVVVVVVVVVVVUxBTUUzLjEwMFVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVf/zQsRbAAADSAAAAABVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVf/zQMSkAAADSAAAAABVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV";
 
+const UNKNOWN = "-:--";
+
 export default function SoundPlayer({ api, interactiveText }) {
   const [state, setState] = useState("initial");
   const [mp3Player, setMp3Player] = useState();
+  const intervalRef = useRef();
+  const [totalTime, setTotalTime] = useState(UNKNOWN);
+  const [currentTime, setCurrentTime] = useState("0:00" + "/" + totalTime);
+  const [counter, setCounter] = useState(0);
 
   function onDestroy() {
     if (mp3Player) {
@@ -15,6 +21,22 @@ export default function SoundPlayer({ api, interactiveText }) {
   }
 
   function onCreate() {}
+
+  useEffect(() => {
+    intervalRef.current = setInterval(() => {
+      if (!mp3Player || !mp3Player.currentTime || !mp3Player.duration) {
+        setCounter(counter + 1);
+        return;
+      }
+
+      if (totalTime == UNKNOWN) {
+        setTotalTime(formatMMSS(mp3Player.duration));
+      }
+
+      setCurrentTime(formatMMSS(mp3Player.currentTime) + " / " + totalTime);
+    }, 1000);
+    return () => clearInterval(intervalRef.current);
+  }, [currentTime, counter]);
 
   useEffect(() => {
     onCreate();
@@ -33,6 +55,7 @@ export default function SoundPlayer({ api, interactiveText }) {
       (linkToMp3) => {
         player.src = linkToMp3;
         setMp3Player(player);
+
         setState("playing");
       }
     );
@@ -63,6 +86,7 @@ export default function SoundPlayer({ api, interactiveText }) {
         <button onClick={pausePlay}>
           <img width="35px" src="/static/images/pause-button.svg" />
         </button>
+        <div class="buttonText">{currentTime}</div>
       </div>
     );
   } else if (state === "paused") {
@@ -71,6 +95,7 @@ export default function SoundPlayer({ api, interactiveText }) {
         <button onClick={resumePlay}>
           <img width="35px" src="/static/images/play-button.svg" />
         </button>
+        <div class="buttonText">{currentTime}</div>
       </div>
     );
   }
@@ -81,4 +106,10 @@ function playSilentAudio() {
   player.autoplay = true;
   player.src = EMPTY_SOUND_DATA;
   return player;
+}
+
+function formatMMSS(seconds) {
+  let minutes = Math.floor(seconds / 60);
+  let remainingSeconds = Math.floor(seconds - minutes * 60);
+  return minutes + ":" + (remainingSeconds < 10 ? "0" : "") + remainingSeconds;
 }
