afef504f7f48cbe9039ed956c7aa5ee3e7efb79a
Mircea Filip Lungu <mircea.lungu@gmail.com>
2023-08-09 23:04:52 +0300

    changed title

--- src/articles/FindArticles.js
+++ src/articles/FindArticles.js
@@ -56,7 +56,7 @@ export default function NewArticles({ api }) {
         setOriginalList([...articles]);
       });
     }
-    setTitle("Zeeguu");
+    document.title = "Zeeguu";
   }, []);
 
   useEffect(() => {
