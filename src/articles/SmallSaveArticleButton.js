55aaf2f6ff866b61728874cfd00bb602614e2a1e
iga waclawska <iga.waclawska@gmail.com>
2023-09-03 17:59:51 +0200

    lift the article's isSaved state to allow the SmallSaveButton and the modal to communicate

--- src/articles/SmallSaveArticleButton.js
+++ src/articles/SmallSaveArticleButton.js
@@ -1,41 +1,51 @@
 import * as s from "./SmallSaveArticleButton.sc.js";
-import {toast, ToastContainer} from "react-toastify";
-import {useState} from "react";
-import 'react-toastify/dist/ReactToastify.css';
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
+export default function SmallSaveArticleButton({
+  article,
+  api,
+  isSaved,
+  setIsSaved,
+}) {
+  function saveArticle() {
+    api.makePersonalCopy(article.id, (data) => {
+      if (data === "OK") {
+        setIsSaved(true);
+        toast("Article added to your Saves!");
+      }
+    });
+  }
 
-export default function SmallSaveArticleButton ({article, api}) {
+  return (
+    <>
+      {isSaved ? (
+        <s.SavedLabel>
+          {" "}
+          Saved <img
+            src="/static/images/zeeguuLogo.svg"
+            width="11"
+            alt={""}
+          />{" "}
+        </s.SavedLabel>
+      ) : (
+        <div>
+          <s.SaveButton onClick={saveArticle}>Save</s.SaveButton>
+        </div>
+      )}
 
-    const [isSaved, setIsSaved] = useState(article.has_personal_copy);
-
-    function saveArticle() {
-        api.makePersonalCopy(article.id, (data)=> {
-            if (data === "OK") {
-                setIsSaved(true);
-                toast('Article added to your Saves!');
-            }
-        })
-    }
-
-    return <>
-
-        {isSaved?
-            <s.SavedLabel> Saved <img src="/static/images/zeeguuLogo.svg" width="11" alt={""}/> </s.SavedLabel> :
-            <div><s.SaveButton onClick={saveArticle}>Save</s.SaveButton></div>}
-
-        <ToastContainer
-            position="bottom-right"
-            autoClose={2000}
-            hideProgressBar={true}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="light"
-        />
-        </>
-
-
+      <ToastContainer
+        position="bottom-right"
+        autoClose={2000}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
+  );
 }
