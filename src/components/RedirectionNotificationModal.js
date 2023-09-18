a61d4c20ab23db7a64709591bf09720559f23251
iga waclawska <iga.waclawska@gmail.com>
2023-09-18 10:26:37 +0200

    apply RedirectionNotificationForMobile

--- src/components/RedirectionNotificationModal.js
+++ src/components/RedirectionNotificationModal.js
@@ -4,6 +4,7 @@ import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 import * as s from "../components/RedirectionNotificationModal.sc";
 import { isMobile } from "../utils/misc/browserDetection";
 import RedirectionNotificationForDesktop from "./RedirectionNotificationForDesktop";
+import RedirectionNotificationForMobile from "./RedirectionNotificationForMobile";
 
 //This modal is used in the ArticlePreview component
 
@@ -53,53 +54,23 @@ export default function RedirectionNotificationModal({
     <Modal open={open} onClose={handleClose}>
       <s.ModalWrapper>
         {!isMobile() ? (
-            <RedirectionNotificationForDesktop
-              handleVisibilityCheckboxSelection={
-                handleVisibilityCheckboxSelection
-              }
-              selectedDoNotShowRedirectionModal={
-                selectedDoNotShowRedirectionModal
-              }
-              article={article}
-              handleCloseAndSavePreferences={handleCloseAndSavePreferences}
-              handleClose={handleClose}
-            />
+          <RedirectionNotificationForDesktop
+            handleVisibilityCheckboxSelection={
+              handleVisibilityCheckboxSelection
+            }
+            selectedDoNotShowRedirectionModal={
+              selectedDoNotShowRedirectionModal
+            }
+            article={article}
+            handleCloseAndSavePreferences={handleCloseAndSavePreferences}
+            handleClose={handleClose}
+          />
         ) : (
-          // Displayed to the users who access Zeeguu from mobile browsers
-          //Todo: Create modal content components for these separate views
-          <>
-            <s.Header>
-              <h1>It looks like you are using&nbsp;a&nbsp;mobile device</h1>
-            </s.Header>
-            <s.Body>
-              <p>
-                If you want to read articles on your mobile device using Zeeguu,
-                just tap on the
-                <strong> Save </strong> button below the article's title or
-                click<strong> Save and view the article</strong> to add it to
-                your Saves.
-              </p>
-            </s.Body>
-            <s.CloseButton role="button" onClick={handleClose}>
-              <CloseRoundedIcon fontSize="medium" />
-            </s.CloseButton>
-            <s.Footer>
-              {/* "Do not show this message" option temporarily not
-              implemented here as the function handleModalUse() within
-              handleSaveAndOpenArticle() seems to fully work with React Link
-              on mobile only when target="_blank". This issue didn't occur
-              on the desktop and for regular <a> links. Needs further investigation
-              if we want this functionality here  */}
-              <Link to={`/read/article?id=${article.id}`}>
-                <s.GoToArticleButton
-                  role="button"
-                  onClick={handleSaveAndOpenArticle}
-                >
-                  Save and view the article
-                </s.GoToArticleButton>
-              </Link>
-            </s.Footer>
-          </>
+          <RedirectionNotificationForMobile
+            handleClose={handleClose}
+            article={article}
+            handleSaveAndOpenArticle={handleSaveAndOpenArticle}
+          />
         )}
       </s.ModalWrapper>
     </Modal>
