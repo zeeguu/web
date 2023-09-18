cbce43f88e2cae7029f8b26c9459c7541ec61407
iga waclawska <iga.waclawska@gmail.com>
2023-09-18 10:18:22 +0200

    apply RedirectionNotificationForDesktop

--- src/components/RedirectionNotificationForDesktop.js
+++ src/components/RedirectionNotificationForDesktop.js
@@ -1,9 +1,72 @@
 import * as s from "./RedirectionNotificationModal.sc";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 
 export default function RedirectionNotificationForDesktop(props) {
   return (
-    <s.ModalWrapper>
-      <div>Hello from Redirection Notification For Desktop</div>
-    </s.ModalWrapper>
+    <>
+      <s.Header>
+        <h1>
+          You are ready to&nbsp;continue<br></br>
+          to the original article's website
+        </h1>
+      </s.Header>
+      <s.Body>
+        <p>
+          <strong>Once there</strong>, find and{" "}
+          <strong>
+            click The Zeeguu Reader{" "}
+            <s.Icon>
+              <img
+                className="fullDivWidthImage"
+                alt=""
+                src="../static/images/zeeguuLogo.svg"
+              ></img>
+            </s.Icon>{" "}
+            icon
+          </strong>{" "}
+          in the top right corner of&nbsp;your browser's toolbar
+          or&nbsp;on&nbsp;the&nbsp;list of your installed extensions{" "}
+          <s.Icon>
+            <img
+              className="fullDivWidthImage"
+              alt=""
+              src="../static/images/puzzle.svg"
+            ></img>
+          </s.Icon>
+          . <strong>Then&nbsp;select Read Article</strong>.
+        </p>
+        <img
+          className="fullDivWidthImage"
+          src={"../static/images/find-extension.png"}
+          alt="Zeeguu browser extension"
+        />
+      </s.Body>
+      <s.Footer>
+        <s.CheckboxWrapper>
+          <input
+            onChange={props.handleVisibilityCheckboxSelection}
+            checked={props.selectedDoNotShowRedirectionModal}
+            type="checkbox"
+            id="checkbox"
+            name=""
+            value=""
+          ></input>{" "}
+          <label htmlFor="checkbox">Don't show this message</label>
+        </s.CheckboxWrapper>
+        <a target="_blank" rel="noreferrer" href={props.article.url}>
+          {/* Clicking the GoToArticleButton button sends the reader
+                to the article, saves visibility preferences of the modal and closes it */}
+          <s.GoToArticleButton
+            role="button"
+            onClick={props.handleCloseAndSavePreferences}
+          >
+            Enter the article's website
+          </s.GoToArticleButton>
+        </a>
+      </s.Footer>
+      <s.CloseButton role="button" onClick={props.handleClose}>
+        <CloseRoundedIcon fontSize="medium" />
+      </s.CloseButton>
+    </>
   );
 }
--- src/components/RedirectionNotificationModal.js
+++ src/components/RedirectionNotificationModal.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom/cjs/react-router-dom";
 import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 import * as s from "../components/RedirectionNotificationModal.sc";
 import { isMobile } from "../utils/misc/browserDetection";
+import RedirectionNotificationForDesktop from "./RedirectionNotificationForDesktop";
 
 //This modal is used in the ArticlePreview component
 
@@ -52,72 +53,17 @@ export default function RedirectionNotificationModal({
     <Modal open={open} onClose={handleClose}>
       <s.ModalWrapper>
         {!isMobile() ? (
-          // Displayed to the users who access Zeeguu from desktop browsers
-          <>
-            <s.Header>
-              <h1>
-                You are ready to&nbsp;continue<br></br>
-                to the original article's website
-              </h1>
-            </s.Header>
-            <s.Body>
-              <p>
-                <strong>Once there</strong>, find and{" "}
-                <strong>
-                  click The Zeeguu Reader{" "}
-                  <s.Icon>
-                    <img
-                      className="fullDivWidthImage"
-                      alt=""
-                      src="../static/images/zeeguuLogo.svg"
-                    ></img>
-                  </s.Icon>{" "}
-                  icon
-                </strong>{" "}
-                in the top right corner of&nbsp;your browser's toolbar
-                or&nbsp;on&nbsp;the&nbsp;list of your installed extensions{" "}
-                <s.Icon>
-                  <img
-                    className="fullDivWidthImage"
-                    alt=""
-                    src="../static/images/puzzle.svg"
-                  ></img>
-                </s.Icon>
-                . <strong>Then&nbsp;select Read Article</strong>.
-              </p>
-              <img
-                className="fullDivWidthImage"
-                src={"../static/images/find-extension.png"}
-                alt="Zeeguu browser extension"
-              />
-            </s.Body>
-            <s.Footer>
-              <s.CheckboxWrapper>
-                <input
-                  onChange={handleVisibilityCheckboxSelection}
-                  checked={selectedDoNotShowRedirectionModal}
-                  type="checkbox"
-                  id="checkbox"
-                  name=""
-                  value=""
-                ></input>{" "}
-                <label htmlFor="checkbox">Don't show this message</label>
-              </s.CheckboxWrapper>
-              <a target="_blank" rel="noreferrer" href={article.url}>
-                {/* Clicking the GoToArticleButton button sends the reader
-                to the article, saves visibility preferences of the modal and closes it */}
-                <s.GoToArticleButton
-                  role="button"
-                  onClick={handleCloseAndSavePreferences}
-                >
-                  Enter the article's website
-                </s.GoToArticleButton>
-              </a>
-            </s.Footer>
-            <s.CloseButton role="button" onClick={handleClose}>
-              <CloseRoundedIcon fontSize="medium" />
-            </s.CloseButton>
-          </>
+            <RedirectionNotificationForDesktop
+              handleVisibilityCheckboxSelection={
+                handleVisibilityCheckboxSelection
+              }
+              selectedDoNotShowRedirectionModal={
+                selectedDoNotShowRedirectionModal
+              }
+              article={article}
+              handleCloseAndSavePreferences={handleCloseAndSavePreferences}
+              handleClose={handleClose}
+            />
         ) : (
           // Displayed to the users who access Zeeguu from mobile browsers
           //Todo: Create modal content components for these separate views
