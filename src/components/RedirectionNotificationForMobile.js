6864e38d8820c3659f5130e637a3a8003cd34bb8
iga waclawska <iga.waclawska@gmail.com>
2023-09-18 10:01:38 +0200

    create empty components for mobile and desktop version of RedirectionNotificationModal.js

---
+++ src/components/RedirectionNotificationForMobile.js
@@ -0,0 +1,9 @@
+import * as s from "./RedirectionNotificationModal.sc";
+
+export default function RedirectionNotificationForMobile(props) {
+  return (
+    <s.ModalWrapper>
+      <div>Hello from Redirection Notification For Mobile</div>
+    </s.ModalWrapper>
+  );
+}
