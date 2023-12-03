5479967245f62e7b2fcad1249202e24277785040
Darinka <dariinkagm@gmail.com>
2023-12-03 19:19:47 +0100

    code cleanup

--- src/reader/ReportBroken.js
+++ src/reader/ReportBroken.js
@@ -106,28 +106,3 @@ export default function ReportBroken({ api, articleID, UMR_SOURCE, history }) {
     </React.Fragment>
   );
 }
-
-// export default function ReportBroken({ api, articleID, UMR_SOURCE, history }) {
-//   function reportBroken(e) {
-//     let answer = prompt("What is wrong with the article?");
-//     if (answer) {
-//       let feedback = "broken_" + answer.replace(/ /g, "_");
-//       api.logReaderActivity(api.USER_FEEDBACK, articleID, feedback, UMR_SOURCE);
-//       setTimeout(() => history.push("/articles"), 500);
-//     }
-//   }
-
-//   return (
-//     <div style={{ marginLeft: '5px', marginRight: '5px' }}>
-//      <Chip
-//         label="Report broken article"
-//         component="a"
-//         onClick={reportBroken}
-//         variant="outlined"
-//         clickable
-//         color="warning"
-//         size="small"
-//         icon={<ReportProblemRoundedIcon />}/>
-//     </div>
-//   );
-// }
