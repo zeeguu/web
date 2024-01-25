d58aa14a653d59ef7f022245320ca4930ea6e9e2
Tiago Ribeiro <tfnribeiro@gmail.com>
2024-01-25 13:59:49 +0100

    Redifined defineExerciseSession
    It wasn't clear how batch counts and remaining exercises were being used, and when utilizing a higher number of exercises the logic would result in undefined exercises.
    
    Now calculateExerciseSequence takes a list of exercise types and iterates it from first to last, and adding it as long as there are enough bookmarks to fit that exercise. If there are too many bookmarks, it will start from 0 onwards, re-using exercises until the total number of bookmarks is used.
    
    Tested in both the Exercise tab and in the Article 'Practice Words'. They both work. In Practice words, a session will be the entire words that were translated.
    
    NUMBER_OF_BOOKMARKS_TO_PRACTICE  was changed to 10 as before this was the number of bookmarks used.

--- src/exercises/assignBookmarksToExercises.js
+++ src/exercises/assignBookmarksToExercises.js
@@ -1,23 +1,20 @@
-import {NUMBER_OF_BOOKMARKS_TO_PRACTICE} from "./exerciseSequenceTypes";
-
 /**
  * The bookmarks fetched by the API are assigned to the various exercises in the defined exercise session --
  * with the required amount of bookmarks assigned to each exercise and the first set of bookmarks set as
  * currentBookmarksToStudy to begin the exercise session.
  */
 function assignBookmarksToExercises(bookmarkList, exerciseSession) {
-    let k = 0;
+  let k = 0;
 
-    for (let session of exerciseSession) {
-        for (let i=0; i< session.requiredBookmarks; i++) {
-            session.bookmarks.push(bookmarkList[k + i]);
-        }
-        k+= session.requiredBookmarks;
+  for (let session of exerciseSession) {
+    for (let i = 0; i < session.requiredBookmarks; i++) {
+      session.bookmarks.push(bookmarkList[k + i]);
     }
+    k += session.requiredBookmarks;
+  }
 
-    return exerciseSession;
+  return exerciseSession;
 }
-
 
 /**
  * Calculates the exercise batches based on the amount of bookmarks received by the API and the amount of
@@ -27,75 +24,27 @@ function assignBookmarksToExercises(bookmarkList, exerciseSession) {
  *
  * @param bookmarks - passed to function assignBookmarksToExercises(bookmarks, exerciseSequence)
  */
-function calculateExerciseSequence(exerciseSequenceType, bookmarks) {
 
-    let bookmarksPerBatch = exerciseSequenceType.reduce(
-        (a, b) => a + b.requiredBookmarks,
-        0
-    );
-
-    let batchCount = parseInt(bookmarks.length / bookmarksPerBatch);
-    let remainingExercises = bookmarks.length % bookmarksPerBatch;
-
-    let exerciseSequence = defineExerciseSession(
-        exerciseSequenceType,
-        batchCount,
-        remainingExercises,
-        bookmarks.length
-    );
-    return exerciseSequence
-}
-
-
-
-
-function defineExerciseSession(exerciseTypesList, batches, rest, bookmark_count) {
-
-    let exerciseSession = [];
-    if (bookmark_count < NUMBER_OF_BOOKMARKS_TO_PRACTICE) {
-        let count = bookmark_count;
-        while (count > 0) {
-            for (let i = exerciseTypesList.length - 1; i >= 0; i--) {
-                let currentTypeRequiredCount = exerciseTypesList[i].requiredBookmarks;
-                if (count < currentTypeRequiredCount) continue;
-                if (count === 0) break;
-                let exercise = {
-                    type: exerciseTypesList[i].type,
-                    requiredBookmarks: currentTypeRequiredCount,
-                    bookmarks: [],
-                };
-                exerciseSession.push(exercise);
-                count = count - currentTypeRequiredCount;
-            }
-        }
-    } else {
-        for (let i = 0; i < batches; i++) {
-            for (let j = exerciseTypesList.length - 1; j >= 0; j--) {
-                let exercise = {
-                    type: exerciseTypesList[j].type,
-                    requiredBookmarks: exerciseTypesList[j].requiredBookmarks,
-                    bookmarks: [],
-                };
-                exerciseSession.push(exercise);
-            }
-        }
-        while (rest > 0) {
-            for (let k = exerciseTypesList.length - 1; k >= 0; k--) {
-                if (rest >= exerciseTypesList[k].requiredBookmarks) {
-                    let exercise = {
-                        type: exerciseTypesList[k].type,
-                        requiredBookmarks: exerciseTypesList[k].requiredBookmarks,
-                        bookmarks: [],
-                    };
-                    exerciseSession.push(exercise);
-                    rest--;
-                }
-            }
-        }
+function calculateExerciseSequence(exerciseTypesList, bookmark_count) {
+  let exerciseSession = [];
+  let exerciseType_i = 0;
+  while (bookmark_count > 0) {
+    if (bookmark_count >= exerciseTypesList[exerciseType_i].requiredBookmarks) {
+      let exercise = {
+        type: exerciseTypesList[exerciseType_i].type,
+        requiredBookmarks: exerciseTypesList[exerciseType_i].requiredBookmarks,
+        bookmarks: [],
+      };
+      exerciseSession.push(exercise);
+      bookmark_count -= exerciseTypesList[exerciseType_i].requiredBookmarks;
     }
-
-    return exerciseSession;
+    exerciseType_i++;
+    if (exerciseType_i === exerciseTypesList.length) exerciseType_i = 0;
+  }
+  return exerciseSession;
 }
 
-
-export {defineExerciseSession, assignBookmarksToExercises, calculateExerciseSequence}
+export {
+    calculateExerciseSequence,
+  assignBookmarksToExercises,
+};
