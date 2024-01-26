9aba6e5327b67a8df9242b55987d2936559e0a05
Mircea Filip Lungu <mircea.lungu@gmail.com>
2024-01-26 23:17:26 +0100

    should not send stuff to sentry when in dev mode

--- src/index.js
+++ src/index.js
@@ -16,11 +16,11 @@ Sentry.init({
     new Sentry.Replay(),
   ],
   // Performance Monitoring
-  tracesSampleRate: 1.0, // Capture 100% of the transactions
+  tracesSampleRate: 0.1, // Capture 100% of the transactions
   // Session Replay
   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-  enabled: process.env.NODE_ENV !== "development",
+  enabled: process.env.NODE_ENV !== "development", // cf: https://stackoverflow.com/a/70537824
 });
 
 ReactDOM.render(
