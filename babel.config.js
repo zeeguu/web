b596c98c2830a2a1c9a3bcd5797962ffe9f16f20
Mircea Filip Lungu <mircea.lungu@gmail.com>
2024-01-28 12:53:34 +0100

    formatting

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,7 @@
-module.exports ={
-    presets: [
-        ["@babel/preset-env", {targets: { esmodules: true}}],
-       ["@babel/preset-react", {runtime: "automatic"}]
-    ],
-    plugins: [
-        ["@babel/transform-runtime"]
-    ],
-}
+module.exports = {
+  presets: [
+    ["@babel/preset-env", { targets: { esmodules: true } }],
+    ["@babel/preset-react", { runtime: "automatic" }],
+  ],
+  plugins: [["@babel/transform-runtime"]],
+};
