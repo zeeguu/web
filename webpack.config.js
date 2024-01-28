943278954fd02c501a61ff2b996d38eb60ee083f
Mircea Filip Lungu <mircea.lungu@gmail.com>
2024-01-28 13:05:50 +0100

    formatting

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   module: {
     rules: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
-      {test: /\.svg$/, loader: 'svg-inline-loader'}
-    ]
+      { test: /\.js$/, loader: "babel-loader", exclude: /node_modules/ },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      { test: /\.svg$/, loader: "svg-inline-loader" },
+    ],
   },
   resolve: {
-      alias: {
-        react: path.resolve('./node_modules/react'),
-      },
-},
-}; 
+    alias: {
+      react: path.resolve("./node_modules/react"),
+    },
+  },
+};
