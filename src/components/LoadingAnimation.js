b8eee877d33ba50807eff5613384c919c30a4df4
Tiago Ribeiro <tfnribeiro@gmail.com>
2024-02-06 15:32:38 +0100

    Update LoadingAnimation.js
    Set a timeout so the loading screen doesn't get displayed when loading is less than 500ms

--- src/components/LoadingAnimation.js
+++ src/components/LoadingAnimation.js
@@ -1,18 +1,35 @@
 import * as s from "./LoadingAnimation.sc";
 import strings from "../i18n/definitions";
+import React from "react";
+import { useState, useEffect } from "react";
 
 export default function LoadingAnimation({ text }) {
   let _text = text ? text : strings.loadingMsg;
+  const [showLoadingScreen, setShowLoadingScreen] = useState(false);
+
+  useEffect(() => {
+    // Code from: https://stackoverflow.com/questions/53090432/react-hooks-right-way-to-clear-timeouts-and-intervals
+    // Only show the loading if more that 500ms have passed.
+    let loadingTimer = setTimeout(() => setShowLoadingScreen(true), 500);
+    return () => {
+      clearTimeout(loadingTimer);
+    };
+  }, []);
+
   return (
     <>
-      <s.LoadingAnimation>
-        <div className="lds-ellipsis">
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>
-      </s.LoadingAnimation>
+      {showLoadingScreen ? (
+        <s.LoadingAnimation>
+          <div className="lds-ellipsis">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </s.LoadingAnimation>
+      ) : (
+        <div></div>
+      )}
     </>
   );
 }
