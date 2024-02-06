fb005eaf878438880fab42d6adc445adc5665fa1
Tiago Ribeiro <tfnribeiro@gmail.com>
2024-02-06 15:19:48 +0100

    Added loading bars dots
    Code from: https://loading.io/css/

--- src/components/LoadingAnimation.js
+++ src/components/LoadingAnimation.js
@@ -1,7 +1,18 @@
 import * as s from "./LoadingAnimation.sc";
 import strings from "../i18n/definitions";
 
-export default function LoadingAnimation({text}) {
-    let _text = text ? text : strings.loadingMsg;
-    return <></>;
+export default function LoadingAnimation({ text }) {
+  let _text = text ? text : strings.loadingMsg;
+  return (
+    <>
+      <s.LoadingAnimation>
+        <div className="lds-ellipsis">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </s.LoadingAnimation>
+    </>
+  );
 }
