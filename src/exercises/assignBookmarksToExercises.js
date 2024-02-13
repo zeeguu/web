e07657febad158690fc62177faeef71c5c85dfd9
Tiago Ribeiro <tfnribeiro@gmail.com>
2024-02-13 10:51:56 +0100

    Update assignBookmarksToExercises.js
    Formatting changes + randomizing order of bookmarks

--- src/exercises/assignBookmarksToExercises.js
+++ src/exercises/assignBookmarksToExercises.js
@@ -3,35 +3,51 @@
  * with the required amount of bookmarks assigned to each exercise and the first set of bookmarks set as
  * currentBookmarksToStudy to begin the exercise session.
  */
-function assignBookmarksToExercises(bookmarks, exerciseTypesList) {
 
-    console.log("about to test:")
-    console.log(bookmarks);
+import shuffle from "../assorted/fisherYatesShuffle";
 
-    let exerciseSequence = [];
-    let exerciseType_i = 0;
-    let bookmark_i = 0;
+const OrderStrategies = {
+  Default: 0,
+  Random: 1,
+};
 
-    while (bookmark_i < bookmarks.length) {
-        let currExRequiredBookmarks = exerciseTypesList[exerciseType_i].requiredBookmarks;
-
-        if (bookmark_i + currExRequiredBookmarks <= bookmarks.length) {
-            let exercise = {
-                type: exerciseTypesList[exerciseType_i].type,
-                bookmarks: bookmarks.slice(bookmark_i, bookmark_i + currExRequiredBookmarks)
-            };
-            exerciseSequence.push(exercise);
-            bookmark_i += currExRequiredBookmarks;
-        }
-        exerciseType_i++;
-        if (exerciseType_i === exerciseTypesList.length) exerciseType_i = 0;
-    }
-    return exerciseSequence;
-
+function reorderSequence(originalSequence, method) {
+  switch (method) {
+    case OrderStrategies.Random:
+      return shuffle([...originalSequence]);
+    case OrderStrategies.Default:
+    default:
+      return originalSequence;
+  }
 }
 
+function assignBookmarksToExercises(bookmarks, exerciseTypesList) {
+  let exerciseSequence = [];
+  let exerciseType_i = 0;
+  let bookmark_i = 0;
 
-export {
+  // Re-order the bookmarks from the API
+  bookmarks = reorderSequence(bookmarks, OrderStrategies.Random);
 
-    assignBookmarksToExercises,
-};
+  while (bookmark_i < bookmarks.length) {
+    let currExRequiredBookmarks =
+      exerciseTypesList[exerciseType_i].requiredBookmarks;
+
+    if (bookmark_i + currExRequiredBookmarks <= bookmarks.length) {
+      let exercise = {
+        type: exerciseTypesList[exerciseType_i].type,
+        bookmarks: bookmarks.slice(
+          bookmark_i,
+          bookmark_i + currExRequiredBookmarks,
+        ),
+      };
+      exerciseSequence.push(exercise);
+      bookmark_i += currExRequiredBookmarks;
+    }
+    exerciseType_i++;
+    if (exerciseType_i === exerciseTypesList.length) exerciseType_i = 0;
+  }
+  return exerciseSequence;
+}
+
+export { assignBookmarksToExercises };
