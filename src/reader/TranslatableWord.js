81f88bee43bc5b285469e3df6c8894431543f99a
merleschoen <merle.schoen@hotmail.de>
2024-03-11 15:58:50 +0100

    updated hints

--- src/reader/TranslatableWord.js
+++ src/reader/TranslatableWord.js
@@ -65,7 +65,7 @@ export default function TranslatableWord({
   if (!word.translation || isBookmarkWord) {
     return (
       <>
-        <z-tag onClick={(e) => clickOnWord(e, word)} style={isUnderlineWord ? {textDecoration: "underline"} : {}}>{word.word}</z-tag>
+        <z-tag onClick={(e) => clickOnWord(e, word)} style={isUnderlineWord ? {fontWeight: "bold"} : {}}>{word.word}</z-tag>
         <span> </span>
       </>
     );
