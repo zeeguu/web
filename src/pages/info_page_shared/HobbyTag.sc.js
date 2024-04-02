c6c96db748fa6e0e8ff48f4aaa65869a53406c7e
iga waclawska <iga.waclawska@gmail.com>
2024-04-02 14:55:42 +0200

    adapt for mobile devices

--- src/pages/info_page_shared/HobbyTag.sc.js
+++ src/pages/info_page_shared/HobbyTag.sc.js
@@ -4,14 +4,14 @@ import { almostBlack } from "../../components/colors";
 import { zeeguuOrange } from "../../components/colors";
 
 const HobbyTag = styled.button`
-  flex: 1;
-  max-width: 8rem;
+  /* flex: 1; */
+  /* max-width: 8rem; */
   display: flex;
   flex-direction: row;
   justify-content: center;
   font-size: 1rem;
   line-height: 150%;
-  font-weight: 500;
+  font-weight: 600;
   margin: 0.5rem;
   border: 1px solid ${lightGrey};
   color: ${almostBlack};
@@ -20,14 +20,16 @@ const HobbyTag = styled.button`
   background: none;
   cursor: pointer;
 
-  &:hover {
-    border: 1px solid ${zeeguuOrange};
-  }
-
   &.active {
     border: 1px solid ${zeeguuOrange};
     background-color: ${zeeguuOrange};
     color: white;
+  }
+
+  @media (max-width: 768px) {
+    font-size: 0.9rem;
+    margin: 0.4rem;
+    padding: 0.75rem 1rem;
   }
 `;
 
