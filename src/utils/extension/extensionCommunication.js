e9202974c82cfd16c88f25b2bfeb5797afe70c03
Tiago Ribeiro <tfnribeiro@gmail.com>
2024-04-03 14:00:15 +0200

    Update extensionCommunication.js

--- src/utils/extension/extensionCommunication.js
+++ src/utils/extension/extensionCommunication.js
@@ -2,7 +2,7 @@ function checkExtensionInstalled(setHasExtension) {
   window.addEventListener("message", function (event) {
     if (
       event.source == window &&
-      event.data.message === "CONFIRM_FLAG" &&
+      event.data.message === "CONFIRM_EXTENSION" &&
       event.data.source === "EXT"
     ) {
       setHasExtension(true);
@@ -11,7 +11,7 @@ function checkExtensionInstalled(setHasExtension) {
   window.postMessage(
     {
       source: "APP",
-      message: "REQUEST_FLAG",
+      message: "REQUEST_EXTENSION",
     },
     "*",
   );
