f775ea202c16c6ee6f60ba187f0248e6512aeeec
merleschoen <merle.schoen@hotmail.de>
2024-04-05 11:46:05 +0200

    implemented logic for bookmark learning_cycle

--- src/exercises/assignBookmarksToExercises.js
+++ src/exercises/assignBookmarksToExercises.js
@@ -1,3 +1,5 @@
+import { filter } from "lodash";
+
 /**
  * The bookmarks fetched by the API are assigned to the various exercises in the defined exercise session --
  * with the required amount of bookmarks assigned to each exercise and the first set of bookmarks set as
@@ -7,29 +9,63 @@ function assignBookmarksToExercises(bookmarks, exerciseTypesList) {
   console.log("about to test:");
   console.log(bookmarks);
 
+  const learningCycleEnum = Object.freeze({
+    0: "not set",
+    1: "receptive",
+    2: "productive",
+  });
+
   let exerciseSequence = [];
   let exerciseType_i = 0;
   let bookmark_i = 0;
 
-  while (bookmark_i < bookmarks.length) {
-    let currExRequiredBookmarks =
-      exerciseTypesList[exerciseType_i].requiredBookmarks;
+  const hasLearningCycle = exerciseTypesList.some(exercise => 'learningCycle' in exercise);
 
-    if (bookmark_i + currExRequiredBookmarks <= bookmarks.length) {
-      let exercise = {
-        type: exerciseTypesList[exerciseType_i].type,
-        bookmarks: bookmarks.slice(
-          bookmark_i,
-          bookmark_i + currExRequiredBookmarks,
-        ),
-      };
-      exerciseSequence.push(exercise);
-      bookmark_i += currExRequiredBookmarks;
+  if (hasLearningCycle) {
+    for (let i = 0; i < bookmarks.length; i++) {
+      // Filter the exercises based on the learning_cycle attribute of the bookmark
+      let filteredExercises = exerciseTypesList.filter(exercise => 
+        learningCycleEnum[bookmarks[i].learning_cycle] === exercise.learningCycle
+      );
+      // Randomly select an exercise from the filtered list
+      let selectedExercise = filteredExercises[Math.floor(Math.random() * filteredExercises.length)];
+      console.log(filteredExercises);
+      // Check if there are enough bookmarks for the selected exercise
+      if (i + selectedExercise.requiredBookmarks <= bookmarks.length) {
+        // Assign the required number of bookmarks to the selected exercise
+        let exercise = {
+          type: selectedExercise.type,
+          bookmarks: bookmarks.slice(i, i + selectedExercise.requiredBookmarks),
+        };
+        exerciseSequence.push(exercise);
+
+        // Skip the assigned bookmarks
+        i += selectedExercise.requiredBookmarks - 1;
+      }
     }
-    exerciseType_i++;
-    if (exerciseType_i === exerciseTypesList.length) exerciseType_i = 0;
+    return exerciseSequence;
+
+  } else {
+    while (bookmark_i < bookmarks.length) {
+      let currExRequiredBookmarks =
+        exerciseTypesList[exerciseType_i].requiredBookmarks;
+
+      if (bookmark_i + currExRequiredBookmarks <= bookmarks.length) {
+        let exercise = {
+          type: exerciseTypesList[exerciseType_i].type,
+          bookmarks: bookmarks.slice(
+            bookmark_i,
+            bookmark_i + currExRequiredBookmarks,
+          ),
+        };
+        exerciseSequence.push(exercise);
+        bookmark_i += currExRequiredBookmarks;
+      }
+      exerciseType_i++;
+      if (exerciseType_i === exerciseTypesList.length) exerciseType_i = 0;
+    }
+    return exerciseSequence;
   }
-  return exerciseSequence;
 }
 
 export { assignBookmarksToExercises };
--- src/utils/preprocessing/preprocessing.js
+++ src/utils/preprocessing/preprocessing.js
@@ -3,8 +3,14 @@ function removePunctuation(string) {
     return string.replace(regex, "");
 }
 
+// so punctation isn't removed when it is part of a word, e.g. "l'Italie" or "it's"
+function removePunctuationFollowedBySpace(string) {
+    let regex = /[¡!"”“„#$%&'(–)*+,…./—:;«<=>»¿?@[\]^_`{|}~](?=\s|$)/g;
+    return string.replace(regex, "");
+}
+
 function tokenize(sentence) {
     return sentence.split(" ")
 }
 
-export {tokenize, removePunctuation}
+export {tokenize, removePunctuation, removePunctuationFollowedBySpace}
