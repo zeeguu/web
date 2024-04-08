ab3d9aa3b2ca2dee12fac81b2585f2ced873b7fa
merleschoen <merle.schoen@hotmail.de>
2024-04-08 21:17:29 +0200

    implementation of words lists for receptive and productive knowledge

--- src/words/_WordsRouter.js
+++ src/words/_WordsRouter.js
@@ -8,6 +8,8 @@ import TopTabs from "../components/TopTabs";
 import strings from "../i18n/definitions";
 import {Switch} from "react-router-dom";
 import WordsForArticle from "./WordsForArticle";
+import Receptive from "./Receptive";
+import Productive from "./Productive";
 
 export default function WordsRouter({api}) {
     return (
@@ -28,6 +30,8 @@ export default function WordsRouter({api}) {
                     title={strings.yourWordsHeadline}
                     tabsAndLinks={{
                         "Top Words": "/words",
+                        [strings.titleReceptiveWords]: "/words/receptive",
+                        [strings.titleProductiveWords]: "/words/productive",
                         [strings.learned]: "/words/learned",
                         // [strings.starred]: "/words/starred",
                     }}
@@ -44,6 +48,10 @@ export default function WordsRouter({api}) {
 
                 <PrivateRoute exact path="/words" api={api} component={Top}/>
                 <PrivateRoute exact path="/render/words" api={api} component={Top}/>
+                <PrivateRoute exact path="/words/receptive" api={api} component={Receptive}/>
+                <PrivateRoute exact path="/render/words/receptive" api={api} component={Receptive}/>
+                <PrivateRoute exact path="/words/productive" api={api} component={Productive}/>
+                <PrivateRoute exact path="/render/words/productive" api={api} component={Productive}/>
             </s.NarrowColumn>
         </Switch>
     );
