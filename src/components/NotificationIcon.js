9288c82cac334c5c67f5a2801583e609b8472213
Tiago Ribeiro <tfnribeiro@gmail.com>
2024-04-19 13:21:18 +0200

    Remove unnecessary imports

--- src/components/NotificationIcon.js
+++ src/components/NotificationIcon.js
@@ -1,7 +1,5 @@
 import * as s from "./NotificationIcon.sc";
-import strings from "../i18n/definitions";
 import React from "react";
-import { useState, useEffect } from "react";
 
 export default function NotificationIcon({ text }) {
   return (
