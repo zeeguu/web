dd6f49e5f07dfcd4dfb4503d6bc81172bae364d4
Mircea Filip Lungu <mircea.lungu@gmail.com>
2024-04-25 15:48:05 +0200

    @merleschoen: Receptive words are not filtered from top words but rather from bookmarksToStudy. very likely the same should be the case in Productive.

--- src/words/Receptive.js
+++ src/words/Receptive.js
@@ -7,12 +7,14 @@ import Word from "./Word";
 import * as s from "../components/TopMessage.sc";
 import { UMR_SOURCE } from "../reader/ArticleReader";
 
-export default function Receptive ({ api }) {
+export default function Receptive({ api }) {
   const [words, setWords] = useState(null);
 
   useEffect(() => {
-    api.topBookmarks(300, (topWords) => {
-      const receptiveWords = topWords.filter((word) => word.learning_cycle === 1);
+    api.getUserBookmarksToStudy(300, (bookmarks) => {
+      const receptiveWords = bookmarks.filter(
+        (word) => word.learning_cycle === 1,
+      );
       setWords(receptiveWords);
     });
     setTitle(strings.titleReceptiveWords);
@@ -24,16 +26,16 @@ export default function Receptive ({ api }) {
 
   return (
     <>
-       <s.TopMessage>
-            <img 
-              src="/static/icons/receptive-icon.png" 
-              alt="Productive Icon" 
-              style={{height: '2.5em', width: '2.5em', margin: '0.5em'}}
-            />
-            {strings.receptiveMsg}
-        </s.TopMessage>
+      <s.TopMessage>
+        <img
+          src="/static/icons/receptive-icon.png"
+          alt="Productive Icon"
+          style={{ height: "2.5em", width: "2.5em", margin: "0.5em" }}
+        />
+        {strings.receptiveMsg}
+      </s.TopMessage>
       {words.map((each) => (
-        <Word key={each.id} bookmark={each} api={api} source={UMR_SOURCE}/>
+        <Word key={each.id} bookmark={each} api={api} source={UMR_SOURCE} />
       ))}
     </>
   );
