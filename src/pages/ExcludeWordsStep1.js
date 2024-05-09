fe719190a348323a3c92aadb22b551d8a452431a
iga waclawska <iga.waclawska@gmail.com>
2024-05-09 15:07:48 +0200

    create ExcludeWords initial pages

---
+++ src/pages/ExcludeWordsStep1.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ExcludeWordsStep1() {
+  return <div>Hello From Exclude Words Step1</div>;
+}
---
+++ src/pages/ExcludeWordsStep2.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ExcludeWordsStep2() {
+  return <div>Hello From Exclude Words Step2</div>;
+}
