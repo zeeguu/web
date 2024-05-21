3ea5f2fd70566de5cfdfa6b1ad2d37bd990e61ca
Tiago Ribeiro <tfnribeiro@gmail.com>
2024-05-21 13:16:08 +0200

    Move the button above context

--- src/exercises/exerciseTypes/spellWhatYouHear/SpellWhatYouHear.js
+++ src/exercises/exerciseTypes/spellWhatYouHear/SpellWhatYouHear.js
@@ -139,6 +139,14 @@ export default function SpellWhatYouHear({
 
       {!isCorrect && (
         <>
+          <s.CenteredRowTall>
+            <SpeakButton
+              bookmarkToStudy={bookmarkToStudy}
+              api={api}
+              styling="large"
+              parentIsSpeakingControl={isButtonSpeaking}
+            />
+          </s.CenteredRowTall>
           <div className="contextExample">
             <TranslatableText
               isCorrect={isCorrect}
@@ -148,15 +156,6 @@ export default function SpellWhatYouHear({
               bookmarkToStudy={bookmarksToStudy[0].from}
             />
           </div>
-          <s.CenteredRowTall>
-            <SpeakButton
-              bookmarkToStudy={bookmarkToStudy}
-              api={api}
-              styling="large"
-              parentIsSpeakingControl={isButtonSpeaking}
-            />
-          </s.CenteredRowTall>
-
           <BottomInput
             handleCorrectAnswer={handleCorrectAnswer}
             handleIncorrectAnswer={handleIncorrectAnswer}
