7cb588a4f5d2699c5ec70c1a3806b2032f5a8ed3
Tiago Ribeiro <tfnribeiro@gmail.com>
2024-05-27 16:14:27 +0200

    Add custom Delay to Loading

--- src/components/LoadingAnimation.js
+++ src/components/LoadingAnimation.js
@@ -3,14 +3,14 @@ import strings from "../i18n/definitions";
 import React from "react";
 import { useState, useEffect } from "react";
 
-export default function LoadingAnimation({ text }) {
+export default function LoadingAnimation({ text, delay = 1000 }) {
   let _text = text ? text : strings.loadingMsg;
   const [showLoadingScreen, setShowLoadingScreen] = useState(false);
 
   useEffect(() => {
     // Code from: https://stackoverflow.com/questions/53090432/react-hooks-right-way-to-clear-timeouts-and-intervals
     // Only show the loading if more that 1000ms have passed.
-    let loadingTimer = setTimeout(() => setShowLoadingScreen(true), 1000);
+    let loadingTimer = setTimeout(() => setShowLoadingScreen(true), delay);
     return () => {
       clearTimeout(loadingTimer);
     };
