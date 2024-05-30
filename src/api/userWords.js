03815255a78d4608041547c915ddc77f05af85a2
Tiago Ribeiro <tfnribeiro@gmail.com>
2024-05-30 11:21:28 +0200

    Update API Endpoints
    - Added a userPreference endpoint which will update the status with the user preferences.
    - Moved the prioritize bookmarks to the API, as it is updating the status of the bookmarks in the backend

--- src/api/userWords.js
+++ src/api/userWords.js
@@ -1,4 +1,6 @@
 import { Zeeguu_API } from "./classDef";
+import { MAX_BOOKMARKS_PER_ARTICLE } from "../exercises/ExerciseConstants";
+import { USER_WORD_PREFERENCE } from "../words/userBookmarkPreferences.js";
 
 Zeeguu_API.prototype.getBookmarksByDay = function (callback) {
   this._getJSON("bookmarks_by_day/with_context", callback);
@@ -55,4 +57,69 @@ Zeeguu_API.prototype.setIsFitForStudy = function (bookmark_id) {
 
 Zeeguu_API.prototype.setNotFitForStudy = function (bookmark_id) {
   this._post(`not_fit_for_study/${bookmark_id}`);
+};
+
+Zeeguu_API.prototype.userSetForExercises = function (bookmark_id) {
+  this._post(`is_user_preference/${bookmark_id}`);
+};
+
+Zeeguu_API.prototype.userSetNotForExercises = function (bookmark_id) {
+  this._post(`not_user_preference/${bookmark_id}`);
+};
+
+Zeeguu_API.prototype.prioritizeBookmarksToStudy = function (
+  articleID,
+  setUpdatedBookmarks,
+) {
+  this.bookmarksForArticle(articleID, (bookmarks) => {
+    let seenBookmarks = new Set([]);
+    let sortedBookmarks = [...bookmarks].sort(
+      (bookmark_a, bookmark_b) =>
+        bookmark_b.origin_importance - bookmark_a.origin_importance,
+    );
+    let totalAddedBookmarks = 0;
+    for (let i = 0; i < sortedBookmarks.length; i++) {
+      let bookmark = sortedBookmarks[i];
+
+      let isRepeatedBookmark = seenBookmarks.has(bookmark.from.toLowerCase());
+      let isBookmarkTooLong = bookmark.from.split(" ").length >= 3;
+      let isUserExcludedBookmark =
+        bookmark.user_preference === USER_WORD_PREFERENCE.DONT_USE_IN_EXERCISES;
+      let isUserAddedBookmark =
+        bookmark.user_preference === USER_WORD_PREFERENCE.USE_IN_EXERCISES;
+      let isBookmarkLearned = bookmark.learned_datetime !== "";
+
+      if (isUserAddedBookmark && !isBookmarkLearned) {
+        bookmark.fit_for_study = true;
+        this.setIsFitForStudy(bookmark.id);
+        seenBookmarks.add(bookmark.from.toLowerCase());
+      } else if (isUserExcludedBookmark) {
+        bookmark.fit_for_study = false;
+        this.setNotFitForStudy(bookmark.id);
+      } else {
+        if (
+          !isBookmarkTooLong &&
+          !isRepeatedBookmark &&
+          !isBookmarkLearned &&
+          totalAddedBookmarks < MAX_BOOKMARKS_PER_ARTICLE
+        ) {
+          if (!bookmark.fit_for_study) {
+            this.setIsFitForStudy(bookmark.id);
+            bookmark.fit_for_study = true;
+          }
+          seenBookmarks.add(bookmark.from.toLowerCase());
+          totalAddedBookmarks += 1;
+        } else {
+          if (
+            bookmark.fit_for_study &&
+            (!bookmark.starred || isUserExcludedBookmark)
+          ) {
+            this.setNotFitForStudy(bookmark.id);
+            bookmark.fit_for_study = false;
+          }
+        }
+      }
+    }
+    if (setUpdatedBookmarks) setUpdatedBookmarks(bookmarks);
+  });
 };
