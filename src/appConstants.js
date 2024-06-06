7f1b5a72893de34d4069f62e8bc0d541592abaee
Tiago Ribeiro <tfnribeiro@gmail.com>
2024-06-06 15:08:26 +0200

    Update to allow expected domains

--- src/appConstants.js
+++ src/appConstants.js
@@ -1,5 +1,20 @@
-let APP_DOMAIN = window.location.origin || "https://www.zeeguu.org";
+let APP_DOMAIN = window.location.origin;
 let API_ENDPOINT = "https://api.zeeguu.org";
+
+/* 
+  This is done to allow Netlify previews to shown new
+  resources that are added in a PR.
+  We want to keep a possibility to point to another 
+  location, which can be done by setting the env variables.
+
+  In the Extension, we want to ensure we keep the default
+  to be the server, and for this reason we test the expected
+  domains, to evaluate if it's the extension being run somewhere
+  else.
+*/
+const isExpectedDomain = /netlify.app|zeeguu.org|localhost/g;
+
+if (!isExpectedDomain.test(APP_DOMAIN)) APP_DOMAIN = "https://www.zeeguu.org";
 
 try {
   if (typeof process.env !== "undefined") {
