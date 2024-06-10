d7e548782fbef82bfe8f30dbfd06b3d1d7aad206
Klara Krag <klak@itu.dk>
2024-06-10 15:52:06 +0200

    Moved the edits in search to its own components. Made keywords ready for functionality.

--- src/articles/FindArticles.sc.js
+++ src/articles/FindArticles.sc.js
@@ -1,9 +1,4 @@
 import styled from "styled-components";
-import {
-  almostBlack, 
-  zeeguuOrange, 
-  white
-} from "../components/colors";
 
 const MaterialSelection = styled.div`
   display: flex;
@@ -14,49 +9,6 @@ const MaterialSelection = styled.div`
   }
 `;
 
-const HeadlineSearch = styled.div`
-  h1{
-    color: ${almostBlack};
-    margin: 0;
-    font-size: 34px;
-    font-weight: bold;
-  }
-  p{
-    margin: 0;
-    color: ${zeeguuOrange};
-    font-size: 14px;
-    font-weight: bold;
-  }
-`;
-
-const RowHeadlineSearch = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  flex-wrap: wrap;
-`;
-
-let Keywords = styled.span`
-  display: flex;
-  margin-top: 1em;
-
-  
-
-  span {
-    height: 1.2em;
-    margin-left: 0.5em;
-    border: solid ${zeeguuOrange};
-    border-radius: 1.0416666666666667em;
-    padding: 0.20833333333333334em 1.3541666666666667em;
-    font-size: 0.8333333333333334em;
-    @media (max-width: 500px) {
-    display: 'none';
-  }
-  }
-`;
-
 export { 
   MaterialSelection,
-  HeadlineSearch,
-  RowHeadlineSearch,
-  Keywords };
+  };
