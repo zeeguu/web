ef6e650955a4446c237a05533319de699b46e1e4
Klara Krag <klak@itu.dk>
2024-06-20 12:19:22 +0200

    MySearches functionalities fixed. Shows five articles pr. search. SavedSearches is not fetched from api yet.

---
+++ src/articles/MySearches.sc.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import {
+  almostBlack
+} from "../components/colors";
+
+const HeadlineSavedSearches = styled.div`
+    color: ${almostBlack};
+    margin: 0;
+    font-size: 34px;
+    font-weight: bold;
+`;
+
+export {HeadlineSavedSearches};
