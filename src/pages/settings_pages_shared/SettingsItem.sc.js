feff81c1f5c8ad74e36089198401c53787b5bc3a
iga waclawska <iga.waclawska@gmail.com>
2024-08-15 14:26:36 +0200

    create SettingsItem navigation component and ListOfSettingsItems component

---
+++ src/pages/settings_pages_shared/SettingsItem.sc.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import { lightGrey } from "../../components/colors";
+
+const SettingsItem = styled.li`
+  .settings-link {
+    color: inherit;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    padding: 1rem;
+    font-size: 1rem;
+    font-weight: 500;
+  }
+
+  box-sizing: border-box;
+  display: flex;
+  list-style-type: none;
+  border: 2px solid ${lightGrey};
+  height: 56px;
+  width: 100%;
+  cursor: pointer;
+  /* margin-left: auto;
+  margin-right: auto; */
+  &:hover {
+    background-color: #f6f6f6;
+  }
+  &:active {
+    background-color: #ededed;
+  }
+`;
+
+export { SettingsItem };
