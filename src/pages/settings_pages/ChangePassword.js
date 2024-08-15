e1a6ebd304a30df221b0b836601be3406311a345
iga waclawska <iga.waclawska@gmail.com>
2024-08-15 16:20:20 +0200

    add return navLinks to the individual settings pages

--- src/pages/settings_pages/ChangePassword.js
+++ src/pages/settings_pages/ChangePassword.js
@@ -1,5 +1,13 @@
-import React from "react";
+import ArrowBackRoundedIcon from "@mui/icons-material/ArrowBackRounded";
+import { NavLink } from "react-router-dom";
 
 export default function ChangePassword() {
-  return <div>Change Password</div>;
+  return (
+    <div>
+      <NavLink to="/account_settings/options">
+        <ArrowBackRoundedIcon />
+      </NavLink>{" "}
+      Change Password
+    </div>
+  );
 }
--- src/pages/settings_pages/Interests.js
+++ src/pages/settings_pages/Interests.js
@@ -1,5 +1,13 @@
-import React from "react";
+import ArrowBackRoundedIcon from "@mui/icons-material/ArrowBackRounded";
+import { NavLink } from "react-router-dom";
 
 export default function Interests() {
-  return <div>Interests</div>;
+  return (
+    <div>
+      <NavLink to="/account_settings/options">
+        <ArrowBackRoundedIcon />
+      </NavLink>{" "}
+      Interests
+    </div>
+  );
 }
