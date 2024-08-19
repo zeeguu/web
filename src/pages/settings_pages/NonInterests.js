4c80b5ae1c02ae8111d87878dc8121a0d886edcc
iga waclawska <iga.waclawska@gmail.com>
2024-08-19 10:38:45 +0200

    implement settings logic for interests exclusion

--- src/pages/settings_pages/NonInterests.js
+++ src/pages/settings_pages/NonInterests.js
@@ -1,13 +1,29 @@
 import ArrowBackRoundedIcon from "@mui/icons-material/ArrowBackRounded";
 import { NavLink } from "react-router-dom";
+import Tag from "../info_page_shared/Tag";
+import TagContainer from "../info_page_shared/TagContainer";
+import useUnwantedContentPreferences from "../../hooks/useUnwantedContentPreferences";
 
-export default function NonInterests() {
+export default function NonInterests({ api }) {
+  const { topicsAvailableForExclusion, toggleTopicExclusion, isExcludedTopic } =
+    useUnwantedContentPreferences(api);
   return (
     <div>
       <NavLink to="/account_settings/options">
         <ArrowBackRoundedIcon />
       </NavLink>{" "}
       Non Interests
+      <TagContainer>
+        {topicsAvailableForExclusion.map((topic) => (
+          <Tag
+            key={topic.id}
+            className={isExcludedTopic(topic) && "selected"}
+            onClick={() => toggleTopicExclusion(topic)}
+          >
+            {topic.title}
+          </Tag>
+        ))}
+      </TagContainer>
     </div>
   );
 }
