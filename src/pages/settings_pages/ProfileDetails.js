a78429e9aae37ed132d19ff5c98d6571ec8d5588
iga waclawska <iga.waclawska@gmail.com>
2024-08-19 13:36:55 +0200

    add settings logic for user profile details edition - first attempt

--- src/pages/settings_pages/ProfileDetails.js
+++ src/pages/settings_pages/ProfileDetails.js
@@ -1,13 +1,104 @@
 import ArrowBackRoundedIcon from "@mui/icons-material/ArrowBackRounded";
 import { NavLink } from "react-router-dom";
+import { useState, useEffect, useContext } from "react";
+import { UserContext } from "../../contexts/UserContext";
+import { saveUserInfoIntoCookies } from "../../utils/cookies/userInfo";
+import LocalStorage from "../../assorted/LocalStorage";
+import strings from "../../i18n/definitions";
 
-export default function ProfileDetails() {
+import LoadingAnimation from "../../components/LoadingAnimation";
+
+import * as s from "../../components/FormPage.sc";
+import * as scs from "../Settings.sc";
+
+export default function ProfileDetails({ api, setUser }) {
+  const [userDetails, setUserDetails] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [cefr, setCEFR] = useState("");
+
+  const user = useContext(UserContext);
+
+  useEffect(() => {
+    api.getUserDetails((data) => {
+      setUserDetails(data);
+      setCEFRlevel(data);
+    });
+  }, [user.session, api]);
+
+  function setCEFRlevel(data) {
+    const levelKey = data.learned_language + "_cefr_level";
+    const levelNumber = data[levelKey];
+    setCEFR("" + levelNumber);
+    setUserDetails({
+      ...data,
+      cefr_level: levelNumber,
+    });
+  }
+
+  function updateUserInfo(info) {
+    LocalStorage.setUserInfo(info);
+    setUser({
+      ...user,
+      name: info.name,
+    });
+
+    saveUserInfoIntoCookies(info);
+  }
+
+  function handleSave(e) {
+    e.preventDefault();
+    api.saveUserDetails(userDetails, setErrorMessage, () => {
+      updateUserInfo(userDetails);
+      //   if (history.length > 1) {
+      //     history.goBack();
+      //   } else {
+      //     window.close();
+      //   }
+    });
+  }
+
+  if (!userDetails) {
+    return <LoadingAnimation />;
+  }
+
   return (
     <div>
       <NavLink to="/account_settings/options">
         <ArrowBackRoundedIcon />
       </NavLink>{" "}
       Profile Details
+      <s.FormContainer>
+        <scs.StyledSettings>
+          <form className="formSettings">
+            <h5>{errorMessage}</h5>
+            <b>Profile Details</b>
+            <hr></hr>
+            <label>{strings.name}</label>
+            <input
+              name="name"
+              value={userDetails.name}
+              onChange={(e) =>
+                setUserDetails({ ...userDetails, name: e.target.value })
+              }
+            />
+            <br />
+
+            <label>{strings.email}</label>
+            <input
+              type="email"
+              value={userDetails.email}
+              onChange={(e) =>
+                setUserDetails({ ...userDetails, email: e.target.value })
+              }
+            />
+            <div>
+              <s.FormButton onClick={handleSave}>
+                <span>{strings.save}</span>
+              </s.FormButton>
+            </div>
+          </form>
+        </scs.StyledSettings>
+      </s.FormContainer>
     </div>
   );
 }
