f8cdf36b0a313759d213203ac21c7088bf8e5dde
Tiago Ribeiro <tfnribeiro@gmail.com>
2024-08-19 13:39:17 +0200

    Allow a user to edit a word into an expression

--- src/utils/preprocessing/preprocessing.js
+++ src/utils/preprocessing/preprocessing.js
@@ -1,3 +1,5 @@
+import isBookmarkExpression from "../misc/isBookmarkExpression";
+
 // doesn't remove punctuation when it is part of a word, e.g. "l'Italie" or "it's"
 function removePunctuation(string) {
   let removeLeadingPunctuation =
@@ -13,10 +15,17 @@ function tokenize(sentence) {
   return sentence.split(" ");
 }
 
+function isExpression(word) {
+  return word.includes(" ");
+}
+
 function isWordInSentence(word, sentence) {
+  if (isExpression(word)) {
+    return sentence.includes(word);
+  }
   let tokens = tokenize(sentence);
   tokens = tokens.map((each) => removePunctuation(each));
   return tokens.includes(removePunctuation(word));
 }
 
-export { tokenize, removePunctuation, isWordInSentence };
+export { tokenize, removePunctuation, isWordInSentence, isExpression };
