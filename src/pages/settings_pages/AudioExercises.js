079cda85b7bb4bfbca34b3e64a2f21d065c5f0b6
iga waclawska <iga.waclawska@gmail.com>
2024-08-19 15:08:31 +0200

    add settings logic for audio exercises - first attempt

--- src/pages/settings_pages/AudioExercises.js
+++ src/pages/settings_pages/AudioExercises.js
@@ -1,13 +1,108 @@
 import ArrowBackRoundedIcon from "@mui/icons-material/ArrowBackRounded";
 import { NavLink } from "react-router-dom";
+import { useState, useEffect, useContext } from "react";
+import { UserContext } from "../../contexts/UserContext";
+import SessionStorage from "../../assorted/SessionStorage";
+import LocalStorage from "../../assorted/LocalStorage";
+import Feature from "../../features/Feature";
 
-export default function AudioExercises() {
+import strings from "../../i18n/definitions";
+
+import * as s from "../../components/FormPage.sc";
+import * as scs from "../Settings.sc";
+
+export default function AudioExercises({ api }) {
+  const user = useContext(UserContext);
+
+  const [errorMessage, setErrorMessage] = useState("");
+  const [audioExercises, setAudioExercises] = useState(true);
+  let preferenceNotSet =
+    LocalStorage.getProductiveExercisesEnabled() === undefined;
+
+  const [productiveExercises, setProductiveExercises] = useState(
+    preferenceNotSet || LocalStorage.getProductiveExercisesEnabled(),
+  );
+
+  useEffect(() => {
+    api.getUserPreferences((preferences) => {
+      setAudioExercises(
+        (preferences["audio_exercises"] === undefined ||
+          preferences["audio_exercises"] === "true") &&
+          SessionStorage.isAudioExercisesEnabled(),
+      );
+    });
+  }, [user.session, api]);
+
+  function handleAudioExercisesChange(e) {
+    setAudioExercises((state) => !state);
+  }
+
+  function handleProductiveExercisesChange(e) {
+    // Toggle the state locally
+    setProductiveExercises((state) => !state);
+
+    // Update local storage
+    const newProductiveValue = !productiveExercises;
+    localStorage.setItem(
+      "productiveExercisesEnabled",
+      JSON.stringify(newProductiveValue),
+    );
+  }
+
+  function handleSave(e) {
+    e.preventDefault();
+
+    SessionStorage.setAudioExercisesEnabled(audioExercises);
+    api.saveUserPreferences({
+      audio_exercises: audioExercises,
+      productive_exercises: productiveExercises,
+    });
+  }
+
   return (
     <div>
       <NavLink to="/account_settings/options">
         <ArrowBackRoundedIcon />
       </NavLink>{" "}
       Audio Exercises
+      <s.FormContainer>
+        <scs.StyledSettings>
+          <h5>{errorMessage}</h5>
+          <form className="formSettings">
+            <label>Exercise Type Preferences</label>
+            <div style={{ display: "flex" }} className="form-group">
+              <input
+                style={{ width: "1.5em" }}
+                type={"checkbox"}
+                checked={audioExercises}
+                onChange={handleAudioExercisesChange}
+              />
+              <label>
+                Include Audio Exercises{" "}
+                {SessionStorage.isAudioExercisesEnabled()
+                  ? ""
+                  : "(Temporaly Disabled)"}
+              </label>
+            </div>
+            {Feature.merle_exercises() && (
+              <div style={{ display: "flex" }} className="form-group">
+                <input
+                  style={{ width: "1.5em" }}
+                  type={"checkbox"}
+                  checked={productiveExercises}
+                  onChange={handleProductiveExercisesChange}
+                />
+                <label>Enable Productive Exercises</label>
+              </div>
+            )}
+            <div>
+              <s.FormButton onClick={handleSave}>
+                <span>{strings.save}</span>
+              </s.FormButton>
+            </div>
+          </form>
+        </scs.StyledSettings>
+      </s.FormContainer>
     </div>
   );
 }
