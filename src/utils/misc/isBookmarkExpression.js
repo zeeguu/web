b4e6cba4891fecd3bbffb181691286f0a57f1508
Tiago Ribeiro <tfnribeiro@gmail.com>
2024-08-21 10:19:41 +0200

    476 when i forget the indefinite article (#484)
    * Provide feedback on expressions
    * Changed hint text
    * Check only when there is a single word
    - Only trigger in the case the user only typed one word
    * renamed /util/preprocessing to /util/text
    
    Co-authored-by: Mircea Filip Lungu <mircea.lungu@gmail.com>

--- src/utils/misc/isBookmarkExpression.js
+++ src/utils/misc/isBookmarkExpression.js
@@ -1,4 +1,4 @@
-import { isExpression } from "../preprocessing/preprocessing";
+import { isExpression } from "../text/expressions";
 
 export default function isBookmarkExpression(bookmark) {
   return isExpression(bookmark.from);
