00809ac8c6635c7a984ba4f508c1d9463be32b94
iga waclawska <iga.waclawska@gmail.com>
2024-08-22 14:32:35 +0200

    add styling to the back arrow

---
+++ src/pages/settings_pages_shared/BackArrow.sc.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import { darkGrey, veryDarkGrey } from "../../components/colors";
+
+const BackArrow = styled.button`
+  width: fit-content;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  cursor: pointer;
+  gap: 0.25rem;
+  color: ${darkGrey};
+  background: inherit;
+  border: inherit;
+  font-family: inherit;
+  font-size: inherit;
+  font-weight: 600;
+  &:hover {
+    color: ${veryDarkGrey};
+  }
+  &:active {
+    color: ${veryDarkGrey};
+  }
+`;
+
+export { BackArrow };
