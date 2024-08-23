a881fd1f86384f596f76d4ab570a947493b6693c
Tiago Ribeiro <tfnribeiro@gmail.com>
2024-08-23 11:21:12 +0200

    Resume articles only in Zeeguu
    - Currently this feature will only allow users to continue to read in Zeeguu, we might be able to support this later on in the extension.

--- src/articles/UnfinishedArticlePreview.js
+++ src/articles/UnfinishedArticlePreview.js
@@ -1,102 +1,21 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
-import { isMobile } from "../utils/misc/browserDetection";
 import * as s from "./ArticlePreview.sc";
-import RedirectionNotificationModal from "../components/redirect_notification/RedirectionNotificationModal";
-import Feature from "../features/Feature";
-
-export default function UnfinishedArticlePreview({
-  article,
-  hasExtension,
-  api,
-  doNotShowRedirectionModal_UserPreference,
-  setDoNotShowRedirectionModal_UserPreference,
-  onArticleClick,
-}) {
-  const [isRedirectionModalOpen, setIsRedirectionModaOpen] = useState(false);
-  const [isArticleSaved, setIsArticleSaved] = useState(
-    article.has_personal_copy,
-  );
-
+export default function UnfinishedArticlePreview({ article, onArticleClick }) {
   const handleArticleClick = () => {
     if (onArticleClick) {
       onArticleClick(article.id);
     }
   };
 
-  function handleCloseRedirectionModal() {
-    setIsRedirectionModaOpen(false);
-  }
-
-  function handleOpenRedirectionModal() {
-    setIsRedirectionModaOpen(true);
-  }
-
   function titleLink(article) {
     let linkToRedirect = `/read/article?id=${article.id}`;
     if (article.last_reading_percentage)
       linkToRedirect += `&percentage=${parseFloat(article.last_reading_percentage).toFixed(2)}`;
-    let open_in_zeeguu = (
+    return (
       <Link to={linkToRedirect} onClick={handleArticleClick}>
         {article.title}
       </Link>
     );
-
-    let open_externally_with_modal = (
-      //The RedirectionNotificationModal modal informs the user that they are about
-      //to be redirected to the original article's website and guides them on what steps
-      //should be taken to start reading the said article with The Zeeguu Reader extension
-      //The modal is displayed when the user clicks the article's title from the recommendation
-      //list and can be deactivated when they select "Do not show again" and proceed.
-      <>
-        <RedirectionNotificationModal
-          api={api}
-          hasExtension={hasExtension}
-          article={article}
-          open={isRedirectionModalOpen}
-          handleCloseRedirectionModal={handleCloseRedirectionModal}
-          setDoNotShowRedirectionModal_UserPreference={
-            setDoNotShowRedirectionModal_UserPreference
-          }
-          setIsArticleSaved={setIsArticleSaved}
-        />
-        <s.InvisibleTitleButton
-          onClick={() => {
-            handleArticleClick();
-            handleOpenRedirectionModal();
-          }}
-        >
-          {article.title}
-        </s.InvisibleTitleButton>
-      </>
-    );
-
-    let open_externally_without_modal = (
-      //allow target _self on mobile to easily go back to Zeeguu
-      //using mobile browser navigation
-      <a
-        target={isMobile ? "_self" : "_blank"}
-        rel="noreferrer"
-        href={article.url}
-        onClick={handleArticleClick}
-      >
-        {article.title}
-      </a>
-    );
-
-    let should_open_in_zeeguu =
-      article.video ||
-      (!Feature.extension_experiment1() && !hasExtension) ||
-      article.has_personal_copy ||
-      article.has_uploader ||
-      isArticleSaved === true;
-
-    let should_open_with_modal =
-      doNotShowRedirectionModal_UserPreference === false;
-
-    if (should_open_in_zeeguu) return open_in_zeeguu;
-    else if (should_open_with_modal) return open_externally_with_modal;
-    else return open_externally_without_modal;
   }
 
   return (
