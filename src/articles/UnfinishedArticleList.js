2bb20439dfb05a4ed82013b72e3f1a3facadfe00
Tiago Ribeiro <tfnribeiro@gmail.com>
2024-08-26 07:56:11 +0200

    Removed Unnecessary Extension Related Code

--- src/articles/UnfinishedArticleList.js
+++ src/articles/UnfinishedArticleList.js
@@ -1,47 +1,19 @@
 import React, { useState, useEffect, useContext } from "react";
 
-import ExtensionMessage from "./ExtensionMessage";
-import LocalStorage from "../assorted/LocalStorage";
 import { APIContext } from "../contexts/APIContext";
-import useExtensionCommunication from "../hooks/useExtensionCommunication";
 import * as s from "./UnfinishedArticleList.sc";
 import UnfinishedArticlePreview from "./UnfinishedArticlePreview";
 
 export default function UnfinishedArticlesList({}) {
   let api = useContext(APIContext);
 
-  const doNotShowRedirectionModal_LocalStorage =
-    LocalStorage.getDoNotShowRedirectionModal() === "true" ? true : false;
   const [unreadArticleList, setUnreadArticleList] = useState();
-  const [isExtensionAvailable] = useExtensionCommunication();
-  const [extensionMessageOpen, setExtensionMessageOpen] = useState(false);
-  const [displayedExtensionPopup, setDisplayedExtensionPopup] = useState(false);
-  const [
-    doNotShowRedirectionModal_UserPreference,
-    setDoNotShowRedirectionModal_UserPreference,
-  ] = useState(doNotShowRedirectionModal_LocalStorage);
 
   useEffect(() => {
-    LocalStorage.setDoNotShowRedirectionModal(
-      doNotShowRedirectionModal_UserPreference,
-    );
-  }, [doNotShowRedirectionModal_UserPreference]);
-
-  useEffect(() => {
-    setDisplayedExtensionPopup(LocalStorage.displayedExtensionPopup());
-    console.log(
-      "Localstorage displayed extension: " +
-        LocalStorage.displayedExtensionPopup(),
-    );
     api.getUnfinishedUserReadingSessions((articles) => {
       setUnreadArticleList(articles);
     });
   }, []);
-  useEffect(() => {
-    if (!isExtensionAvailable) {
-      setExtensionMessageOpen(true);
-    }
-  }, [isExtensionAvailable]);
 
   if (unreadArticleList === undefined || unreadArticleList.length == 0) {
     return <></>;
@@ -49,13 +21,6 @@ export default function UnfinishedArticlesList({}) {
 
   return (
     <>
-      <ExtensionMessage
-        open={extensionMessageOpen}
-        hasExtension={isExtensionAvailable}
-        displayedExtensionPopup={displayedExtensionPopup}
-        setExtensionMessageOpen={setExtensionMessageOpen}
-        setDisplayedExtensionPopup={setDisplayedExtensionPopup}
-      ></ExtensionMessage>
       <s.UnfinishedArticlesBox>
         <s.UnfishedArticleBoxTitle>
           Continue where you left off
@@ -64,14 +29,7 @@ export default function UnfinishedArticlesList({}) {
           <UnfinishedArticlePreview
             key={each.id}
             article={each}
-            hasExtension={isExtensionAvailable}
             api={api}
-            doNotShowRedirectionModal_UserPreference={
-              doNotShowRedirectionModal_UserPreference
-            }
-            setDoNotShowRedirectionModal_UserPreference={
-              setDoNotShowRedirectionModal_UserPreference
-            }
             onArticleClick={() => {
               api.logUserActivity(api.CLICKED_RESUME_ARTICLE, each.id, "", "");
             }}
