0f871b11800c8493dbe4305b75dcd38c29ec41dc
Tiago Ribeiro <tfnribeiro@gmail.com>
2024-08-26 08:54:54 +0200

    Extended Date Conversion to display when article was read

--- src/utils/misc/readableTime.js
+++ src/utils/misc/readableTime.js
@@ -1,3 +1,44 @@
+function secondsToMinutes(timeInSeconds) {
+  return Math.floor(timeInSeconds / 60);
+}
+function secondsToHours(timeInSeconds) {
+  return Math.floor(secondsToMinutes(timeInSeconds) / 60);
+}
+function secondsToDays(timeInSeconds) {
+  return Math.floor(secondsToHours(timeInSeconds) / 24);
+}
+
+function getHumanRoundedTime(timeInSeconds, precision = "seconds") {
+  // Currently supports precision: "seconds", "minutes", "hours", "days"
+  let valid_precision = ["seconds", "minutes", "hours", "days"];
+  if (!valid_precision.includes(precision))
+    throw `getHumanRoundedTime called with: '${precision}' it supports: ${valid_precision}`;
+  if (precision === "seconds")
+    return timeInSeconds + (timeInSeconds > 1 ? " seconds" : " second");
+  if (timeInSeconds < 60) {
+    if (precision === "minutes") return "< 1 minute";
+    else if (precision === "hours") return "< 1 hour";
+    else return "< 1 day";
+  } else {
+    let days = secondsToDays(timeInSeconds);
+    let hours = secondsToHours(timeInSeconds);
+    let minutes = secondsToMinutes(timeInSeconds);
+    if (precision === "minutes")
+      return minutes + (minutes > 1 ? " minutes" : " minute");
+    else if (precision === "hours") {
+      if (hours === 0) {
+        return "< 1 hour";
+      }
+      return hours + (hours > 1 ? " hours" : " hour");
+    } else {
+      if (days === 0) {
+        return "< 1 day";
+      }
+      return days + (days > 1 ? " days" : " day");
+    }
+  }
+}
+
 function timeToHumanReadable(timeInSeconds, precision = "seconds") {
   // Currently supports two precision: "seconds" and "minutes"
   if (timeInSeconds < 60) {
@@ -5,8 +46,8 @@ function timeToHumanReadable(timeInSeconds, precision = "seconds") {
       return timeInSeconds + (timeInSeconds > 1 ? " seconds" : " second");
     else return "< 1 minute";
   } else {
-    let minutes = Math.floor(timeInSeconds / 60);
     let seconds = timeInSeconds % 60;
+    let minutes = secondsToMinutes(timeInSeconds);
     let string = minutes + (minutes > 1 ? " minutes" : " minute");
     if (seconds > 0 && precision === "seconds")
       string += " " + seconds + (seconds > 1 ? " seconds" : " second");
@@ -19,4 +60,11 @@ function estimateReadingTime(wordCount) {
   // Let'say a language lerner takes 160 WPM + Wait + Translation Words
   return timeToHumanReadable(Math.ceil(wordCount / 160) * 60, "minutes");
 }
-export { timeToHumanReadable, estimateReadingTime };
+export {
+  secondsToMinutes,
+  secondsToHours,
+  secondsToDays,
+  getHumanRoundedTime,
+  timeToHumanReadable,
+  estimateReadingTime,
+};
