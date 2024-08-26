88f45cdc980651fb142381e9a0d2b8f317f4bddd
Tiago Ribeiro <tfnribeiro@gmail.com>
2024-08-26 08:54:54 +0200

    Extended Date Conversion to display when article was read

--- src/articles/UnfinishedArticlePreview.js
+++ src/articles/UnfinishedArticlePreview.js
@@ -1,5 +1,10 @@
 import { Link } from "react-router-dom";
 import * as s from "./ArticlePreview.sc";
+import {
+  secondsToMinutes,
+  secondsToHours,
+  getHumanRoundedTime,
+} from "../utils/misc/readableTime";
 export default function UnfinishedArticlePreview({ article, onArticleClick }) {
   const handleArticleClick = () => {
     if (onArticleClick) {
@@ -17,6 +22,15 @@ export default function UnfinishedArticlePreview({ article, onArticleClick }) {
       </Link>
     );
   }
+  function getDateString(article) {
+    let seconds = article.seconds_since_read;
+    if (secondsToMinutes(seconds) < 10) return "just now";
+    if (secondsToMinutes(seconds) < 61)
+      return getHumanRoundedTime(seconds, "minutes") + " ago";
+    if (secondsToHours(seconds) < 25)
+      return "about " + getHumanRoundedTime(seconds, "hours") + " ago";
+    return "about " + getHumanRoundedTime(seconds, "days") + " ago";
+  }
 
   return (
     <s.ArticlePreview>
@@ -26,7 +40,7 @@ export default function UnfinishedArticlePreview({ article, onArticleClick }) {
       </s.UnfinishedArticleContainer>
       <div>
         <s.UnfinishedArticleStats>
-          ({article.time_until_last_read},{" "}
+          ({getDateString(article)},{" "}
           {Math.round(article.last_reading_percentage * 100)}% read)
         </s.UnfinishedArticleStats>
       </div>
