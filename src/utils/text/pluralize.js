cd1663aed6f5073121c753ac70dad6c13c66e334
Mircea Filip Lungu <mircea.lungu@gmail.com>
2024-09-06 13:21:08 +0300

    one more method in Pluralize

--- src/utils/text/pluralize.js
+++ src/utils/text/pluralize.js
@@ -1,7 +1,10 @@
 class Pluralize {
-  // This method takes a number and returns the correct form of "has" or "have"
   static word(count) {
     return count === 1 ? "word" : "words";
+  }
+
+  static is(count) {
+    return count === 1 ? "is" : "are";
   }
 }
 
