17249325ceeaeac94479e178f90bf5cb5fd7f0dd
iga waclawska <iga.waclawska@gmail.com>
2024-09-11 10:43:19 +0200

    update the accent color from blue to dark orange

--- src/pages/Settings/Classrooms/LeaveClassroomModal.sc.js
+++ src/pages/Settings/Classrooms/LeaveClassroomModal.sc.js
@@ -1,9 +1,9 @@
 import styled from "styled-components";
-import { blue600 } from "../../../components/colors";
+import { zeeguuDarkOrange } from "../../../components/colors";
 
 const ColorAccent = styled.span`
   font-size: inherit;
-  color: ${blue600};
+  color: ${zeeguuDarkOrange};
 `;
 
 export { ColorAccent };
