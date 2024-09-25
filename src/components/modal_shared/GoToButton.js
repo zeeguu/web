7e742ee6023107a360d3ffd2be093d4b20b88080
iga waclawska <iga.waclawska@gmail.com>
2024-09-25 11:41:20 +0200

    remove the link wrapper from the GoToButton

--- src/components/modal_shared/GoToButton.js
+++ src/components/modal_shared/GoToButton.js
@@ -1,11 +1,9 @@
 import * as s from "./GoToButton.sc";
 
-export default function GoToButton({ href, target, onClick, children }) {
+export default function GoToButton({ onClick, children }) {
   return (
-    <a target={target} rel="noopener noreferrer" href={href} className="link">
-      <s.GoToButton role="button" onClick={onClick}>
-        {children}
-      </s.GoToButton>
-    </a>
+    <s.GoToButton role="button" onClick={onClick}>
+      {children}
+    </s.GoToButton>
   );
 }
