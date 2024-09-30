326b60b1e4a0d0692e1954b173025f0ccff33441
iga waclawska <iga.waclawska@gmail.com>
2024-09-30 11:40:47 +0200

    create a docstring for setTitle from assorted

--- src/assorted/setTitle.js
+++ src/assorted/setTitle.js
@@ -1,3 +1,14 @@
+/**
+ * Sets the document's title to the specified title followed by ": Zeeguu".
+ *
+ * @param {string} title - The title to set for the document.
+ * This title will be appended with ": Zeeguu".
+ *
+ * @example
+ * // Sets the document title to "Home: Zeeguu"
+ * setTitle("Home");
+ */
+
 export function setTitle(title) {
   document.title = title + ": Zeeguu";
 }
