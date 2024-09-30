0076ab4ea158b7ea4c23af0c785154ab0840bb85
iga waclawska <iga.waclawska@gmail.com>
2024-09-30 11:48:43 +0200

    improve the doc string

--- src/assorted/setTitle.js
+++ src/assorted/setTitle.js
@@ -4,6 +4,8 @@
  * @param {string} title - The title to set for the document.
  * This title will be appended with ": Zeeguu".
  *
+ * @returns {void} - This function does not return a value.
+ *
  * @example
  * // Sets the document title to "Home: Zeeguu"
  * setTitle("Home");
