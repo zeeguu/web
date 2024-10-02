6acb8a2cd280d3f98cecf70cf6cf3f07c9d58207
iga waclawska <iga.waclawska@gmail.com>
2024-10-02 08:59:50 +0200

    remove the Form.js components

--- src/pages/_pages_shared/Form.js
+++
@@ -1,9 +0,0 @@
-import * as s from "./Form.sc";
-
-export default function Form({ children, action, method }) {
-  return (
-    <s.Form action={action} method={method}>
-      {children}
-    </s.Form>
-  );
-}
