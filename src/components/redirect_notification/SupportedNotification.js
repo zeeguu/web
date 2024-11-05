3d7350ba22dcb6a5812fc450fcea6077a02b2a91
iga waclawska <iga.waclawska@gmail.com>
2024-11-05 12:30:36 +0100

    remove the paragraph from the redirect modal, and update the comment

--- src/components/redirect_notification/SupportedNotification.js
+++ src/components/redirect_notification/SupportedNotification.js
@@ -1,4 +1,3 @@
-import * as s from "../modal_shared/Modal.sc";
 import { useState, useEffect } from "react";
 import Modal from "../modal_shared/Modal";
 import Header from "../modal_shared/Header.sc";
@@ -21,8 +20,8 @@ export default function SupportedNotification({
   const [gifSrc, setGifSrc] = useState("enable-zeeguu.gif");
 
   useEffect(() => {
-    // When 'open' is true, sets the GIF source to a unique URL with a timestamp query parameter.
-    // This forces the browser to reload the GIF each time the modal is displayed,
+    // When 'open' is true, the useEffect hook sets the GIF source to a unique URL with a timestamp query parameter.
+    // This forces the browser to reload the GIF each time the modal is open,
     // preventing it from using a cached version and ensuring the GIF plays from the start.
     if (open) {
       setGifSrc(`enable-zeeguu.gif?timestamp=${new Date().getTime()}`);
@@ -60,10 +59,6 @@ export default function SupportedNotification({
         </Heading>
       </Header>
       <Main>
-        <p>
-          Once there, <s.Strong>right-click</s.Strong> anywhere on the page and
-          select the "Read with Zeeguu" option.
-        </p>
         <FullWidthImage src={gifSrc} alt={"Zeeguu browser extension"} />
       </Main>
       <Footer>
