25df96f988a716e7218223995882a3f4f5478b32
Tiago Ribeiro <tfnribeiro@gmail.com>
2024-11-11 09:44:44 +0100

    Added pagination to Saved Texts
    - Works similarly to the one used in FindArticles, by updating the list with extra articles until we do not get anything from the API.
    - Each call takes about 1.4s - 1.6s in my development environment (to fetch 20 articles). Before, this user had about 76 articles taking 6.3s to fetch all.

--- src/articles/OwnArticles.js
+++ src/articles/OwnArticles.js
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
+import useShadowRef from "../hooks/useShadowRef";
 import LoadingAnimation from "../components/LoadingAnimation";
 import { setTitle } from "../assorted/setTitle";
 import strings from "../i18n/definitions";
+import { getPixelsFromScrollBarToEnd } from "../utils/misc/getScrollLocation";
 
 import ArticlePreview from "./ArticlePreview";
 
@@ -12,14 +14,63 @@ import * as s from "../components/TopMessage.sc";
 export default function OwnArticles({ api }) {
   const [articleList, setArticleList] = useState(null);
   const [originalList, setOriginalList] = useState(null);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [isWaitingForNewArticles, setIsWaitingForNewArticles] = useState(false);
+  const [noMoreArticlesToShow, setNoMoreArticlesToShow] = useState(false);
+
+  const articleListRef = useShadowRef(articleList);
+  const currentPageRef = useShadowRef(currentPage);
+  const noMoreArticlesToShowRef = useShadowRef(noMoreArticlesToShow);
+  const isWaitingForNewArticlesRef = useShadowRef(isWaitingForNewArticles);
+
+  function insertNewArticlesIntoArticleList(
+    fetchedArticles,
+    newCurrentPage,
+    newArticles,
+  ) {
+    if (fetchedArticles.length === 0) {
+      setNoMoreArticlesToShow(true);
+    }
+    newArticles = newArticles.concat(fetchedArticles);
+    setArticleList(newArticles);
+    setCurrentPage(newCurrentPage);
+    setIsWaitingForNewArticles(false);
+  }
+
+  function handleScroll() {
+    let scrollBarPixelDistToPageEnd = getPixelsFromScrollBarToEnd();
+    let articlesHaveBeenFetched =
+      currentPageRef.current !== undefined &&
+      articleListRef.current !== undefined;
+
+    if (
+      scrollBarPixelDistToPageEnd <= 50 &&
+      !isWaitingForNewArticlesRef.current &&
+      !noMoreArticlesToShowRef.current &&
+      articlesHaveBeenFetched
+    ) {
+      setIsWaitingForNewArticles(true);
+      setTitle("Getting more articles...");
+
+      let newCurrentPage = currentPageRef.current + 1;
+      let newArticles = [...articleListRef.current];
+      api.getSavedUserArticles(newCurrentPage, (articles) => {
+        insertNewArticlesIntoArticleList(articles, newCurrentPage, newArticles);
+        setTitle("Saved Articles");
+      });
+    }
+  }
 
   useEffect(() => {
     setTitle("Saved Articles");
-
-    api.getSavedUserArticles((articles) => {
+    api.getSavedUserArticles(currentPage, (articles) => {
       setArticleList(articles);
       setOriginalList(articles);
     });
+    window.addEventListener("scroll", handleScroll, true);
+    return () => {
+      window.removeEventListener("scroll", handleScroll, true);
+    };
   }, []);
 
   if (articleList == null) {
@@ -47,6 +98,21 @@ export default function OwnArticles({ api }) {
           dontShowSourceIcon={true}
         />
       ))}
+      {isWaitingForNewArticles && (
+        <LoadingAnimation delay={0}></LoadingAnimation>
+      )}
+      {noMoreArticlesToShow && articleList.length > 0 && (
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-around",
+            margin: "2em 0px",
+          }}
+        >
+          There are no more saved articles.
+        </div>
+      )}
     </>
   );
 }
