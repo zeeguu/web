07c8b16567965b02883ea40b461e855e4f6179e2
chloeolson <61156007+chloeolson@users.noreply.github.com>
2024-11-23 12:53:14 +0100

    Moved and edited api calls

--- src/api/userActivityAndCommitment.js
+++ src/api/userActivityAndCommitment.js
@@ -1,19 +1,31 @@
 import { Zeeguu_API } from "./classDef";
 
-/*
-
-This function could be responsible for fetching the raw data from an endpoint
-called "data_user_commitment". Should do nothing with the data yet, other than
-fetching the raw data. Later, this function could be called in the file UserDashboard.js, 
-like api.getUserActivityByDay, where the callback function is activitiesArray.When _getJSON
-is finished with fetching the data, it will execute this callback with the data it retreived.
-
-*/
 Zeeguu_API.protoype.getUserActivityAndCommitment = function (callback) {
   this._getJSON("user_activity_and_commitment", callback);
 };
 
-/*
-_getJSON is a method defined for the Zeeguu_API class. When calling this. this probably refers
-to the class Zeeguu_API .. I'm too tired to think more about this. To be continued :-)
-*/
+Zeeguu_API.prototype.createUserCommitment = function (
+  userMinutes,
+  userDays,
+  callback,
+) {
+  let payload = {
+    user_minutes: userMinutes,
+    user_days: userDays,
+  };
+
+  this._postJSON("user_commitment_create", payload, callback);
+};
+
+Zeeguu_API.prototype.updateUserCommitment= function (
+  consecutiveWeeks,
+  commitmentLastUpdated,
+  callback,
+) {
+  let payload = {
+    consecutive_weeks: consecutiveWeeks,
+    commitment_last_updated: commitmentLastUpdated,
+  };
+
+  this._putJSON("user_commitment_update", payload, callback);
+};
