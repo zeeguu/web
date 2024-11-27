55322604eeb225eab54d2d1d0675ca93a3a58b45
chloeolson <61156007+chloeolson@users.noreply.github.com>
2024-11-27 14:08:07 +0100

    updated minutes options

--- src/assorted/minutesGoal.js
+++ src/assorted/minutesGoal.js
@@ -1,9 +1,10 @@
 import strings from "../i18n/definitions";
 
 export const MINUTES_GOAL = [
+  { value: "2", label: strings.twoMinutes },
   { value: "5", label: strings.fiveMinutes },
   { value: "10", label: strings.tenMinutes },
   { value: "15", label: strings.fifteenMinutes },
+  { value: "20", label: strings.twentyMinutes },
   { value: "30", label: strings.thirtyMinutes },
-  { value: "60", label: strings.sixtyMinutes },
 ];
