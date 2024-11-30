b4180922ede3f22a0ff595f6218c678ecaade57e
Mircea Filip Lungu <mircea.lungu@gmail.com>
2024-11-30 13:24:31 +0100

    script for downloading favicon as png

---
+++ public/news-icons/download.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]] ; then
+    echo 'Provide the domain you want to get the favicon image from'
+    exit 0
+fi
+
+if [[ $# -eq 2 ]] ; then
+    echo "Saving to provided filename: "
+    FILENAME=$2
+else
+	FILENAME=`echo $1 | awk -F[/:] '{print $4}'`.png
+	echo "Saving to filename derived from url: " 
+fi
+
+echo "" 
+echo "    $FILENAME"
+echo "" 
+
+echo "Press ENTER to continue."
+read
+
+curl https://t1.gstatic.com/faviconV2\?client\=SOCIAL\&type\=FAVICON\&fallback_opts\=TYPE,SIZE,URL\&url\=$1\&size\=48 > $FILENAME
+open $FILENAME
+
