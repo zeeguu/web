90489a959ed9f5eb37d1a9651ce459f2cf2f05cc
jolb <jolb@itu.dk>
2024-12-13 16:00:23 +0100

    debugging adding comments

--- src/assorted/useCommitmentAndActivity.js
+++ src/assorted/useCommitmentAndActivity.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 const useCommitmentAndActivity = (api) => {
   const [commitmentAndActivityData, setCommitmentAndActivityData] = useState(0);
@@ -7,15 +7,17 @@ const useCommitmentAndActivity = (api) => {
   useEffect(() => {
     const fetchData = () => {
       api.getUserActivityAndCommitment((activitiesAndCommitmentArray) => {
-        console.log(activitiesAndCommitmentArray);
-
+        //currentDate = Fri Dec 13 2024 15:03:13 GMT+0100 (centraleuropeisk normaltid)
         const currentDate = new Date();
+        //dayOfWeek = 5 (since it is friday today)
         const dayOfWeek = currentDate.getDay();
         let daysAwayFromMonday;
 
+        //if day of week is 0 it is sunday monday was 6 days ago
         if (dayOfWeek === 0) {
           daysAwayFromMonday = 6;
         } else {
+          //else monday was this many days ago
           daysAwayFromMonday = dayOfWeek - 1;
         }
 
@@ -37,24 +39,12 @@ const useCommitmentAndActivity = (api) => {
           date: date,
           seconds: combinedObject[date],
         }));
+        //combinedArray[0].seconds) could be 300
+        //combinedArray[0].date) could be 2024-12-12
 
         const activitiesSorted = combinedArray.sort((a, b) =>
           b.date.localeCompare(a.date),
         );
-
-        const filteredActivitiesSorted = activitiesSorted.slice(
-          0,
-          daysAwayFromMonday + 1,
-        );
-
-        const userMinutes = activitiesAndCommitmentArray.user_minutes;
-
-        const filteredActivities = filteredActivitiesSorted.filter(
-          (activity) => activity.seconds >= userMinutes,
-        );
-
-        const weeklyActivitiesCount = filteredActivities.length;
-        const userDaysPerWeek = activitiesAndCommitmentArray.user_days;
 
         let startOfWeek = new Date(currentDate);
         startOfWeek.setDate(currentDate.getDate() - daysAwayFromMonday);
@@ -63,6 +53,40 @@ const useCommitmentAndActivity = (api) => {
         let endOfWeek = new Date(startOfWeek);
         endOfWeek.setDate(startOfWeek.getDate() + 6);
         endOfWeek.setHours(23, 59, 59);
+
+        //filteredActivitiesSorted only keeps track of the sessions withing the current week.
+        const filteredActivitiesSorted = activitiesSorted.filter((activity) => {
+          const activityDate = new Date(activity.date); // Convert date string to Date object
+          return activityDate >= startOfWeek && activityDate <= endOfWeek;
+        });
+
+        console.log(
+          "This is the filteredActivitesSorted before filtering out short sessions",
+        );
+        filteredActivitiesSorted.forEach((item) => {
+          console.log(item);
+        });
+
+        //stores the minutes goal the user picked
+        const userMinutes = activitiesAndCommitmentArray.user_minutes;
+
+        //CHLOE! BUG: Does not filter out sessions that is less than the minutes the user picked even if it should
+        const filteredActivities = filteredActivitiesSorted.filter(
+          (activity) => activity.seconds >= userMinutes,
+        );
+
+        console.log(
+          "This is the filteredActivitesSorted when we have filtered out the one with too few time",
+        );
+        filteredActivities.forEach((item) => {
+          console.log(item);
+        });
+
+        //How many times the user have practiced this week
+        const weeklyActivitiesCount = filteredActivities.length;
+
+        //Stores how many days the user wants to practice every week
+        const userDaysPerWeek = activitiesAndCommitmentArray.user_days;
 
         const inCurrentWeek =
           currentDate >= startOfWeek && currentDate <= endOfWeek;
@@ -77,11 +101,11 @@ const useCommitmentAndActivity = (api) => {
           const lastWeeklyCommitmentUpdate = new Date(
             activitiesAndCommitmentArray.commitment_last_updated,
           );
-      
-          console.log("lastWeeklyCommitmentUpdate",lastWeeklyCommitmentUpdate);
+
+          console.log("lastWeeklyCommitmentUpdate", lastWeeklyCommitmentUpdate);
           console.log("startofWeek", startOfWeek);
           console.log("endOfWeek", endOfWeek);
-     
+
           if (
             lastWeeklyCommitmentUpdate != null &&
             lastWeeklyCommitmentUpdate.getTime() >= startOfWeek.getTime() &&
@@ -89,16 +113,17 @@ const useCommitmentAndActivity = (api) => {
           ) {
             setCommitmentAndActivityData(currentCommitmentAndActivityData);
             console.log("It is in the week!");
-            console.log("currentCommitmentAndActivityData",currentCommitmentAndActivityData);
-            
-          } else{
+            console.log(
+              "currentCommitmentAndActivityData",
+              currentCommitmentAndActivityData,
+            );
+          } else {
             console.log("It is NOT in the week!");
             setCommitmentAndActivityData(currentCommitmentAndActivityData + 1);
             setLastCommitmentUpdate(currentDate);
           }
-      
         } else {
-          console.log("is this what is happening?")
+          console.log("is this what is happening?");
           setCommitmentAndActivityData(0);
         }
       });
@@ -107,7 +132,11 @@ const useCommitmentAndActivity = (api) => {
     fetchData();
   }, [api]);
 
-  console.log( "This is the commitmentAndActivityData, lastCommitmentUpdate", commitmentAndActivityData, lastCommitmentUpdate);
+  console.log(
+    "This is the commitmentAndActivityData, lastCommitmentUpdate",
+    commitmentAndActivityData,
+    lastCommitmentUpdate,
+  );
   return { commitmentAndActivityData, lastCommitmentUpdate };
 };
 
