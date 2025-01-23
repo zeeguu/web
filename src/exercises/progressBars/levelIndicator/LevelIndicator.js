4c3e014490f4023e6e54f46983c5c7360dd46202
Mircea Filip Lungu <mircea.lungu@gmail.com>
2025-01-23 14:03:31 +0100

    towards simpler code

--- src/exercises/progressBars/levelIndicator/LevelIndicator.js
+++ src/exercises/progressBars/levelIndicator/LevelIndicator.js
@@ -1,6 +1,12 @@
 import * as s from "./LevelIndicator.sc.js";
 import LevelIndicatorBar from "./LevelIndicatorBar.js";
 import LevelIndicatorCircles from "./LevelIndicatorCircles.js";
+import isBookmarkExpression from "../../../utils/misc/isBookmarkExpression";
+import strings from "../../../i18n/definitions";
+
+export const LEVELS = 4;
+export const COOLING_INTERVALS_PER_LEVEL = 3; // cooling intervals 0, 1, and 2
+export const LEVELS_IN_PERCENT = 100 / LEVELS;
 
 export default function LevelIndicator({
   bookmark,
@@ -8,33 +14,46 @@ export default function LevelIndicator({
   userIsWrong,
   isHidden,
 }) {
-  const totalLearningStages = 5; // 4 levels + 1 for learned stage
-  const numberOfCoolingIntervalsPerLevel = 3; // cooling intervals 0, 1, and 2
-  const levelsInPercent = 25; //100% bar width divided by 4 levels
+  const totalCircles = LEVELS + 1;
 
-  const { cooling_interval, level } = bookmark;
+  let { cooling_interval, level, is_last_in_cycle } = bookmark;
+  console.log(bookmark);
 
-  const newBookmark = level === 0 && cooling_interval === null && !isHidden;
+  // const isNewBookmark = level === 0 && cooling_interval === null && !isHidden;
+  // when we create a new bookmark, the level is automatically set to zero
+  // for backwards compatibility we also set all the levels to zero
+  const isNewBookmark = level === 0 && cooling_interval === null;
+
+  // if the backend sent us a bookmark with level 0 that actually means
+  // it's level 1, but was not exercised until now; so for the remaining
+  // code level is going to actually be 1
+  // the same story with the cooling interval; if it's null on the backend
+  // the semantics is that it's actually 0
+  level = level ? level : 1;
+  cooling_interval = cooling_interval ?? 0;
+
+  const bookmarkMovesToNextLevel = is_last_in_cycle && userIsCorrect;
+  const bookmarkStaysAtCurrentLevel = cooling_interval === 0 && userIsWrong;
 
   return (
     <s.LevelIndicator isHidden={isHidden}>
       <div className="level-indicator">
         <LevelIndicatorBar
-          totalLearningStages={totalLearningStages}
-          bookmark={bookmark}
-          userIsWrong={userIsWrong}
-          newBookmark={newBookmark}
           isHidden={isHidden}
-          userIsCorrect={userIsCorrect}
-          levelsInPercent={levelsInPercent}
-          numberOfCoolingIntervalsPerLevel={numberOfCoolingIntervalsPerLevel}
+          cooling_interval={cooling_interval}
+          level={level}
         />
         <LevelIndicatorCircles
-          totalLearningStages={totalLearningStages}
-          userIsWrong={userIsWrong}
-          newBookmark={newBookmark}
-          bookmark={bookmark}
-          userIsCorrect={userIsCorrect}
+          totalLearningStages={totalCircles}
+          levelCompleted={bookmarkMovesToNextLevel}
+          levelIsBlinking={bookmarkStaysAtCurrentLevel}
+          showNewNotification={isNewBookmark}
+          levelInProgress={level}
+          tooltipText={
+            isBookmarkExpression(bookmark)
+              ? strings.newExpressionExercisesTooltip
+              : strings.newWordExercisesTooltip
+          }
         />
       </div>
     </s.LevelIndicator>
--- src/exercises/progressBars/levelIndicator/LevelIndicatorBar.js
+++ src/exercises/progressBars/levelIndicator/LevelIndicatorBar.js
@@ -1,59 +1,26 @@
-import { useEffect, useState } from "react";
+import {
+  LEVELS,
+  COOLING_INTERVALS_PER_LEVEL,
+  LEVELS_IN_PERCENT,
+} from "./LevelIndicator";
 
 export default function LevelIndicatorBar({
   isHidden,
-  newBookmark,
-  userIsCorrect,
-  userIsWrong,
-  levelsInPercent,
-  numberOfCoolingIntervalsPerLevel,
-  totalLearningStages,
-  bookmark,
+  cooling_interval,
+  level,
 }) {
-  const { cooling_interval, level, is_last_in_cycle } = bookmark;
-  // Normalize level and cooling_interval to handle null values the first time a bookmark is practiced
-  const normalizedLevel = level ? level : 1;
-  const normalizedCoolingInterval = cooling_interval ?? 0;
+  const progressWithinLevel = cooling_interval / COOLING_INTERVALS_PER_LEVEL;
 
-  const initialProgressWithinLevel =
-    normalizedCoolingInterval / numberOfCoolingIntervalsPerLevel;
-
-  const initialProgressBarWidth =
-    isHidden || newBookmark
-      ? `0%`
-      : `${((normalizedLevel - 1) / (totalLearningStages - 1)) * 100 + initialProgressWithinLevel * levelsInPercent}%`;
-
-  const [progressBarWidth, setProgressBarWidth] = useState(
-    initialProgressBarWidth,
-  );
-
-  useEffect(() => {
-    const calculateProgressWithinLevel = userIsCorrect
-      ? (normalizedCoolingInterval + 1) / numberOfCoolingIntervalsPerLevel
-      : userIsWrong && normalizedCoolingInterval === 0
-        ? initialProgressBarWidth
-        : userIsWrong
-          ? (normalizedCoolingInterval - 1) / numberOfCoolingIntervalsPerLevel
-          : initialProgressBarWidth;
-
-    const calculateProgressBarWidth =
-      is_last_in_cycle && userIsCorrect
-        ? `${(normalizedLevel / (totalLearningStages - 1)) * 100}%`
-        : `${((normalizedLevel - 1) / (totalLearningStages - 1)) * 100 + calculateProgressWithinLevel * levelsInPercent}%`;
-    setProgressBarWidth(calculateProgressBarWidth);
-  }, [
-    userIsCorrect,
-    userIsWrong,
-    normalizedCoolingInterval,
-    is_last_in_cycle,
-    normalizedLevel,
-    newBookmark,
-    isHidden,
-  ]);
+  const progressBarWidth = isHidden
+    ? 0
+    : ((level - 1) / LEVELS) * 100 + progressWithinLevel * LEVELS_IN_PERCENT;
 
   return (
     <div className="progress-bar">
-      <div className="progress-fill" style={{ width: progressBarWidth }} />
+      <div
+        className="progress-fill"
+        style={{ width: `${progressBarWidth}%` }}
+      />
     </div>
   );
 }
