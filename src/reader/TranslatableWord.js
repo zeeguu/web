15f944f93a7e3685733ae0fd5f0cf7a3df187da3
Tiago Ribeiro <tfnribeiro@gmail.com>
2025-01-28 13:20:50 +0100

    Update TranslatableWord.js
    - The api is part of the interactiveText, so we don't need the context.

--- src/reader/TranslatableWord.js
+++ src/reader/TranslatableWord.js
@@ -1,7 +1,6 @@
-import { useState, useContext, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useClickOutside } from "react-click-outside-hook";
 import AlterMenu from "./AlterMenu";
-import { APIContext } from "../contexts/APIContext";
 import extractDomain from "../utils/web/extractDomain";
 import redirect from "../utils/routing/routing";
 import LinkOffIcon from "@mui/icons-material/LinkOff";
@@ -24,8 +23,6 @@ export default function TranslatableWord({
   const [prevWord, setPreviousWord] = useState("");
   const [isVisible, setIsVisible] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-
-  const api = useContext(APIContext);
 
   useEffect(() => {
     if (word.isVisible) {
@@ -80,7 +77,7 @@ export default function TranslatableWord({
 
   function unlinkLastWord(e, word) {
     setIsLoading(true);
-    api.deleteBookmark(
+    interactiveText.api.deleteBookmark(
       word.bookmark_id,
       (response) => {
         if (response === "OK") {
@@ -108,7 +105,7 @@ export default function TranslatableWord({
   }
 
   function deleteTranslation(e, word) {
-    api.deleteBookmark(
+    interactiveText.api.deleteBookmark(
       word.bookmark_id,
       (response) => {
         if (response === "OK") {
