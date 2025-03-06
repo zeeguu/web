e9215090a7a83ea47319d7673473059c51c797da
Mircea Filip Lungu <mircea.lungu@gmail.com>
2025-03-06 16:06:10 +0100

    Update UserContext.js

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -10,15 +10,7 @@ const UserContext = React.createContext({
   // - logoutMethod
 });
 
-// who uses what from userContext
-// - learned_language: SideNavLanguageOption: learned_language
-// - session
-// - is_teacher: SideNav.js
-// - native_language
 
-// In LanguageModal we call setUser ...
 export {
   UserContext, // Export it so it can be used by other Components
 };
-
-// https://www.digitalocean.com/community/tutorials/react-manage-user-login-react-context
