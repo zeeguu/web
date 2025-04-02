dfd18ee73c476e657bd9c7837066085e28cf140d
Tiago Ribeiro <tfnribeiro@gmail.com>
2025-04-02 11:37:14 +0200

    Update background.js
    - Added a function to inject our main Web font (Montserrat) into the tab which the extension runs on.
    - I had googled a bit around, but doing some more research and using Gemini 2.5 this seems the easiest solution, the only downside is that it's fetching the font every time the extension is run, but I guess the alternative would be to package the font in our bundle and have a larger extension.
    
    This results in the extension rendering our font style aligning itself more with the application itself.
    
    When using the ContextClick this is done by the background script, but when using the button, we need to call this function before rendering the modal.

--- src/background/background.js
+++ src/background/background.js
@@ -9,11 +9,67 @@ import { getIsLoggedIn, getUserInfoDictFromCookies } from "../popup/cookies";
 import Zeeguu_API from "../zeeguu-react/src/api/Zeeguu_API";
 import { EXTENSION_SOURCE } from "../JSInjection/constants";
 
+// Montserrat font
+const googleFontUrl =
+  "https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap";
+
+// From Zeeguu's Web CSS
+const BODY_ZEEGUU_FONT_RULE = `
+body {
+  margin: 0;
+  font-family:
+    "Montserrat",
+    -apple-system,
+    BlinkMacSystemFont,
+    "Segoe UI",
+    "Roboto",
+    "Oxygen",
+    "Ubuntu",
+    "Cantarell",
+    "Fira Sans",
+    "Droid Sans",
+    "Helvetica Neue",
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+`;
+
+// --- Function to Inject CSS ---
+async function injectFontAndStyles(tabId) {
+  try {
+    // 1. Fetch the Google Font CSS content
+    const response = await fetch(googleFontUrl);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const fontCss = await response.text();
+    console.log("Fetched Font CSS for tab:", tabId);
+
+    // 2. Inject the fetched @font-face rules
+    await BROWSER_API.scripting.insertCSS({
+      target: { tabId: tabId },
+      // Injecting the fetched CSS which contains @font-face rules
+      css: fontCss,
+    });
+    console.log("Injected @font-face rules for tab:", tabId);
+
+    // 3. Inject the Zeeguu Web font rules, this will use the font attached in 2.
+    await BROWSER_API.scripting.insertCSS({
+      target: { tabId: tabId },
+      css: BODY_ZEEGUU_FONT_RULE,
+    });
+    console.log("Injected font application rules for tab:", tabId);
+  } catch (error) {
+    console.error("Failed to inject font and styles:", error);
+  }
+}
+
 BROWSER_API.runtime.onMessageExternal.addListener(
   (request, sender, sendResponse) => {
     console.log("Received message from " + sender.url + ": ", request);
     sendResponse({ message: true });
-  },
+  }
 );
 
 BROWSER_API.runtime.onInstalled.addListener(function (object) {
@@ -28,7 +84,7 @@ BROWSER_API.runtime.onMessage.addListener(async function (request) {
     try {
       // Chrome TTS gets the speak engine from the OS.
       // This means the OS needs to have a voice installed.
-      await chrome.tts.speak(request.options.text, {
+      await BROWSER_API.tts.speak(request.options.text, {
         lang: request.options.language,
       });
       console.log(request.options.language);
@@ -64,11 +120,11 @@ async function startReader() {
       let api = new Zeeguu_API(API_URL);
       let userData = await getUserInfoDictFromCookies(WEB_URL);
       setUserInLocalStorage(userData, api);
-      await api.logReaderActivity(
+      await api.logUserActivity(
         api.OPEN_CONTEXT,
         "",
         tab.url,
-        EXTENSION_SOURCE,
+        EXTENSION_SOURCE
       );
     } catch (err) {
       console.error(`failed to execute script: ${err}`);
@@ -79,7 +135,11 @@ async function startReader() {
           files: ["main.js"],
           func: setCurrentURL(tab.url),
         })
-        .then(() => console.log("injected the function!"));
+        .then(() => {
+          console.log("Zeeguu code injected successfully!");
+          console.log("Adding Zeeguu font-styling...");
+          injectFontAndStyles(tab.id);
+        });
     }
   }
 }
@@ -93,3 +153,5 @@ BROWSER_API.contextMenus.onClicked.addListener(async (clickData) => {
 BROWSER_API.runtime.onInstalled.addListener(() => {
   BROWSER_API.contextMenus.create(contextMenuReadArticle);
 });
+
+export { injectFontAndStyles };
