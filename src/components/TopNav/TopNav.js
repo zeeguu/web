c8f18fddec61e20e0b8c4c2c09b7eccf411234d2
igawaclawska <iga.waclawska@gmail.com>
2025-04-04 11:36:54 +0200

    explain the reason behind the TopNav structure

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -1,4 +1,7 @@
 import * as s from "./TopNav.sc";
+// More info on why we use <ul> and <li> inside <nav> is available here:
+// https://www.w3.org/WAI/tutorials/menus/structure/#identify-menus
+// https://www.w3.org/WAI/tutorials/menus/structure/#menu-representation
 
 export default function TopNav({ children }) {
   return (
--- src/components/TopNav/TopNavOption.js
+++ src/components/TopNav/TopNavOption.js
@@ -1,4 +1,7 @@
 import * as s from "./TopNavOption.sc";
+// More info on why links are wrapped in <li> is available here:
+// https://www.w3.org/WAI/tutorials/menus/structure/#identify-menus
+// https://www.w3.org/WAI/tutorials/menus/structure/#menu-representation
 
 export default function TopNavOption({
   children,
