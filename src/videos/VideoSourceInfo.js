e383eeb7b53bce3b80d4c78be056ea337095bee5
martinkimoe <m.kimoelarsen@gmail.com>
2025-04-07 23:02:15 +0200

    Remove unused import

--- src/videos/VideoSourceInfo.js
+++ src/videos/VideoSourceInfo.js
@@ -1,17 +1,16 @@
 import * as s from "../components/ArticleSourceInfo.sc";
-import { getNewsIconPath } from "../utils/misc/staticPath";
 import moment from "moment";
 
 export default function VideoSourceInfo({ video }) {
   return (
     <s.SourceContainer>
-        <s.SourceImage>
-            <img src={video.channel.thumbnail_url} alt="" />
-        </s.SourceImage>
-        <s.FeedName>{video.channel.name}</s.FeedName>
-        <s.PublishingTime>
-          ({moment.utc(video.published_at).fromNow()})
-        </s.PublishingTime>
+      <s.SourceImage>
+        <img src={video.channel.thumbnail_url} alt="" />
+      </s.SourceImage>
+      <s.FeedName>{video.channel.name}</s.FeedName>
+      <s.PublishingTime>
+        ({moment.utc(video.published_at).fromNow()})
+      </s.PublishingTime>
     </s.SourceContainer>
   );
 }
