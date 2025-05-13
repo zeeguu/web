9cbae0d1e1fc3012ecd73b7d5c53980a8b3f30dd
Mircea Filip Lungu <mircea.lungu@gmail.com>
2025-05-13 12:03:32 +0200

    now with input validation also

--- src/hooks/useUserPreferences.js
+++ src/hooks/useUserPreferences.js
@@ -1,17 +1,27 @@
 import { useEffect, useState } from "react";
 import SessionStorage from "../assorted/SessionStorage";
 
-export default function useUserPreferences(api) {
-  const TRANSLATE_IN_READER = "translate_reader";
-  const PRONOUNCE_IN_READER = "pronounce_reader";
-  const AUDIO_EXERCISES = "audio_exercises";
+const TRANSLATE_IN_READER = "translate_reader";
+const PRONOUNCE_IN_READER = "pronounce_reader";
+const AUDIO_EXERCISES = "audio_exercises";
+const MAX_WORDS_TO_SCHEDULE = "max_words_to_schedule";
 
+export default function useUserPreferences(api) {
   const [audioExercises, setAudioExercises] = useState(true);
   const [translateInReader, setTranslateInReader] = useState(true);
   const [pronounceInReader, setPronounceInReader] = useState(true);
+  const [maxWordsToSchedule, setMaxWordsToSchedule] = useState(15);
 
   function _boolPreferenceParse(preferences, key) {
     return preferences[key] === undefined || preferences[key] === "true";
+  }
+
+  function updateMaxWordsToSchedule(value) {
+    setMaxWordsToSchedule(value);
+
+    api.saveUserPreferences({
+      [MAX_WORDS_TO_SCHEDULE]: value,
+    });
   }
 
   function updateTranslateInReader(value) {
@@ -39,16 +49,10 @@ export default function useUserPreferences(api) {
   useEffect(() => {
     api.getUserPreferences((preferences) => {
       console.log(preferences);
-      setAudioExercises(
-        _boolPreferenceParse(preferences, AUDIO_EXERCISES) &&
-          SessionStorage.isAudioExercisesEnabled(),
-      );
-      setTranslateInReader(
-        _boolPreferenceParse(preferences, TRANSLATE_IN_READER),
-      );
-      setPronounceInReader(
-        _boolPreferenceParse(preferences, PRONOUNCE_IN_READER),
-      );
+      setAudioExercises(_boolPreferenceParse(preferences, AUDIO_EXERCISES) && SessionStorage.isAudioExercisesEnabled());
+      setTranslateInReader(_boolPreferenceParse(preferences, TRANSLATE_IN_READER));
+      setPronounceInReader(_boolPreferenceParse(preferences, PRONOUNCE_IN_READER));
+      setMaxWordsToSchedule(parseInt(preferences[MAX_WORDS_TO_SCHEDULE]));
     });
   }, [api]);
 
@@ -59,5 +63,7 @@ export default function useUserPreferences(api) {
     updateAudioExercises,
     updateTranslateInReader,
     updatePronounceInReader,
+    maxWordsToSchedule,
+    updateMaxWordsToSchedule,
   };
 }
