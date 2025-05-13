16be8d93392ed1ecb1f0207403f7b621e6279cb7
jolobert <jolbitu.dk>
2025-05-13 12:56:12 +0200

    First draft of Exercise Progress Summary page

--- src/pages/Settings/TopbarIconPreferences.js
+++ src/pages/Settings/TopbarIconPreferences.js
@@ -9,7 +9,6 @@ import Heading from "../_pages_shared/Heading.sc";
 import ButtonContainer from "../_pages_shared/ButtonContainer.sc";
 import Button  from "../_pages_shared/Button.sc";
 import NavIcon from "../../components/MainNav/NavIcon";
-<<<<<<< HEAD
 import {IconRow, AdjustedIcon, Description} from "./TopBarIconPreferences.sc";
 import { useEffect, useState, React } from "react";
 
@@ -40,18 +39,6 @@ export default function TopbarIconPreferences(){
         window.location.reload();
     }
     
-=======
-import {IconRow, AdjustedIcon, Description} from "./TopbarIconPreferences.sc";
-
-export default function TopbarIconPreferences(){
-    function handleSave(e){
-        e.preventDefault();
-    }
-
-    function handleIconPreferences(){   
-    }
-
->>>>>>> 74f25417 (Added UI for settings page)
     return(
         <PreferencesPage layoutVariant={"minimalistic-top-aligned"}>
         <BackArrow />
@@ -61,39 +48,23 @@ export default function TopbarIconPreferences(){
         <Main>
             <FormSection>
                 <IconRow>
-<<<<<<< HEAD
                 <Checkbox checked={whichItems.includes("wordsPracticedTopBar")} onChange={(e) => handleIconPreferences(e, "wordsPracticedTopBar")}/>
-=======
-                <Checkbox onChange={handleIconPreferences}/>
->>>>>>> 74f25417 (Added UI for settings page)
                 <AdjustedIcon>
                 <NavIcon name="words"/>
                 <Description>{strings.wordsPracticedThisWeek}</Description>
                 </AdjustedIcon>
                </IconRow>
                <IconRow>
-<<<<<<< HEAD
                 <Checkbox checked={whichItems.includes("articleMinutesTopBar")} onChange={(e) => handleIconPreferences(e, "articleMinutesTopBar")}/>
                 <AdjustedIcon>
                 <NavIcon name="headerArticles"/>
-=======
-                <Checkbox onChange={handleIconPreferences}/>
-                <AdjustedIcon>
-                <NavIcon name="words"/>
->>>>>>> 74f25417 (Added UI for settings page)
                 <Description>{strings.articlesReadThisWeek}</Description>
                 </AdjustedIcon>
                </IconRow>
                <IconRow>
-<<<<<<< HEAD
                 <Checkbox checked={whichItems.includes("streakTopBar")} onChange={(e) => handleIconPreferences(e, "streakTopBar")}/>
                 <AdjustedIcon>
                 <NavIcon name="headerStreak"/>
-=======
-                <Checkbox onChange={handleIconPreferences}/>
-                <AdjustedIcon>
-                <NavIcon name="words"/>
->>>>>>> 74f25417 (Added UI for settings page)
                 <Description>{strings.weeklyStreak}</Description>
                 </AdjustedIcon>
                </IconRow>
