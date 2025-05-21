071acb1e8a55fbef53585c65b0c51673d9ebda57
Mircea Filip Lungu <mircea.lungu@gmail.com>
2025-05-21 09:40:44 +0300

    fixing bug with undefined onClickCallback being called

--- src/exercises/exerciseTypes/SpeakButton.js
+++ src/exercises/exerciseTypes/SpeakButton.js
@@ -96,7 +96,7 @@ export default function SpeakButton({
         disabled={isSpeaking}
         onClick={() => {
           !isSpeaking && handleSpeak();
-          onClickCallback();
+          onClickCallback && onClickCallback();
         }}
       >
         {isSpeaking && (
