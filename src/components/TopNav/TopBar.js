8bb7e7f0e6debe6af613a4e5820e6f22dd943ed5
chloeolson <61156007+chloeolson@users.noreply.github.com>
2025-05-21 08:42:45 +0200

    TopBar first draft and added icons

--- src/components/TopNav/TopBar.js
+++ src/components/TopNav/TopBar.js
@@ -1,16 +1,44 @@
 import NavIcon from "../MainNav/NavIcon";
 import * as s from "./TopBar.sc";
 import {zeeguuOrange} from "../colors";
+import { getTopBarData } from "../../utils/progressTracking/ProgressOverviewItems";
+import { useEffect, useState, React } from "react";
 
-export default function TopBar() {
+
+export default function TopBar({weeklyTranslated, weeklyReadingMinutes}) {
+  const {weeklyProgressOverview} = getTopBarData({weeklyTranslated, weeklyReadingMinutes});
+
+  useEffect(() => {
+    const savedPrefs = JSON.parse(localStorage.getItem("topBarPrefs")) || [];
+    setWhichItems(savedPrefs);
+  }, []);
+  
+  const [whichItems, setWhichItems] = useState([]);
+  console.log("whichItems", whichItems);
+  const savedItems = JSON.parse(localStorage.getItem("topBarPrefs") || "[]");
+
   return (
     <s.StatContainer>
-      <NavIcon name="words" color={zeeguuOrange} />
-      <s.StatNumber>12</s.StatNumber>
-      <NavIcon name="headerArticles" color= {zeeguuOrange}/>
-      <s.StatNumber>5</s.StatNumber>
-      <NavIcon name="headerStreak" color={zeeguuOrange} />
-      <s.StatNumber>10</s.StatNumber>
+   {savedItems.includes("wordsPracticedTopBar") && (
+  <>
+    <NavIcon name={weeklyProgressOverview[1].icon} color={zeeguuOrange} />
+    <s.StatNumber>{weeklyProgressOverview[1].value}</s.StatNumber>
+  </>
+)}
+ {savedItems.includes("articleMinutesTopBar") && (
+  <>
+    <NavIcon name={weeklyProgressOverview[0].icon} color= {zeeguuOrange}/>
+    <s.StatNumber>{weeklyProgressOverview[0].value}</s.StatNumber>
+  </>
+)}
+ {savedItems.includes("streakTopBar") && (
+  <>
+   <NavIcon name={weeklyProgressOverview[2].icon} color={zeeguuOrange} />
+   <s.StatNumber>{weeklyProgressOverview[2].value}</s.StatNumber>
+  </>
+)}
+      
+      
     </s.StatContainer>
   );
 }
