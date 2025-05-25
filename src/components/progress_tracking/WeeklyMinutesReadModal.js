f544fec301c740160a8c9edf4225f4bb00dc795f
jolobert <jolbitu.dk>
2025-05-25 16:25:32 +0200

    added modal for topbar

--- src/components/progress_tracking/WeeklyMinutesReadModal.js
+++ src/components/progress_tracking/WeeklyMinutesReadModal.js
@@ -1,15 +1,10 @@
-import { useEffect } from "react";
 import Modal from "../modal_shared/Modal.js";
 import Header from "../modal_shared/Header.sc.js";
 import Heading from "../modal_shared/Heading.sc.js";
 import Main from "../modal_shared/Main.sc.js";
+import * as s from "../../components/progress_tracking/ProgressTrackingModal.sc.js";
 
-export default function WeeklyMinutesReadModal({open, setOpen}){
-    useEffect(() => {
-        if (open){
-            //fetch correct data here
-        }
-    });
+export default function WeeklyMinutesReadModal({open, setOpen, value}){
     return (
         <Modal
             open={open}
@@ -21,6 +16,7 @@ export default function WeeklyMinutesReadModal({open, setOpen}){
                 <Heading>Your weekly minutes read</Heading>
             </Header>
             <Main>
+                <s.TextRow>You have spent time reading in {value} articles this week!</s.TextRow>
             </Main>
         </Modal>
 
--- src/components/progress_tracking/WeeklyStreakModal.js
+++ src/components/progress_tracking/WeeklyStreakModal.js
@@ -1,15 +1,15 @@
-import { useEffect } from "react";
 import Modal from "../modal_shared/Modal.js";
 import Header from "../modal_shared/Header.sc.js";
 import Heading from "../modal_shared/Heading.sc.js";
 import Main from "../modal_shared/Main.sc.js";
+import * as s from "../../components/progress_tracking/ProgressTrackingModal.sc.js";
+import NavIcon from "../../components/MainNav/NavIcon.js";
+import {zeeguuOrange} from "../colors";
+import ReactLink from "../ReactLink.sc.js";
+import ArrowForwardRoundedIcon from "@mui/icons-material/ArrowForwardRounded";
 
-export default function WeeklyStreakModal({open, setOpen}){
-    useEffect(() => {
-        if (open){
-            //fetch correct data here
-        }
-    });
+export default function WeeklyStreakModal({open, setOpen, value}){
+   
     return (
         <Modal
             open={open}
@@ -18,9 +18,23 @@ export default function WeeklyStreakModal({open, setOpen}){
             }}
         >
             <Header withoutLogo>
-                <Heading>Your weekly streak</Heading>
+                <Heading>Current Weekly Streak</Heading>
             </Header>
             <Main>
+            <s.IconAndIntegerRow>
+                <NavIcon name="headerStreak" size={90} color={zeeguuOrange}/>
+                <s.Value>{value}</s.Value>
+                <h3>weeks</h3>
+                </s.IconAndIntegerRow>
+                
+                <s.TextRow>You have practiced every week for {value} weeks straigth!</s.TextRow>
+                 <ReactLink
+                 className="small"
+                 onClick={() => setOpen(false)}
+                 to="user_dashboard"
+                 >
+               More details of your practice frequency <ArrowForwardRoundedIcon />
+            </ReactLink>
             </Main>
         </Modal>
 
