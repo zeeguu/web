4ad5d0043e92ee712d9bc6b300b3bc1c1620f8e1
chloeolson <61156007+chloeolson@users.noreply.github.com>
2025-06-30 15:15:02 +0200

    use next_practice_time instead of cooling interval for tooltip

--- src/words/WordsTooltip.js
+++ src/words/WordsTooltip.js
@@ -13,25 +13,28 @@ export default function WordsToolTip({open, setOpen, value}){
             }}
             title={
                 <span>
-                    {
-                value.level === 4 && value.is_about_to_be_learned === true
-                ? "Congratulations! You have learned this word. You won't practice this word anymore"
-                : <>
-                   You need to get this word correct {
-                    value.cooling_interval === 0 
-                    ? "today " : value.cooling_interval === 1 ? "tomorrow "
-                    : `in ${value.cooling_interval} days `
-                    }
-                    {
-                    value.level === 4 
-                        ? "to get closer to learning this word"
-                        : `to get closer to level ${value.level + 1}`
-                    }
-                </>
-             
-                    }
-            </span>}
-        >
+                  {
+                    value.level === 4 && value.is_about_to_be_learned === true
+                      ? "Congratulations! You have learned this word. You won't practice this word anymore"
+                      : (() => {
+                          const nextPractice = new Date(value.next_practice_time);
+                          const today = new Date();
+                          // Set both to midnight for date-only comparison
+                          nextPractice.setHours(0, 0, 0, 0);
+                          today.setHours(0, 0, 0, 0);
+                          const diffDays = Math.round((nextPractice - today) / (1000 * 60 * 60 * 24));
+                          if (diffDays === 1) {
+                            return <>You need to get this word correct tomorrow {value.level === 4 ? "to get closer to learning this word" : `to get closer to level ${value.level + 1}`}</>;
+                          } else if (diffDays > 1) {
+                            return <>You need to get this word correct in {diffDays} days {value.level === 4 ? "to get closer to learning this word" : `to get closer to level ${value.level + 1}`}</>;
+                          } else {
+                            return <>You need to get this word correct today {value.level === 4 ? "to get closer to learning this word" : `to get closer to level ${value.level + 1}`}</>;
+                          }
+                        })()
+                  }
+                </span>
+              }
+              >
             <div></div>
         </Tooltip>
     );
