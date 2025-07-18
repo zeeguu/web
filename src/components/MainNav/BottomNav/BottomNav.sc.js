cd1118a4b41448b99205170e7c4b380e303c1b1f
Mircea Filip Lungu <mircea.lungu@gmail.com>
2025-07-18 12:34:18 +0300

    a bit more bottom padding on iphone

--- src/components/MainNav/BottomNav/BottomNav.sc.js
+++ src/components/MainNav/BottomNav/BottomNav.sc.js
@@ -5,8 +5,8 @@ const BottomNav = styled.nav`
   width: 100%;
   position: fixed;
   bottom: 0;
-  padding: 0.5rem 1rem 1rem 1rem;
-  padding-bottom: calc(1rem + env(safe-area-inset-bottom, 0px));
+  padding: 0.5rem 1rem 0.5rem 1rem;
+  padding-bottom: calc(0.5rem + env(safe-area-inset-bottom, 0px));
   background-color: ${({ theme }) => theme.navBg};
   z-index: 2;
   transition: 0.3s ease-in-out;
