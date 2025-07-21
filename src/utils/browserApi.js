3d33c37dddc9d878d40c9f8ea991d9631e914a62
Mircea Filip Lungu <mircea.lungu@gmail.com>
2025-07-21 16:59:48 +0300

    removed files lost in translocation

--- src/utils/browserApi.js
+++
@@ -1,5 +0,0 @@
-/*global chrome*/
-/*global browser*/
-import { runningInChromeDesktop } from "../zeeguu-react/src/utils/misc/browserDetection";
-
-export const BROWSER_API = runningInChromeDesktop() ? chrome : browser
