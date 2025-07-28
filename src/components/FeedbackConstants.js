add794f586f24e394a3164a7db79c2e2cc88d386
Mircea Filip Lungu <mircea.lungu@gmail.com>
2025-07-28 10:57:09 +0300

    single source of truth

--- src/components/FeedbackConstants.js
+++ src/components/FeedbackConstants.js
@@ -1,32 +1,35 @@
 /**
- * These values need to be aligned with the API values in the Database
- * (FeedbackComponent)
- *
+ * Single source of truth for feedback categories
+ * These values need to be aligned with the API values in the Database (FeedbackComponent)
  */
-export const FEEDBACK_CODES = Object.freeze({
-  1: "Article Reader",
-  2: "Article Recommendations", 
-  3: "Daily Audio",
-  4: "Exercises",
-  5: "Extension",
-  6: "Sound",
-  7: "Translation",
-  8: "Other",
-});
+const FEEDBACK_CATEGORIES = [
+  { id: 1, name: "Article Reader", key: "ARTICLE_READER" },
+  { id: 2, name: "Article Recommendations", key: "ARTICLE_RECOMMENDATION" }, 
+  { id: 3, name: "Translation", key: "TRANSLATION" },
+  { id: 4, name: "Sound", key: "SOUND" },
+  { id: 5, name: "Exercises", key: "EXERCISE" },
+  { id: 6, name: "Extension", key: "EXTENSION" },
+  { id: 7, name: "Other", key: "OTHER" },
+  { id: 8, name: "Daily Audio", key: "DAILY_AUDIO" },
+];
 
-export const FEEDBACK_CODES_NAME = Object.freeze({
-  ARTICLE_READER: 1,
-  ARTICLE_RECOMMENDATION: 2,
-  DAILY_AUDIO: 3,
-  EXERCISE: 4,
-  EXTENSION: 5,
-  SOUND: 6,
-  TRANSLATION: 7,
-  OTHER: 8,
-});
+// Auto-generated from single source of truth
+export const FEEDBACK_CODES = Object.freeze(
+  FEEDBACK_CATEGORIES.reduce((acc, cat) => {
+    acc[cat.id] = cat.name;
+    return acc;
+  }, {})
+);
+
+export const FEEDBACK_CODES_NAME = Object.freeze(
+  FEEDBACK_CATEGORIES.reduce((acc, cat) => {
+    acc[cat.key] = cat.id;
+    return acc;
+  }, {})
+);
 
 export const FEEDBACK_OPTIONS = {
-  ALL: Object.keys(FEEDBACK_CODES).map(Number),
+  ALL: FEEDBACK_CATEGORIES.map(cat => cat.id),
   EXERCISE: [
     FEEDBACK_CODES_NAME.EXERCISE,
     FEEDBACK_CODES_NAME.SOUND,
