798da824eb35fea22a2558bcf9dac1ee1cf5b902
Mircea Filip Lungu <mircea.lungu@gmail.com>
2025-07-29 12:41:17 +0300

    moved progress bar on the same line with back button; quick hack to move a bit the show solution button;

---
+++ config-overrides.js
@@ -0,0 +1,11 @@
+const { override, addBabelPlugin } = require('customize-cra');
+
+module.exports = override(
+  addBabelPlugin([
+    'babel-plugin-styled-components',
+    {
+      displayName: true,
+      fileName: true
+    }
+  ])
+);
