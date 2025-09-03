2ec64ab8d4de947adc0049c90dd85c5da4464c3e
lobs@itu.dk <70694477+louiselynggaard@users.noreply.github.com>
2025-09-03 12:22:41 +0200

    created page for the swiping and added to nav bar

---
+++ src/articles/ArticleSwipeBrowser.sc.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import { SortButton } from "./SortingButtons.sc";
+
+const MaterialSelection = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
+`;
+
+const SortHolder = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const SearchHolder = styled.div`
+  display: block;
+`;
+
+const ShowVideoOnlyButton = styled(SortButton)`
+  &.selected {
+    background-color: grey;
+    color: white !important;
+    font-weight: 600;
+    &:hover {
+      filter: brightness(1.02);
+    }
+  }
+`;
+
+export { MaterialSelection, SortHolder, SearchHolder, ShowVideoOnlyButton };
