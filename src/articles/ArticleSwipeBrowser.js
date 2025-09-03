4bd7d5feecb0b0e9eea78e216523e5a3ff78ce76
lobs@itu.dk <70694477+louiselynggaard@users.noreply.github.com>
2025-09-03 12:22:41 +0200

    created page for the swiping and added to nav bar

---
+++ src/articles/ArticleSwipeBrowser.js
@@ -0,0 +1,10 @@
+import React from "react";
+import * as s from "./ArticleSwipeBrowser.sc";
+
+export default function ArticleSwipeBrowser() {
+  return (
+    <div>
+      <h2>Swipe Articles</h2>
+    </div>
+  );
+}
