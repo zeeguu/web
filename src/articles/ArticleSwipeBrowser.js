95b4125e61160b7a3a56f560757c5c080dd05e7c
sofiahenden <hendensofia@gmail.com>
2025-09-03 15:28:11 +0200

    Created and added a styled article card without functionality to swipe interface.

--- src/articles/ArticleSwipeBrowser.js
+++ src/articles/ArticleSwipeBrowser.js
@@ -1,9 +1,11 @@
 import React from "react";
+import ArticleCard from "../components/article_swipe/ArticleCard";
 
 export default function ArticleSwipeBrowser() {
   return (
     <div>
       <h2>Swipe through articles</h2>
+        <ArticleCard></ArticleCard>
     </div>
   );
 }
