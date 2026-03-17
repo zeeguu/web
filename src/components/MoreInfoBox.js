245d515818e5a73646d7d43ac7c044ec65d253ac
AnnieeBennie <anse@itu.dk>
2026-03-17 13:54:16 +0100

    created a moreInfoBox to show extra info

---
+++ src/components/MoreInfoBox.js
@@ -0,0 +1,21 @@
+import { MoreInfoBoxContainer } from "./MoreInfoBox.sc.js";
+
+export default function MoreInfoBox({ type }) {
+  const contentMap = {
+    willBeInExercises: {
+      mainInfo: "These words might appear in your exercises depending on your current exercise scheduling.",
+      extraInfo: "You can always change the amount of Words in Learning in Settings",
+    },
+    wontBeInExercises: {
+      mainInfo: "Phrases composed of 3 or more words are not used in the exercises.",
+      extraInfo: "You can still find them in Words or History tab.",
+    },
+  };
+  const data = contentMap[type] || { mainInfo: "", extraInfo: "" };
+  return (
+    <MoreInfoBoxContainer>
+      <h3>{data.mainInfo}</h3>
+      <p>{data.extraInfo}</p>
+    </MoreInfoBoxContainer>
+  );
+}
