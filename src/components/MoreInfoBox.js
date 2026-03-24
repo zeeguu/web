84e8b44198bd052b7ca4d3bce03389eff3b2f1cd
AnnieeBennie <anse@itu.dk>
2026-03-24 13:26:03 +0100

    added links for Words and Words in Learning

--- src/components/MoreInfoBox.js
+++ src/components/MoreInfoBox.js
@@ -1,14 +1,24 @@
+import { Link } from "react-router-dom";
 import { MoreInfoBoxContainer } from "./MoreInfoBox.sc.js";
 
 export default function MoreInfoBox({ type }) {
   const contentMap = {
     willBeInExercises: {
       mainInfo: "These words might appear in your exercises depending on your current scheduling.",
-      extraInfo: "You can always change the amount of Words in Learning in Settings",
+      extraInfo: (
+        <>
+          You can always change the amount of <Link to="/account_settings/exercise_scheduling">Words in Learning</Link>{" "}
+          in Settings
+        </>
+      ),
     },
     wontBeInExercises: {
       mainInfo: "Phrases composed of 3 or more words are not used in the exercises.",
-      extraInfo: "You can still find them in Words or History tab.",
+      extraInfo: (
+        <>
+          You can still find them in <Link to="/words">Words</Link> or History tab.
+        </>
+      ),
     },
   };
   const data = contentMap[type] || { mainInfo: "", extraInfo: "" };
