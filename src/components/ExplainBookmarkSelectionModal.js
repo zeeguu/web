0d008930f06d16dc5ad67cee6228cb589d9b0ffb
AnnieeBennie <anse@itu.dk>
2026-03-24 15:27:08 +0100

    changed the icon

--- src/components/ExplainBookmarkSelectionModal.js
+++ src/components/ExplainBookmarkSelectionModal.js
@@ -5,10 +5,7 @@ import Main from "../components/modal_shared/Main.sc";
 import { APP_DOMAIN } from "../appConstants";
 import { MAX_BOOKMARKS_TO_STUDY_PER_ARTICLE } from "../exercises/ExerciseConstants";
 
-export default function ExplainBookmarkSelectionModal({
-  open,
-  setShowExplainBookmarkSelectionModal,
-}) {
+export default function ExplainBookmarkSelectionModal({ open, setShowExplainBookmarkSelectionModal }) {
   function handleClose() {
     setShowExplainBookmarkSelectionModal(false);
   }
@@ -23,29 +20,20 @@ export default function ExplainBookmarkSelectionModal({
       <Header>
         <p style={{ textAlign: "center" }}>
           {" "}
-          <img
-            style={{ height: "2em" }}
-            src={APP_DOMAIN + "/static/icons/info-icon.png"}
-            alt=""
-          />
+          <img style={{ height: "2em" }} src={APP_DOMAIN + "/static/icons/new-info-icon.png"} alt="" />
         </p>
 
         <Heading>How Zeeguu Selects Words to Practice</Heading>
       </Header>
       <Main>
         <p>
-          When you translate more than{" "}
-          <b>{MAX_BOOKMARKS_TO_STUDY_PER_ARTICLE}</b> bookmarks Zeeguu
-          prioritizes bookmarks based on how common they are in the language you
-          are learning.
+          When you translate more than <b>{MAX_BOOKMARKS_TO_STUDY_PER_ARTICLE}</b> bookmarks Zeeguu prioritizes
+          bookmarks based on how common they are in the language you are learning.
         </p>
+        <p>We do this to ensure that you focus on words that you are more likely to encounter in new texts.</p>
         <p>
-          We do this to ensure that you focus on words that you are more likely
-          to encounter in new texts.
-        </p>
-        <p>
-          Zeeguu remembers any words you add or remove manually, and you will
-          see them in the exercises according to your preferences.
+          Zeeguu remembers any words you add or remove manually, and you will see them in the exercises according to
+          your preferences.
         </p>
       </Main>
     </Modal>
