20b981c02c230a893ded7ea8167b776963cde885
gato <gato@itu.dk>
2026-04-16 21:08:29 +0200

    Readded moreOptions Profile option

--- src/components/MainNav/BottomNav/MoreOptionsPanel.js
+++ src/components/MainNav/BottomNav/MoreOptionsPanel.js
@@ -87,6 +87,7 @@ export default function MoreOptionsPanel({
           <NavOption {...NavigationOptions.settings} currentPath={path} onClick={handleHideMoreOptions} />
 
           <FeedbackButton />
+          {isOnStudentSide && <SideNavProfileOption onClick={handleHideMoreOptions}/>}
         </s.MoreOptionsList>
       </s.MoreOptionsPanel>
 
