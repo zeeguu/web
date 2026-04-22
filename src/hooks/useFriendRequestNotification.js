b73c507c2747818c834d03a6b49393ac2561e43a
xXPinkmagicXx <nickras2001@gmail.com>
2026-04-22 10:34:41 +0200

    Fix userFriendRequestNotification

--- src/hooks/useFriendRequestNotification.js
+++ src/hooks/useFriendRequestNotification.js
@@ -1,9 +1,7 @@
 import { useContext, useEffect, useState } from "react";
-import { useLocation } from "react-router-dom/cjs/react-router-dom";
 import { APIContext } from "../contexts/APIContext";
 
 export default function useFriendRequestNotification() {
-  const path = useLocation().pathname;
   const api = useContext(APIContext);
 
   const [friendRequestCount, setFriendRequestCount] = useState(0);
@@ -11,8 +9,7 @@ export default function useFriendRequestNotification() {
 
   useEffect(() => {
     updateFriendRequestCounter();
-    // eslint-disable-next-line
-  }, [path]);
+  }, []);
 
   function updateFriendRequestCounter() {
     api.getNumberOfReceivedFriendRequests((count) => {
