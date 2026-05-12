5895eb3b4c1bd339a744cbb94e39b3db630efd5e
Anna Semeriuk <annasemeriuk@gmail.com>
2026-05-12 18:25:38 +0200

    fixes

--- src/pages/onboarding/notifications/MoreTranslationsPopup.js
+++ src/pages/onboarding/notifications/MoreTranslationsPopup.js
@@ -3,26 +3,41 @@ import Modal from "../../../components/modal_shared/Modal";
 import Main from "../../../components/modal_shared/Main.sc";
 import Footer from "../../../components/modal_shared/Footer.sc";
 import ButtonContainer from "../../../components/modal_shared/ButtonContainer.sc";
-import { StyledButton } from "../../../components/allButtons.sc";
+import { useContext } from "react";
+import { APIContext } from "../../../contexts/APIContext";
+import { ONBOARDING_MESSAGE_IDS } from "../../../appConstants";
 
 export default function MoreTranslationsPopup({ open, handleCancel }) {
-    if (!open) return null;
+  const api = useContext(APIContext);
+  const onboardingMessageId = ONBOARDING_MESSAGE_IDS.moreTranslations;
 
-    return (
-        <Modal open={open} onClose={handleCancel} wrapperBackgroundColor="#fff1d4" hideCloseButton>
-            <s.MoreTranslationImage src="/static/images/MoreTranslation.png" alt="See more translations illustration" />
-            <Main>
-                <p style={{ textAlign: "center", fontWeight: "500" }}>
-                    Press  <s.ArrowIcon src="/static/icons/ArrowDown.svg" alt="Arrow icon" />    to see more translation options, to unselect a word press <s.DeleteTranslationText>Delete translation.</s.DeleteTranslationText>
-                </p>
-            </Main>
-            <Footer>
-                <ButtonContainer $buttonCountNum={1}>
-                    <StyledButton $onboarding onClick={handleCancel} style={{ minWidth: "190px", margin: "0 auto"}}>
-                        Continue
-                    </StyledButton>
-                </ButtonContainer>
-            </Footer>
-        </Modal>
-    );
+  const handleDismiss = async () => {
+    if (onboardingMessageId) {
+      try {
+        await api.markOnboardingMessageDismissed(onboardingMessageId);
+      } catch (e) {
+        // ignore dismissal recording failures
+      }
+    }
+    if (handleCancel) handleCancel();
+  };
+
+  return (
+    <Modal open={open} onClose={handleDismiss} wrapperBackgroundColor="var(--onboarding-modal-bg)" hideCloseButton>
+      <s.MoreTranslationImage src="/static/images/MoreTranslation.png" alt="See more translations illustration" />
+      <Main>
+        <s.CenteredText>
+          Press <s.ArrowIcon src="/static/icons/ArrowDown.svg" alt="Arrow icon" /> to see more translation options, to
+          unselect a word press <s.DeleteTranslationText>Delete translation.</s.DeleteTranslationText>
+        </s.CenteredText>
+      </Main>
+      <Footer>
+        <ButtonContainer $buttonCountNum={1}>
+          <s.OnboardingPrimaryButton $onboarding onClick={handleDismiss}>
+            Continue
+          </s.OnboardingPrimaryButton>
+        </ButtonContainer>
+      </Footer>
+    </Modal>
+  );
 }
--- src/pages/onboarding/notifications/MoreTranslationsPopup.sc.js
+++ src/pages/onboarding/notifications/MoreTranslationsPopup.sc.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { StyledButton } from "../../../components/allButtons.sc";
 
 const MoreTranslationImage = styled.img`
   width: min(100%, 300px);
@@ -9,9 +10,9 @@ const MoreTranslationImage = styled.img`
   margin-right: auto;
   object-fit: contain;
 
-    @media (max-width: 576px) {
-      width: min(100%, 280px);
-    }
+  @media (max-width: 576px) {
+    width: min(100%, 280px);
+  }
 `;
 
 const ArrowIcon = styled.img`
@@ -25,8 +26,17 @@ const ArrowIcon = styled.img`
 const DeleteTranslationText = styled.b`
   @media (max-width: 576px) {
     display: block;
-    
   }
 `;
+const CenteredText = styled.p`
+  && {
+    text-align: center;
+    font-weight: 500;
+  }
+`;
+const OnboardingPrimaryButton = styled(StyledButton)`
+  min-width: 190px;
+  margin: 0 auto;
+`;
 
-export { MoreTranslationImage, ArrowIcon, DeleteTranslationText };
+export { MoreTranslationImage, ArrowIcon, DeleteTranslationText, CenteredText, OnboardingPrimaryButton };
