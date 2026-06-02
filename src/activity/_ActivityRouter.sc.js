667a8952044871902065b378b1d93328d28be2f1
Anna Semeriuk <annasemeriuk@gmail.com>
2026-06-02 12:01:25 +0200

    added margins

---
+++ src/activity/_ActivityRouter.sc.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const ActivityNarrowColumn = styled.div`
+  max-width: 768px;
+  padding: 1px;
+
+  @media (max-width: 768px) {
+    margin-left: 12px;
+    margin-right: 12px;
+  }
+`;
+
+export { ActivityNarrowColumn };
