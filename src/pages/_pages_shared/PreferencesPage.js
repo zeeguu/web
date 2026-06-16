52850445c14439596282be3872dff346cc1cd7cc
Anna Semeriuk <annasemeriuk@gmail.com>
2026-06-16 12:36:25 +0200

    prettier fix

--- src/pages/_pages_shared/PreferencesPage.js
+++ src/pages/_pages_shared/PreferencesPage.js
@@ -1,13 +1,25 @@
 import * as s from "./PreferencesPage.sc";
 
-export default function PreferencesPage({ children, pageWidth, layoutVariant, isBackgroundFixed, hideBackground, reducedPadding }) {
+export default function PreferencesPage({
+  children,
+  pageWidth,
+  layoutVariant,
+  isBackgroundFixed,
+  hideBackground,
+  reducedPadding,
+}) {
   return (
     <s.PageBackground
       $layoutVariant={layoutVariant}
       $isBackgroundFixed={isBackgroundFixed}
       $hideBackground={hideBackground}
     >
-      <s.PageContainer $layoutVariant={layoutVariant} $pageWidth={pageWidth} $hideBackground={hideBackground} $reducedPadding={reducedPadding}>
+      <s.PageContainer
+        $layoutVariant={layoutVariant}
+        $pageWidth={pageWidth}
+        $hideBackground={hideBackground}
+        $reducedPadding={reducedPadding}
+      >
         <s.ContentWrapper>{children}</s.ContentWrapper>
       </s.PageContainer>
     </s.PageBackground>
