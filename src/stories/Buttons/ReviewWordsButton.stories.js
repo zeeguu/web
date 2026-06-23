d9c7e94bcbde75a6e5a6e8b7da01dcb28ad44b53
Anna Semeriuk <annasemeriuk@gmail.com>
2026-06-23 11:17:02 +0200

    reviewWordsButton story

---
+++ src/stories/Buttons/ReviewWordsButton.stories.js
@@ -0,0 +1,13 @@
+import ReviewWordsButton from "../../reader/ReviewVocabularyInfoBox";
+import "../../index.css";
+
+export default {
+  title: "Buttons/ReviewWordsButton",
+  component: ReviewWordsButton,
+};
+
+export const Default = {
+  args: {
+    bookmarks: ["word1"],
+  },
+};
