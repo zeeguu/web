4efb7cb8cd03718a08fd949981f82ebe97a1c79e
Anna Semeriuk <annasemeriuk@gmail.com>
2026-06-23 13:50:11 +0200

    refactoring

---
+++ src/reader/TopToolbar.sc.js
@@ -0,0 +1,37 @@
+export const leftGroup = {
+  display: "flex",
+  alignItems: "center",
+  gap: "0.5rem",
+};
+
+export const middleGroup = {
+  display: "flex",
+  gap: "1rem",
+};
+
+export const saveButton = {
+  padding: "0.75rem 1.5rem",
+  fontSize: "1rem",
+  fontFamily: "Montserrat",
+  fontWeight: "500",
+  backgroundColor: "#1565C0",
+  color: "white",
+  border: "none",
+  borderRadius: "1.0625rem",
+  cursor: "pointer",
+  display: "flex",
+  alignItems: "center",
+  gap: "0.5rem",
+  whiteSpace: "nowrap",
+};
+
+export const rightGroup = {
+  display: "flex",
+  alignItems: "center",
+  gap: "0.25rem",
+  marginRight: "0.5rem",
+};
+
+export const progressStyle = {
+  margin: "0",
+};
