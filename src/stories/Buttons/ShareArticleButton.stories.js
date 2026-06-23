0f2cb2924b9f1396f0c9b80764d7a45ccc7abe74
Anna Semeriuk <annasemeriuk@gmail.com>
2026-06-23 11:19:58 +0200

    share article button story

---
+++ src/stories/Buttons/ShareArticleButton.stories.js
@@ -0,0 +1,9 @@
+import ShareArticleButton from "../../reader/ShareArticle";
+import "../../index.css";
+
+export default {
+  title: "Buttons/ShareArticleButton",
+  component: ShareArticleButton,
+};
+
+export const Default = {};
