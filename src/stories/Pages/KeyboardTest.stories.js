fd4a1abf7811b2b2d7b450ce4a72fe47aaca123b
Anna Semeriuk <annasemeriuk@gmail.com>
2026-07-07 17:24:52 +0200

    keyboardTest story deleted

--- src/stories/Pages/KeyboardTest.stories.js
+++
@@ -1,9 +0,0 @@
-import KeyboardTest from "../../pages/KeyboardTest/KeyboardTest";
-import "../../index.css";
-
-export default {
-  title: "Pages/KeyboardTest",
-  component: KeyboardTest,
-};
-
-export const Default = {};
