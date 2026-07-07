9d69923b4f95ad50195973856a78d0a536eea5e9
Anna Semeriuk <annasemeriuk@gmail.com>
2026-07-07 16:33:06 +0200

    separated the styles from the keyboardTest file (#1185)
    * separated the styles from the keyboardTest file
    
    + added a folder for the keyboardTest file and its styles file
    
    * fix
    
    * fixed the scrolling issue on the page
    
    the page was not scrollable
    
    * separated keyboard section as a component
    
    * fix

---
+++ src/stories/Pages/KeyboardTest.stories.js
@@ -0,0 +1,9 @@
+import KeyboardTest from "../../pages/KeyboardTest/KeyboardTest";
+import "../../index.css";
+
+export default {
+  title: "Pages/KeyboardTest",
+  component: KeyboardTest,
+};
+
+export const Default = {};
