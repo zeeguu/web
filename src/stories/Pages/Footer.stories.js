d6e9832b898880952884df31366df88578d7f39a
Anna Semeriuk <annasemeriuk@gmail.com>
2026-07-07 14:26:51 +0200

    footer story

---
+++ src/stories/Pages/Footer.stories.js
@@ -0,0 +1,24 @@
+import Footer from "../../pages/_pages_shared/Footer";
+import Button from "../../pages/_pages_shared/Button";
+
+export default {
+  title: "Pages/Footer",
+  component: Footer,
+};
+
+export const Default = {
+  render: () => (
+    <Footer>
+      <Button className="full-width-btn">Primary Action</Button>
+      <Button className="grey small">Secondary Action</Button>
+    </Footer>
+  ),
+};
+
+export const SingleChild = {
+  render: () => (
+    <Footer>
+      <Button>Continue</Button>
+    </Footer>
+  ),
+};
